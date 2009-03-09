4d5b1c5f351cb5c9768c919e7995fc366d6a896b
Xavier Lawrence <xlawrence@jahia.com>
2009-03-09 13:15:52 +0000

    Added optional separator attribute to CurrentPagePathTag in order to allow add separator characters between 2 pages
    git-svn-id: svn://localhost/jahia/trunk@511 69c369ed-ed09-0410-a4a1-e19a46c23626

--- taglib/src/main/java/org/jahia/taglibs/uicomponents/currentpagepath/CurrentPagePathTag.java
+++ taglib/src/main/java/org/jahia/taglibs/uicomponents/currentpagepath/CurrentPagePathTag.java
@@ -1,29 +1,29 @@
 /**
- * 
+ *
  * This file is part of Jahia: An integrated WCM, DMS and Portal Solution
  * Copyright (C) 2002-2009 Jahia Limited. All rights reserved.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * 
+ *
  * As a special exception to the terms and conditions of version 2.0 of
  * the GPL (or any later version), you may redistribute this Program in connection
  * with Free/Libre and Open Source Software ("FLOSS") applications as described
  * in Jahia's FLOSS exception. You should have received a copy of the text
  * describing the FLOSS exception, and it is also available here:
  * http://www.jahia.com/license
- * 
+ *
  * Commercial and Supported Versions of the program
  * Alternatively, commercial and supported versions of the program may be used
  * in accordance with the terms contained in a separate written agreement
@@ -71,7 +71,8 @@ public class CurrentPagePathTag extends AbstractJahiaTag {
     private String unreadablePages = "ellipsis";
     private int hideBeginPages = 0;
     private int hideEndPages = 0;
-    private boolean showHomePage = false ;
+    private boolean showHomePage = false;
+    private String separator;
 
 
     /**
@@ -108,7 +109,11 @@ public class CurrentPagePathTag extends AbstractJahiaTag {
     }
 
     public void setShowHomePage(boolean show) {
-        this.showHomePage = show ;
+        this.showHomePage = show;
+    }
+
+    public void setSeparator(String separator) {
+        this.separator = separator;
     }
 
     public int doStartTag() {
@@ -125,9 +130,9 @@ public class CurrentPagePathTag extends AbstractJahiaTag {
                     getContentPagePath(jData.page().getID(), jParams.getEntryLoadRequest(),
                             jParams.getOperationMode(), jParams.getUser());
 
-            ContentPage homePage = null ;
+            ContentPage homePage = null;
             if (hideBeginPages > 0 && showHomePage) {
-                homePage = vPath.get(0) ;
+                homePage = vPath.get(0);
             }
 
             // remove unwanted entries
@@ -155,7 +160,7 @@ public class CurrentPagePathTag extends AbstractJahiaTag {
             // check page status
             Integer pageState = null;
 
-            boolean checkAcl = SettingsBean.getInstance().isCheckAclInPagePath() ;
+            boolean checkAcl = SettingsBean.getInstance().isCheckAclInPagePath();
 
             for (ContentPage thePage : vPath) {
                 if (thePage != null) {
@@ -185,7 +190,7 @@ public class CurrentPagePathTag extends AbstractJahiaTag {
                             try {
                                 path.append(thePage.getURL(jData.getProcessingContext()));
                             } catch (JahiaException e) {
-                                logger.error(e.getMessage(), e) ;
+                                logger.error(e.getMessage(), e);
                             }
                             path.append("\">");
                         }
@@ -197,6 +202,9 @@ public class CurrentPagePathTag extends AbstractJahiaTag {
                         if (!isCurrentPage) {
                             path.append("</a>");
                         }
+                        if (separator != null) {
+                            path.append(separator);
+                        }
                         path.append("</li>\n");
                     } else {
                         if (unreadablePages.equals("display")) {
@@ -205,9 +213,9 @@ public class CurrentPagePathTag extends AbstractJahiaTag {
                                 path.append("<a>");
                             }
                             path.append(title != null ? title : "n.d.");
-                             if (!isCurrentPage) {
-                            path.append("</a>");
-                             }
+                            if (!isCurrentPage) {
+                                path.append("</a>");
+                            }
                             path.append("</li>");
                         } else if (unreadablePages.equals("ellipsis") && !unreachablePageDisplayed) {
                             path.append("<li><a>...</a></li>");
@@ -235,6 +243,7 @@ public class CurrentPagePathTag extends AbstractJahiaTag {
         // let's reinitialize the tag variables to allow tag object reuse in
         // pooling.
         cssClassName = null;
+        separator = null;
         maxchar = 0;
         unreadablePages = "ellipsis";
         hideBeginPages = 0;

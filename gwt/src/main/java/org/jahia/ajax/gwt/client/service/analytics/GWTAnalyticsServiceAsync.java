622d48ded2a269253ba8e4acda60fd688397dc5c
Ibrahim El Ghandour <ielghandour@jahia.com>
2009-03-13 19:33:35 +0000

    Analytics : service for the analytics module
    git-svn-id: svn://localhost/jahia/trunk@647 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/service/analytics/GWTAnalyticsServiceAsync.java
@@ -0,0 +1,39 @@
+package org.jahia.ajax.gwt.client.service.analytics;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jahia
+ * Date: 4 mars 2009
+ * Time: 10:29:39
+ *
+ * @author Ibrahim El Ghandour
+ */
+public interface GWTAnalyticsServiceAsync {
+    public void getLastNActivities(int N, AsyncCallback<Map<String, String>> async);
+
+    public void getMostNactiveUsers(int N,AsyncCallback<Map<String, String>> asyncCallback);
+
+    public void getLeastNactiveUsers(int N,AsyncCallback<Map<String, String>> asyncCallback);
+
+    public void getActivitiesPerUser(String user, AsyncCallback<Map<String, String>> asyncCallback);
+
+    public void getActivitiesPerObjectId(int id, AsyncCallback<Map<String, String>> asyncCallback);
+
+    public void getActivitiesPerPageUUID(String uuid, AsyncCallback<Map<String, String>> asyncCallback);
+
+    public void getActivitiesPerSiteKey(int id, AsyncCallback<Map<String, String>> asyncCallback);
+
+    public void executeQuery(String user, String op, String type, int objId, String uuid,int pid, int siteId,int N,AsyncCallback<Map<String, String>> asyncCallback);
+
+    public void getAllUsers(AsyncCallback<Map<String, String>> asyncCallback);
+
+    public void flushDatabase(AsyncCallback<Void> asyncCallback);
+
+    public void flushDatabaseOldest(AsyncCallback<Void> asyncCallback);
+
+
+}

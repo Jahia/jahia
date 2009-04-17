9f97c4feeb6c23c5633ba4a80205a189e730fffd
Khaled Tlili <ktlili@jahia.com>
2009-04-17 15:40:38 +0000

    [Product-JIRA] - fix PORTAL-275: remove portlet only if "OK" is clicked
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-0-BRANCH@1250 69c369ed-ed09-0410-a4a1-e19a46c23626

--- gwt/src/main/java/org/jahia/ajax/gwt/client/widget/layoutmanager/listener/OnPortletRemoved.java
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/widget/layoutmanager/listener/OnPortletRemoved.java
@@ -1,29 +1,29 @@
 /**
- * 
+ *
  * This file is part of Jahia: An integrated WCM, DMS and Portal Solution
  * Copyright (C) 2002-2009 Jahia Limited. All rights reserved.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * 
+ *
  * As a special exception to the terms and conditions of version 2.0 of
  * the GPL (or any later version), you may redistribute this Program in connection
  * with Free/Libre and Open Source Software ("FLOSS") applications as described
  * in Jahia's FLOSS exception. You should have received a copy of the text
  * describing the FLOSS exception, and it is also available here:
  * http://www.jahia.com/license
- * 
+ *
  * Commercial and Supported Versions of the program
  * Alternatively, commercial and supported versions of the program may be used
  * in accordance with the terms contained in a separate written agreement
@@ -37,6 +37,8 @@ import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.ComponentEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.MessageBox;
+import com.extjs.gxt.ui.client.widget.Dialog;
+import com.extjs.gxt.ui.client.widget.button.Button;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.Window;
 import com.allen_sauer.gwt.log.client.Log;
@@ -50,7 +52,7 @@ import org.jahia.ajax.gwt.client.core.JahiaPageEntryPoint;
  * Date: 17 nov. 2008
  * Time: 21:35:05
  */
-public class OnPortletRemoved  extends SelectionListener<ComponentEvent> {
+public class OnPortletRemoved extends SelectionListener<ComponentEvent> {
     private JahiaPortlet jahiaPortlet;
 
     public OnPortletRemoved(JahiaPortlet jahiaPortlet) {
@@ -58,18 +60,23 @@ public class OnPortletRemoved  extends SelectionListener<ComponentEvent> {
     }
 
     public void componentSelected(ComponentEvent event) {
+        final MessageBox box = new MessageBox();
         final Listener confirmBoxListener = new Listener<ComponentEvent>() {
             public void handleEvent(ComponentEvent ce) {
-                close();
+                Dialog dialog = (Dialog) ce.component;
+                Button btn = dialog.getButtonPressed();
+                if (btn.getText().equalsIgnoreCase(MessageBox.OK)) {
+                    close();
+                }
             }
         };
 
-        final MessageBox box = new MessageBox();
+
         box.setButtons(MessageBox.YESNOCANCEL);
         box.setIcon(MessageBox.QUESTION);
         box.setTitle(jahiaPortlet.getHeading());
         box.addCallback(confirmBoxListener);
-        box.setMessage("Do you really want to remove: <br/> "+jahiaPortlet.getHeading() +"?");
+        box.setMessage("Do you really want to remove: <br/> " + jahiaPortlet.getHeading() + "?");
         box.show();
 
     }
@@ -78,7 +85,7 @@ public class OnPortletRemoved  extends SelectionListener<ComponentEvent> {
         // make a call ajax
         LayoutmanagerService.App.getInstance().removeLayoutItem(JahiaPageEntryPoint.getJahiaGWTPage(), jahiaPortlet.getPorletConfig(), new AsyncCallback() {
             public void onSuccess(Object o) {
-                Log.debug("Portlet removed: "+jahiaPortlet.getPorletConfig().getPortlet());
+                Log.debug("Portlet removed: " + jahiaPortlet.getPorletConfig().getPortlet());
                 JahiaPortalManager.getInstance().removeJahiaPortlet(jahiaPortlet);
             }
 

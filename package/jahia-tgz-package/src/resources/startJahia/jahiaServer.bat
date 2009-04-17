3a0f6991adf1adbeebee91accf011842a63b0e84
Islam El Sayed <ielsayed@jahia.com>
2009-04-17 09:56:23 +0000

    added loading screen
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-0-BRANCH@1230 69c369ed-ed09-0410-a4a1-e19a46c23626

--- package/jahia-tgz-package/src/resources/startJahia/jahiaServer.bat
+++ package/jahia-tgz-package/src/resources/startJahia/jahiaServer.bat
@@ -30,7 +30,7 @@ set PATH=%BINDIR%\..\jikes\bin;%PATH%
 call bin\catalina-jikes.bat start
 :skipjikes
 cd webapps/config/WEB-INF/classes
-java org/jahia/init/TomcatWaitInstallation
+java -classpath "%CLASSPATH%;.\;.\WEB-INF\lib\log4j-1.2.15.jar" org.jahia.init.TomcatWaitInstallation
 start http://localhost:8080/cms/
 echo Starting browser...
 echo Done. Please wait while systems initialize...

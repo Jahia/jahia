151fefd3d25ae1cfbbeabe727821bf7587eccca7
Thomas Draier <tdraier@jahia.com>
2009-04-30 16:14:39 +0000

    jackrabbit schemas ( JAHIA-4610 )
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-0-BRANCH@1513 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ war/src/main/webapp/WEB-INF/var/db/sql/schema/derby/jackrabbit-schema.sql
@@ -0,0 +1,43 @@
+drop table jr_default_BUNDLE;
+drop table jr_default_REFS;
+drop table jr_default_BINVAL;
+drop table jr_default_NAMES;
+drop table jr_v_BUNDLE;
+drop table jr_v_REFS;
+drop table jr_v_BINVAL;
+drop table jr_v_NAMES;
+drop table jr_fsdefault_FSENTRY;
+drop table jr_fsg_FSENTRY;
+drop table jr_fsv_FSENTRY;
+drop table JOURNAL;
+drop table GLOBAL_REVISION;
+drop table LOCAL_REVISIONS;
+
+create table jr_default_BUNDLE (NODE_ID_HI bigint not null, NODE_ID_LO bigint not null, BUNDLE_DATA blob(2G) not null, PRIMARY KEY (NODE_ID_HI, NODE_ID_LO));
+create table jr_default_REFS (NODE_ID_HI bigint not null, NODE_ID_LO bigint not null, REFS_DATA blob(2G) not null, PRIMARY KEY (NODE_ID_HI, NODE_ID_LO));
+create table jr_default_BINVAL (BINVAL_ID char(64) PRIMARY KEY, BINVAL_DATA blob(2G) not null);
+create table jr_default_NAMES (ID INTEGER GENERATED ALWAYS AS IDENTITY, NAME varchar(255) not null, PRIMARY KEY (ID, NAME));
+create table jr_v_BUNDLE (NODE_ID_HI bigint not null, NODE_ID_LO bigint not null, BUNDLE_DATA blob(2G) not null, PRIMARY KEY (NODE_ID_HI, NODE_ID_LO));
+create table jr_v_REFS (NODE_ID_HI bigint not null, NODE_ID_LO bigint not null, REFS_DATA blob(2G) not null, PRIMARY KEY (NODE_ID_HI, NODE_ID_LO));
+create table jr_v_BINVAL (BINVAL_ID char(64) PRIMARY KEY, BINVAL_DATA blob(2G) not null);
+create table jr_v_NAMES (ID INTEGER GENERATED ALWAYS AS IDENTITY, NAME varchar(255) not null, PRIMARY KEY (ID, NAME));
+create table jr_fsdefault_FSENTRY (FSENTRY_PATH varchar(2048) not null, FSENTRY_NAME varchar(255) not null, FSENTRY_DATA blob(100M), FSENTRY_LASTMOD bigint not null, FSENTRY_LENGTH bigint not null);
+create table jr_fsg_FSENTRY (FSENTRY_PATH varchar(2048) not null, FSENTRY_NAME varchar(255) not null, FSENTRY_DATA blob(100M), FSENTRY_LASTMOD bigint not null, FSENTRY_LENGTH bigint not null);
+create table jr_fsv_FSENTRY (FSENTRY_PATH varchar(2048) not null, FSENTRY_NAME varchar(255) not null, FSENTRY_DATA blob(100M), FSENTRY_LASTMOD bigint not null, FSENTRY_LENGTH bigint not null);
+create table JOURNAL (REVISION_ID BIGINT NOT NULL, JOURNAL_ID varchar(255), PRODUCER_ID varchar(255), REVISION_DATA blob);
+create table GLOBAL_REVISION (REVISION_ID BIGINT NOT NULL);
+create table LOCAL_REVISIONS (JOURNAL_ID varchar(255) NOT NULL, REVISION_ID BIGINT NOT NULL);
+
+create unique index jr_default_BUNDLE_IDX on jr_default_BUNDLE (NODE_ID);
+create unique index jr_default_REFS_IDX on jr_default_REFS (NODE_ID);
+create unique index jr_default_BINVAL_IDX on jr_default_BINVAL (BINVAL_ID);
+create unique index jr_v_BUNDLE_IDX on jr_v_BUNDLE (NODE_ID);
+create unique index jr_v_REFS_IDX on jr_v_REFS (NODE_ID);
+create unique index jr_v_BINVAL_IDX on jr_v_BINVAL (BINVAL_ID);
+create unique index jr_fsdefault_FSENTRY_IDX on jr_fsdefault_FSENTRY (FSENTRY_PATH, FSENTRY_NAME);
+create unique index jr_fsg_FSENTRY_IDX on jr_fsg_FSENTRY (FSENTRY_PATH, FSENTRY_NAME);
+create unique index jr_fsv_FSENTRY_IDX on jr_fsv_FSENTRY (FSENTRY_PATH, FSENTRY_NAME);
+create unique index JOURNAL_IDX on JOURNAL (REVISION_ID);
+create unique index GLOBAL_REVISION_IDX on GLOBAL_REVISION (REVISION_ID);
+
+insert into GLOBAL_REVISION VALUES(0);
---
+++ war/src/main/webapp/WEB-INF/var/db/sql/schema/mssql/jackrabbit-schema.sql
@@ -0,0 +1,43 @@
+drop table jr_default_BUNDLE;
+drop table jr_default_REFS;
+drop table jr_default_BINVAL;
+drop table jr_default_NAMES;
+drop table jr_v_BUNDLE;
+drop table jr_v_REFS;
+drop table jr_v_BINVAL;
+drop table jr_v_NAMES;
+drop table jr_fsdefault_FSENTRY;
+drop table jr_fsg_FSENTRY;
+drop table jr_fsv_FSENTRY;
+drop table JOURNAL;
+drop table GLOBAL_REVISION;
+drop table LOCAL_REVISIONS;
+
+create table jr_default_BUNDLE (NODE_ID binary(16) not null, BUNDLE_DATA image not null);
+create table jr_default_REFS (NODE_ID binary(16) not null, REFS_DATA image not null);
+create table jr_default_BINVAL (BINVAL_ID varchar(64) not null, BINVAL_DATA image not null);
+create table jr_default_NAMES (ID INTEGER IDENTITY(1,1) PRIMARY KEY, NAME varchar(255) COLLATE Latin1_General_CS_AS not null);
+create table jr_v_BUNDLE (NODE_ID binary(16) not null, BUNDLE_DATA image not null);
+create table jr_v_REFS (NODE_ID binary(16) not null, REFS_DATA image not null);
+create table jr_v_BINVAL (BINVAL_ID varchar(64) not null, BINVAL_DATA image not null);
+create table jr_v_NAMES (ID INTEGER IDENTITY(1,1) PRIMARY KEY, NAME varchar(255) COLLATE Latin1_General_CS_AS not null);
+create table jr_fsdefault_FSENTRY (FSENTRY_PATH varchar(2048) not null, FSENTRY_NAME varchar(255) not null, FSENTRY_DATA image null, FSENTRY_LASTMOD bigint not null, FSENTRY_LENGTH bigint not null);
+create table jr_fsg_FSENTRY (FSENTRY_PATH varchar(2048) not null, FSENTRY_NAME varchar(255) not null, FSENTRY_DATA image null, FSENTRY_LASTMOD bigint not null, FSENTRY_LENGTH bigint not null);
+create table jr_fsv_FSENTRY (FSENTRY_PATH varchar(2048) not null, FSENTRY_NAME varchar(255) not null, FSENTRY_DATA image null, FSENTRY_LASTMOD bigint not null, FSENTRY_LENGTH bigint not null);
+create table JOURNAL  (REVISION_ID BIGINT NOT NULL, JOURNAL_ID varchar(255), PRODUCER_ID varchar(255), REVISION_DATA IMAGE);
+create table GLOBAL_REVISION (REVISION_ID BIGINT NOT NULL) ;
+create table LOCAL_REVISIONS (JOURNAL_ID varchar(255) NOT NULL, REVISION_ID BIGINT NOT NULL);
+
+create unique index jr_default_BUNDLE_IDX on jr_default_BUNDLE (NODE_ID);
+create unique index jr_default_REFS_IDX on jr_default_REFS (NODE_ID);
+create unique index jr_default_BINVAL_IDX on jr_default_BINVAL (BINVAL_ID);
+create unique index jr_v_BUNDLE_IDX on jr_v_BUNDLE (NODE_ID);
+create unique index jr_v_REFS_IDX on jr_v_REFS (NODE_ID);
+create unique index jr_v_BINVAL_IDX on jr_v_BINVAL (BINVAL_ID);
+create unique index jr_fsdefault_FSENTRY_IDX on jr_fsdefault_FSENTRY (FSENTRY_PATH, FSENTRY_NAME);
+create unique index jr_fsg_FSENTRY_IDX on jr_fsg_FSENTRY (FSENTRY_PATH, FSENTRY_NAME);
+create unique index jr_fsv_FSENTRY_IDX on jr_fsv_FSENTRY (FSENTRY_PATH, FSENTRY_NAME);
+create unique index JOURNAL_IDX on JOURNAL (REVISION_ID);
+create unique index GLOBAL_REVISION_IDX on GLOBAL_REVISION (REVISION_ID);
+
+insert into GLOBAL_REVISION VALUES(0);

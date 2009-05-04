195dcce5b60521169e1feb0bbd6d644e4f9c7594
Sergiy Shyrkov <sshyrkov@jahia.com>
2009-05-04 15:19:00 +0000

    Added event type for workflow notification mail templates for debug purpose
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-0-BRANCH@1552 69c369ed-ed09-0410-a4a1-e19a46c23626

--- war/src/main/webapp/templates/default/notifications/events/workflowCurrentUser/email.groovy
+++ war/src/main/webapp/templates/default/notifications/events/workflowCurrentUser/email.groovy
@@ -2,5 +2,5 @@
 
 //text = ""
 //html = ""
-subject = i18n.get("notifications.workflow.subject", "[Jahia] Workflow operation report")
+subject = i18n.get("notifications.workflow.subject", "[Jahia] Workflow operation report") + " - ??? " + eventType + " ???"
 
--- war/src/main/webapp/templates/default/notifications/events/workflowNextStepUsers/email.groovy
+++ war/src/main/webapp/templates/default/notifications/events/workflowNextStepUsers/email.groovy
@@ -2,5 +2,4 @@
 
 //text = ""
 //html = ""
-subject = i18n.get("notifications.workflow.subject", "[Jahia] Workflow operation report")
-
+subject = i18n.get("notifications.workflow.subject", "[Jahia] Workflow operation report") + " - ??? " + eventType + " ???"

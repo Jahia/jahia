4c8f9ae9414e01fa261a871b56d5c1f8efa93e0a
Serge Huber <shuber@jahia.com>
2009-05-05 09:17:52 +0000

    Fix for JAHIA-4657 : Fix Oracle scripts that weren't executing correctly.
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-0-BRANCH@1567 69c369ed-ed09-0410-a4a1-e19a46c23626

--- war/src/main/webapp/WEB-INF/var/db/sql/schema/oracle/jackrabbit-schema.sql
+++ war/src/main/webapp/WEB-INF/var/db/sql/schema/oracle/jackrabbit-schema.sql
@@ -33,14 +33,14 @@ create table LOCAL_REVISIONS (JOURNAL_ID varchar(255) NOT NULL, REVISION_ID numb
 create unique index jr_default_BUNDLE_IDX on jr_default_BUNDLE (NODE_ID);
 create unique index jr_default_REFS_IDX on jr_default_REFS (NODE_ID);
 create unique index jr_default_BINVAL_IDX on jr_default_BINVAL (BINVAL_ID);
-create unique index jr_default_NAMES_IDX on jr_default_NAMES (NAME) ${tableSpace}
-create sequence jr_default_seq_names_id
+create unique index jr_default_NAMES_IDX on jr_default_NAMES (NAME);
+create sequence jr_default_seq_names_id;
 create trigger jr_default_t1 before insert on jr_default_NAMES for each row begin select jr_default_seq_names_id.nextval into :new.id from dual; end;
 create unique index jr_v_BUNDLE_IDX on jr_v_BUNDLE (NODE_ID);
 create unique index jr_v_REFS_IDX on jr_v_REFS (NODE_ID);
 create unique index jr_v_BINVAL_IDX on jr_v_BINVAL (BINVAL_ID);
-create unique index jr_v_NAMES_IDX on jr_v_NAMES (NAME) ${tableSpace}
-create sequence jr_v_seq_names_id
+create unique index jr_v_NAMES_IDX on jr_v_NAMES (NAME);
+create sequence jr_v_seq_names_id;
 create trigger jr_v_t1 before insert on jr_v_NAMES for each row begin select jr_v_seq_names_id.nextval into :new.id from dual; end;
 create unique index jr_fsdefault_FSENTRY_IDX on jr_fsdefault_FSENTRY (FSENTRY_PATH, FSENTRY_NAME);
 create unique index jr_fsg_FSENTRY_IDX on jr_fsg_FSENTRY (FSENTRY_PATH, FSENTRY_NAME);

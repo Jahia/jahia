93d6c6ec3c39cbadcc8fd7ed5375e0f2b5a92d8e
Sergiy Shyrkov <sshyrkov@jahia.com>
2009-05-21 12:19:16 +0000

    Replaced enterprise license header with the community one (GPL)
    git-svn-id: svn://localhost/jahia/trunk@1805 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/data/fields/JahiaSmallTextSharedLangField.java
+++ core/src/main/java/org/jahia/data/fields/JahiaSmallTextSharedLangField.java
@@ -1,18 +1,33 @@
 /**
- * Jahia Enterprise Edition v6
+ * This file is part of Jahia: An integrated WCM, DMS and Portal Solution
+ * Copyright (C) 2002-2009 Jahia Solutions Group SA. All rights reserved.
  *
- * Copyright (C) 2002-2009 Jahia Solutions Group. All rights reserved.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * Jahia delivers the first Open Source Web Content Integration Software by combining Enterprise Web Content Management
- * with Document Management and Portal features.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- * The Jahia Enterprise Edition is delivered ON AN "AS IS" BASIS, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
- * IMPLIED.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Jahia Enterprise Edition must be used in accordance with the terms contained in a separate license agreement between
- * you and Jahia (Jahia Sustainable Enterprise License - JSEL).
+ * As a special exception to the terms and conditions of version 2.0 of
+ * the GPL (or any later version), you may redistribute this Program in connection
+ * with Free/Libre and Open Source Software ("FLOSS") applications as described
+ * in Jahia's FLOSS exception. You should have received a copy of the text
+ * describing the FLOSS exception, and it is also available here:
+ * http://www.jahia.com/license
  *
- * If you are unsure which license is appropriate for your use, please contact the sales department at sales@jahia.com.
+ * Commercial and Supported Versions of the program
+ * Alternatively, commercial and supported versions of the program may be used
+ * in accordance with the terms contained in a separate written agreement
+ * between you and Jahia Solutions Group SA. If you are unsure which license is appropriate
+ * for your use, please contact the sales department at sales@jahia.com.
  */
 //  JahiaSmalltextSharedLangField
 

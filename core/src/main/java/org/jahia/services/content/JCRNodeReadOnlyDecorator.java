1d6e57a48a6fde5043aeaa9e0cca4a5e22f3220b
Romain Felden <rfelden@jahia.com>
2009-05-27 13:47:26 +0000

    added name property in jcr readonly decorator
    git-svn-id: svn://localhost/jahia/trunk@1829 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/content/JCRNodeReadOnlyDecorator.java
+++ core/src/main/java/org/jahia/services/content/JCRNodeReadOnlyDecorator.java
@@ -56,6 +56,10 @@ public class JCRNodeReadOnlyDecorator {
         this.node = node;
     }
 
+    public String getName() {
+        return node.getName();
+    }
+
     public String getUrl() {
         return node.getUrl();
     }

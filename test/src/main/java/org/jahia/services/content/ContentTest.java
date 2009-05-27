2283475127211e97743291d6617bdc1f20f842b7
Thomas Draier <tdraier@jahia.com>
2009-05-27 18:14:16 +0000

    small test suite for content providers
    git-svn-id: svn://localhost/jahia/trunk@1837 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ test/src/main/java/org/jahia/services/content/ContentTest.java
@@ -0,0 +1,165 @@
+package org.jahia.services.content;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.apache.commons.io.IOUtils;
+import org.jahia.bin.Jahia;
+import org.jahia.params.ProcessingContext;
+import org.jahia.registries.ServicesRegistry;
+import org.jahia.services.content.impl.jahia.JahiaContentStoreProvider;
+
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * This is a test case
+ */
+public class ContentTest extends TestCase {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        JCRStoreService service = ServicesRegistry.getInstance().getJCRStoreService();
+        final Map<String, JCRStoreProvider> mountPoints = service.getMountPoints();
+        for (String providerRoot : mountPoints.keySet()) {
+            if (mountPoints.get(providerRoot) instanceof JahiaContentStoreProvider) {
+                continue;
+            }
+            if (providerRoot.equals("/")) {
+                providerRoot = "/content/shared";
+            }
+            Method[] methods = ContentTest.class.getMethods();
+            for (Method method : methods) {
+                if (method.getName().startsWith("test")) {
+                    suite.addTest(new ContentTest(method.getName(),providerRoot));
+                }
+            }
+        }
+        return suite;
+    }
+
+    private ProcessingContext ctx;
+    private String providerRoot;
+
+    public ContentTest(String name, String path) {
+        super(name);
+        this.providerRoot = path;
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        ctx = Jahia.getThreadParamBean();
+    }
+
+    @Override
+    public String getName() {
+        return super.getName() + " in " +providerRoot ;
+    }
+
+    /**
+     * Test creation / deletion of folder
+     *
+     * @throws RepositoryException
+     */
+    public void testCreateFolder() throws Exception {
+        JCRStoreService service = ServicesRegistry.getInstance().getJCRStoreService();
+        final Map<String, JCRStoreProvider> mountPoints = service.getMountPoints();
+
+        JCRSessionWrapper session = service.getThreadSession(ctx.getUser());
+
+        try {
+            JCRNodeWrapper rootNode = session.getNode(providerRoot);
+
+            final String name = "test" + System.currentTimeMillis();
+
+            JCRNodeWrapper testCollection = rootNode.createCollection(name);
+            session.save();
+
+            assertTrue(providerRoot + " : Created folder is not valid", testCollection.isValid());
+            assertTrue(providerRoot + " : Created folder is not a collection", testCollection.isCollection());
+
+//        long creationDate = testCollection.getCreationDateAsDate().getTime();
+//        assertTrue(providerRoot+ " : Creation date invalid", creationDate < System.currentTimeMillis() && creationDate > System.currentTimeMillis()-10000);
+
+//        long lastModifiedDate = testCollection.getLastModifiedAsDate().getTime();
+//        assertTrue(providerRoot+ " : Modification date invalid", lastModifiedDate < System.currentTimeMillis() && lastModifiedDate > System.currentTimeMillis()-10000);
+
+            testCollection = session.getNode(providerRoot + "/" + name);
+
+            assertTrue(providerRoot + " : Folder cannot be reloaded", testCollection.isValid());
+
+            testCollection.remove();
+            session.save();
+
+            try {
+                session.getNode(providerRoot + "/" + name);
+                assertTrue(providerRoot + " : Folder has not been deleted", true);
+
+            } catch (PathNotFoundException e) {
+                // ok
+            }
+        } finally {
+            session.logout();
+        }
+    }
+
+    public void testUpload() throws Exception {
+        JCRStoreService service = ServicesRegistry.getInstance().getJCRStoreService();
+        final Map<String, JCRStoreProvider> mountPoints = service.getMountPoints();
+
+        JCRSessionWrapper session = service.getThreadSession(ctx.getUser());
+
+        try {
+            JCRNodeWrapper rootNode = session.getNode(providerRoot);
+
+            String value = "This is a test";
+            String mimeType = "text/plain";
+
+            InputStream is = new ByteArrayInputStream(value.getBytes("UTF-8"));
+
+            String name = "test" + System.currentTimeMillis() + ".txt";
+            JCRNodeWrapper testFile = rootNode.uploadFile(name, is, mimeType);
+            session.save();
+
+            assertTrue(providerRoot + " : Created file is not valid", testFile.isValid());
+
+            assertEquals(providerRoot + " : Size is not the same", value.length(), testFile.getFileContent().getContentLength());
+            assertEquals(providerRoot + " : Mime type is not the same", mimeType, testFile.getFileContent().getContentType());
+
+//        long creationDate = testFile.getCreationDateAsDate().getTime();
+//        assertTrue(providerRoot+ " : Creation date invalid", creationDate < System.currentTimeMillis() && creationDate > System.currentTimeMillis()-10000);
+
+//        long lastModifiedDate = testFile.getLastModifiedAsDate().getTime();
+//        assertTrue(providerRoot+ " : Modification date invalid", lastModifiedDate < System.currentTimeMillis() && lastModifiedDate > System.currentTimeMillis()-10000);
+
+            testFile = session.getNode(providerRoot + "/" + name);
+
+            assertTrue(providerRoot + " : File cannot be reloaded", testFile.isValid());
+
+            is = testFile.getFileContent().downloadFile();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            IOUtils.copy(is, baos);
+            String checkString = new String(baos.toByteArray(), "UTF-8");
+            assertEquals(providerRoot + " : File content is different", value, checkString);
+
+            testFile.remove();
+            session.save();
+
+            try {
+                session.getNode(providerRoot + "/" + name);
+                assertTrue(providerRoot + " : File has not been deleted", true);
+
+            } catch (PathNotFoundException e) {
+                // ok
+            }
+        } finally {
+            session.logout();
+        }
+    }
+
+}

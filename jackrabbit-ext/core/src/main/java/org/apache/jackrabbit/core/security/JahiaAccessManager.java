865116f8460cbdacd32d2554aeabd0507d3a8daf
Cédric Mailleux <cmailleux@jahia.com>
2009-09-08 14:40:22 +0000

    Add Another authorized permission to jcr:write due to migration to jackrabbit 1.6.0
    git-svn-id: svn://localhost/jahia/trunk@2893 69c369ed-ed09-0410-a4a1-e19a46c23626

--- jackrabbit-ext/core/src/main/java/org/apache/jackrabbit/core/security/JahiaAccessManager.java
+++ jackrabbit-ext/core/src/main/java/org/apache/jackrabbit/core/security/JahiaAccessManager.java
@@ -129,10 +129,10 @@ public class JahiaAccessManager implements AccessManager {
         permissions.put("jcr:setProperties", Permission.SET_PROPERTY);
         permissions.put("jcr:addChildNodes", Permission.ADD_NODE);
         permissions.put("jcr:removeChildNodes", Permission.REMOVE_NODE);
-        permissions.put(Constants.JCR_WRITE_RIGHTS, Permission.SET_PROPERTY + Permission.REMOVE_PROPERTY + Permission.ADD_NODE + Permission.REMOVE_NODE);
+        permissions.put(Constants.JCR_WRITE_RIGHTS, Permission.SET_PROPERTY + Permission.REMOVE_PROPERTY + Permission.ADD_NODE + Permission.REMOVE_NODE + Permission.NODE_TYPE_MNGMT);
         permissions.put("jcr:getAccessControlPolicy", Permission.READ);
         permissions.put("jcr:setAccessControlPolicy", Permission.SET_PROPERTY + Permission.REMOVE_PROPERTY);
-        permissions.put("jcr:all", Permission.READ + Permission.SET_PROPERTY + Permission.REMOVE_PROPERTY + Permission.ADD_NODE + Permission.REMOVE_NODE);
+        permissions.put("jcr:all", Permission.READ + Permission.SET_PROPERTY + Permission.REMOVE_PROPERTY + Permission.ADD_NODE + Permission.REMOVE_NODE + Permission.NODE_TYPE_MNGMT);
 
     }
 

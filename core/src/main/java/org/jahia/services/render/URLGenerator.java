c0054e4eb6e595e0e6250dff6d05ef08bc23a304
Sergiy Shyrkov <sshyrkov@jahia.com>
2009-12-16 09:56:34 +0000

    Added getter to obtain current main resource URL using ${url.mainRessource} in templates (current page URL in the corresponding mode)
    git-svn-id: svn://localhost/jahia/trunk@4056 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/URLGenerator.java
+++ core/src/main/java/org/jahia/services/render/URLGenerator.java
@@ -3,26 +3,20 @@ package org.jahia.services.render;
 import org.jahia.api.Constants;
 import org.jahia.bin.Edit;
 import org.jahia.bin.Render;
-import org.jahia.services.usermanager.jcr.JCRUser;
 import org.jahia.services.usermanager.JahiaUserManagerService;
-import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.content.JCRStoreService;
 import org.apache.commons.collections.map.LazyMap;
 import org.apache.commons.collections.Transformer;
 
 import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
 
 /**
  * Main URL generation class. This class is exposed to the template developers to make it easy to them to access
- * basic URLs such as
- * ${url.edit}
- * ${url.userProfile}
+ * basic URLs such as <code>${url.edit}</code>, <code>${url.userProfile}</code>.
  * User: toto
  * Date: Sep 14, 2009
  * Time: 11:13:37 AM
- * To change this template use File | Settings | File Templates.
  */
 public class URLGenerator {
 
@@ -37,6 +31,10 @@ public class URLGenerator {
     private Resource resource;
     private RenderContext context;
     private JCRStoreService jcrStoreService;
+    
+    private Map<String, String> languages;
+
+    private Map<String, String> templates;
 
     public URLGenerator(RenderContext context, Resource resource, JCRStoreService jcrStoreService) {
         this.context = context;
@@ -46,7 +44,7 @@ public class URLGenerator {
     }
 
     /**
-     * Set worksapce url as attribute of the current request
+     * Set workspace url as attribute of the current request
      */
     protected void initURL() {
         if (context.isEditMode()) {
@@ -55,15 +53,11 @@ public class URLGenerator {
             base = context.getRequest().getContextPath() + Render.getRenderServletPath() + "/" + resource.getWorkspace() + "/" + resource.getLocale();
         }
 
-        live = context.getRequest().getContextPath() + Render.getRenderServletPath() + "/" + Constants.LIVE_WORKSPACE + "/" + resource.getLocale();
-        edit = context.getRequest().getContextPath() + Edit.getEditServletPath() + "/" + Constants.EDIT_WORKSPACE + "/" + resource.getLocale();
-        preview = context.getRequest().getContextPath() + Render.getRenderServletPath() + "/" + Constants.EDIT_WORKSPACE + "/" + resource.getLocale();
+        final String resourcePath = context.getMainResource().getNode().getPath()  + ".html";
 
-        final String resourcePath = context.getMainResource().getNode().getPath();
-
-        live += resourcePath + ".html";
-        edit += resourcePath + ".html";
-        preview += resourcePath + ".html";
+        live = context.getRequest().getContextPath() + Render.getRenderServletPath() + "/" + Constants.LIVE_WORKSPACE + "/" + resource.getLocale() + resourcePath;
+        edit = context.getRequest().getContextPath() + Edit.getEditServletPath() + "/" + Constants.EDIT_WORKSPACE + "/" + resource.getLocale() + resourcePath;
+        preview = context.getRequest().getContextPath() + Render.getRenderServletPath() + "/" + Constants.EDIT_WORKSPACE + "/" + resource.getLocale() + resourcePath;
     }
 
     public String getContext() {
@@ -92,10 +86,8 @@ public class URLGenerator {
 
     public String getUserProfile() {
         if (userProfile == null) {
-            if (!context.getUser().getName().equals(JahiaUserManagerService.GUEST_USERNAME)) {
-                List<JCRNodeWrapper> userFolders = jcrStoreService.getUserFolders(null, context.getUser());
-                String userHomePath = userFolders.iterator().next().getPath();
-                userProfile = base + userHomePath + ".html";
+            if (!JahiaUserManagerService.isGuest(context.getUser())) {
+                userProfile = base + jcrStoreService.getUserFolders(null, context.getUser()).iterator().next().getPath() + ".html";
             }
         }
         return userProfile;
@@ -113,25 +105,50 @@ public class URLGenerator {
         }
     }
 
+    @SuppressWarnings("unchecked")
     public Map<String, String> getLanguages() {
-        return LazyMap.decorate(new HashMap(), new Transformer() {
-            public Object transform(Object lang) {
-                String servletPath;
-                if (context.isEditMode()) {
-                    servletPath = Edit.getEditServletPath();
-                } else {
-                    servletPath = Render.getRenderServletPath();
+        if (languages == null) {
+            languages = LazyMap.decorate(new HashMap(), new Transformer() {
+                public Object transform(Object lang) {
+                    String servletPath;
+                    if (context.isEditMode()) {
+                        servletPath = Edit.getEditServletPath();
+                    } else {
+                        servletPath = Render.getRenderServletPath();
+                    }
+                    return context.getRequest().getContextPath() + servletPath + "/" + resource.getWorkspace() + "/" + lang + resource.getNode().getPath() + ".html";
                 }
-                return context.getRequest().getContextPath() + servletPath + "/" + resource.getWorkspace() + "/" + lang + resource.getNode().getPath() + ".html";
-            }
-        });
+            });
+        }
+        
+        return languages;
     }
 
+    @SuppressWarnings("unchecked")
     public Map<String, String> getTemplates() {
-        return LazyMap.decorate(new HashMap(), new Transformer() {
-            public Object transform(Object template) {
-                return base + resource.getNode().getPath() + "." + template + ".html";
-            }
-        });
+        if (templates == null) {
+            templates = LazyMap.decorate(new HashMap(), new Transformer() {
+                public Object transform(Object template) {
+                    return base + resource.getNode().getPath() + "." + template + ".html";
+                }
+            });
+        }
+        return templates;
     }
+
+    /**
+     * Returns the URL of the main resource (normally, page), depending on the
+     * current mode.
+     * 
+     * @return the URL of the main resource (normally, page), depending on the
+     *         current mode
+     */
+    public String getMainResource() {
+        if (context.isEditMode()) {
+            return getEdit();
+        } else {
+            return Constants.LIVE_WORKSPACE.equals(resource.getWorkspace()) ? live : preview;
+        }
+    }
+
 }

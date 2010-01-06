56e138fcd677f87875cb48b7afe9cea607eb7549
Sergiy Shyrkov <sshyrkov@jahia.com>
2010-01-06 15:01:41 +0000

    Activate mod-rss deployment + some cosmetics
    git-svn-id: svn://localhost/jahia/trunk@4294 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ templates/mod-rss/src/main/java/org/jahia/modules/rss/RSSUtil.java
@@ -0,0 +1,132 @@
+/**
+ * This file is part of Jahia: An integrated WCM, DMS and Portal Solution
+ * Copyright (C) 2002-2010 Jahia Solutions Group SA. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * As a special exception to the terms and conditions of version 2.0 of
+ * the GPL (or any later version), you may redistribute this Program in connection
+ * with Free/Libre and Open Source Software ("FLOSS") applications as described
+ * in Jahia's FLOSS exception. You should have received a copy of the text
+ * describing the FLOSS exception, and it is also available here:
+ * http://www.jahia.com/license
+ *
+ * Commercial and Supported Versions of the program
+ * Alternatively, commercial and supported versions of the program may be used
+ * in accordance with the terms contained in a separate written agreement
+ * between you and Jahia Solutions Group SA. If you are unsure which license is appropriate
+ * for your use, please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.modules.rss;
+
+import com.sun.syndication.feed.synd.*;
+import com.sun.syndication.fetcher.FeedFetcher;
+import com.sun.syndication.fetcher.impl.FeedFetcherCache;
+import com.sun.syndication.fetcher.impl.HashMapFeedInfoCache;
+import com.sun.syndication.fetcher.impl.HttpURLFeedFetcher;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
+import org.apache.log4j.Logger;
+import org.jahia.ajax.gwt.utils.RSSHelper;
+import java.io.IOException;
+import java.net.URL;
+
+
+/**
+ * Utility class for loading RSS feeds.
+ * User: ktlili
+ * Date: Jan 5, 2010
+ * Time: 4:07:04 PM
+ */
+public class RSSUtil {
+
+    private static final transient Logger logger = Logger.getLogger(RSSHelper.class);
+    
+    private SyndFeed feed;
+    
+    private boolean loaded;
+
+    private String url;
+
+    /**
+     * Get a SyndFeed from an url
+     * @param url
+     * @return
+     */
+    public static SyndFeed loadSyndFeed(String url){
+        SyndFeed feed = null;
+        try {
+            final SyndFeedInput input = new SyndFeedInput();
+            final XmlReader reader = new XmlReader(new URL(url));
+            feed = input.build(reader);
+        } catch (FeedException e) {
+            logger.error("Error retrieving RSS feed from URL: " + url + ". Cause: " + e.getMessage(), e);
+        } catch (IOException e) {
+            // another way to load feed RSS
+            final FeedFetcherCache feedInfoCache = HashMapFeedInfoCache.getInstance();
+            final FeedFetcher fetcher = new HttpURLFeedFetcher(feedInfoCache);
+            try {
+                feed = fetcher.retrieveFeed(new URL(url));
+            } catch (Exception e1) {
+                logger.error("Error retrieving RSS feed from URL: " + url + ". Cause: " + e1.getMessage(), e1);
+            }
+        } catch (Exception e) {
+            logger.error("Error retrieving RSS feed from URL: " + url + ". Cause: " + e.getMessage(), e);
+        }
+        return feed;
+    }
+
+//    /**
+//     * Reformat codes coming from google news
+//     *
+//     * @param entry
+//     */
+//    private static void reformatGoogleNewsEntry(SyndEntry entry) {
+//        final SyndContent entryDescription = entry.getDescription();
+//        if (entryDescription != null) {
+//            final String description = entryDescription.getValue();
+//            final int descBegin = description.lastIndexOf("<font size=-1>") + 14;
+//            final int descEnd = description.indexOf("</font>", descBegin);
+//
+//            if (descBegin != -1 && descEnd != -1) {
+//                final String realDesc = description.substring(descBegin, descEnd);
+//                entryDescription.setValue(realDesc);
+//            }
+//        }
+//    }
+//    
+    /**
+     * Sets the feed URL to load and parse.
+     * 
+     * @param url the URl of the feed to load
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Returns the parsed feed object. 
+     * @return the parsed feed object
+     */
+    public SyndFeed getFeed() {
+        if (!loaded) {
+            loaded = true;
+            feed = loadSyndFeed(url);
+        }
+        
+        return feed;
+    }
+}
--- templates/mod-rss/src/main/webapp/jnt_rss/html/rss.jsp
+++ templates/mod-rss/src/main/webapp/jnt_rss/html/rss.jsp
@@ -4,50 +4,48 @@
 <%@ taglib prefix="utility" uri="http://www.jahia.org/tags/utilityLib" %>
 <%@ taglib prefix="template" uri="http://www.jahia.org/tags/templateLib" %>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
-<%@ taglib prefix="ui" uri="http://www.jahia.org/tags/uiComponentsLib" %>
-<%@ taglib prefix="query" uri="http://www.jahia.org/tags/queryLib" %>
-<%@ page import="com.sun.syndication.feed.synd.*" %>
-<%@ page import="org.jahia.services.content.JCRNodeWrapper" %>
-<%@ page import="org.jahia.rss.RSSUtil" %>
-<%
-    // to do: replaced by a tag or a least a filter.
-    JCRNodeWrapper node = (JCRNodeWrapper) pageContext.getAttribute("currentNode", PageContext.REQUEST_SCOPE);
-    if (node.hasProperty("url")) {
-        request.setAttribute("syndFeed", RSSUtil.loadSyndFeed(node.getProperty("url").getString()));
-    }
-%>
-<c:if test="${syndFeed == null}">
+
+<jcr:nodeProperty node="${currentNode}" name="jcr:title" var="title"/>
+<jcr:nodeProperty node="${currentNode}" name="url" var="feedUrl"/>
+<jcr:nodeProperty node="${currentNode}" name="nbEntries" var="nbEntries"/>
+<jsp:useBean id="rss" class="org.jahia.modules.rss.RSSUtil"/>
+<c:set target="${rss}" property="url" value="${feedUrl.string}"/>
+<%-- load the feed using RSSUtil --%>
+<c:set var="syndFeed" value="${rss.feed}"/>
+<c:if test="${not empty title && not empty title.string}">
+	<h3>${fn:escapeXml(title.string)}</h3>
+</c:if>
+<c:if test="${empty syndFeed}">
     <jcr:nodeProperty node="${currentNode}" name="url" var="feedUrl"/>
-    Invalid feed url: ${feedUrl.string}
-</c:if>>
-<c:if test="${syndFeed != null}">
+    <fmt:message key="jnt_rss.rssLoadError"><fmt:param value="${feedUrl.string}"/></fmt:message>
+</c:if>
+<c:if test="${not empty syndFeed}">
     <div class="syndFeed">
         <div class="syndFeedTitle">
-            <a href="${syndFeed.link}"> "${syndFeed.title}</a>
-            <c:if test="${syndFeed.image != null}">
-                  <img src="${syndFeed.image.url}" title="${syndFeed.image.title}" alt="${syndFeed.image.description}"/>
+            <a href="${fn:escapeXml(syndFeed.link)}">${fn:escapeXml(syndFeed.title)}</a>
+            <c:if test="${not empty syndFeed.image}">
+                  <img src="${fn:escapeXml(syndFeed.image.url)}" title="${fn:escapeXml(syndFeed.image.title)}" alt="${fn:escapeXml(syndFeed.image.description)}"/>
             </c:if>
         </div>
         <div class="syndFeedEntries">
-            <c:forEach items="${syndFeed.entries}" var="syndEntry">
+            <c:forEach items="${syndFeed.entries}" var="syndEntry" begin="0" end="${nbEntries.long - 1}">
                 <div class="syndEntryTitle">
-                    <a href="${syndEntry.link}"> ${syndEntry.title} [${syndEntry.updatedDate}]</a>
+                    <a href="${fn:escapeXml(syndEntry.link)}">${fn:escapeXml(syndEntry.title)}&nbsp;[${fn:escapeXml(syndEntry.updatedDate)}]</a>
                 </div>
                 <div class="syndEntryDescription">
-                        ${syndEntry.description.value}
+                    ${syndEntry.description.value}
                 </div>
                 <div class="syndEntryContents">
                     <c:forEach items="${syndEntry.contents}" var="syndContent">
                         <div class="syndEntryContent">
-                                ${syndContent.value}
+                        	${syndContent.value}
                         </div>
                     </c:forEach>
                 </div>
                 <div class="syndEntryAuthor">
-                        ${syndEntry.author}
+                        ${fn:escapeXml(syndEntry.author)}
                 </div>
             </c:forEach>
         </div>
     </div>
 </c:if>
-

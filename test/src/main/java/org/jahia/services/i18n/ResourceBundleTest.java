db0eff8b0f32a22fa0b39894ab82f7a696349127
Khaled Tlili <ktlili@jahia.com>
2010-01-27 11:51:27 +0000

    [ResourceBundle ] - test that detect keys with same values
    git-svn-id: svn://localhost/jahia/trunk@4552 69c369ed-ed09-0410-a4a1-e19a46c23626

--- test/src/main/java/org/jahia/services/i18n/ResourceBundleTest.java
+++ test/src/main/java/org/jahia/services/i18n/ResourceBundleTest.java
@@ -4,8 +4,7 @@ import junit.framework.TestCase;
 import org.jahia.utils.LanguageCodeConverters;
 import org.jahia.utils.i18n.JahiaResourceBundle;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 /**
@@ -38,55 +37,21 @@ public class ResourceBundleTest extends TestCase {
                     // check if value exist for different key
                     boolean valueExist = valueKeyMap.containsKey(value);
                     if (valueExist) {
-                        System.err.println(key + " and " + valueKeyMap.get(value) + " have same value: " + value);
+                        System.err.println(key + " = " + value);
+                        System.err.println(valueKeyMap.get(value) + " = " + value);
+                    } else {
+                        // put value in map
+                        valueKeyMap.put(value, key);
                     }
 
                     duplicatedValue = duplicatedValue || valueExist;
 
-                    // put value in map
-                    valueKeyMap.put(value, key);
 
                 }
             } catch (MissingResourceException e) {
                 e.printStackTrace();
             }
             assertFalse(duplicatedValue);
-
-        }
-    }
-
-
-    /**
-     * Test that key name corresponds to a given name comvention
-     * @throws Exception
-     */
-    public void testKeyName() throws Exception {
-        final ResourceBundle resourceBundle = ResourceBundle.getBundle(JahiaResourceBundle.JAHIA_INTERNAL_RESOURCES, Locale.ENGLISH);
-        assertNotNull(resourceBundle);
-
-        if (resourceBundle != null) {
-            boolean valideName = true;
-            try {
-                Enumeration<String> enume = resourceBundle.getKeys();
-                while (enume.hasMoreElements()) {
-                    String key = enume.nextElement();
-
-                    // check if value exist for different key
-                   /* boolean valide = key.indexOf("org.jahia") == 0;
-                    if (!valide) {
-                        System.err.println(key + " not valid." );
-                    }
-
-                    valideName = valideName && valide;*/
-
-                    // put value in map
-
-                }
-            } catch (MissingResourceException e) {
-                e.printStackTrace();
-            }
-            assertTrue(valideName);
-
         }
     }
 

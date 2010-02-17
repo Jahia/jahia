c41f06fd26779efcc398803994b00999b1a6e5df
Thomas Draier <tdraier@jahia.com>
2010-02-17 19:21:58 +0000

    unpublish process
    git-svn-id: svn://localhost/jahia/trunk@4970 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ core/src/main/java/org/jahia/services/workflow/jbpm/custom/Unpublish.java
@@ -0,0 +1,36 @@
+package org.jahia.services.workflow.jbpm.custom;
+
+import org.jahia.api.Constants;
+import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.JCRPublicationService;
+import org.jahia.services.content.JCRSessionFactory;
+import org.jbpm.api.activity.ActivityExecution;
+import org.jbpm.api.activity.ExternalActivityBehaviour;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Publish custom activity for jBPM workflow
+ *
+ * Publish the current node
+ *
+ */
+public class Unpublish implements ExternalActivityBehaviour {
+    private static final long serialVersionUID = 1L;
+
+    public void execute(ActivityExecution execution) throws Exception {
+        String id = (String) execution.getVariable("nodeId");
+        String workspace = (String) execution.getVariable("workspace");
+        Locale locale = (Locale) execution.getVariable("locale");
+
+        JCRNodeWrapper node = JCRSessionFactory.getInstance().getCurrentUserSession().getNodeByUUID(id);
+        JCRPublicationService.getInstance().unpublish(node.getPath(), Collections.singleton(locale.toString()));
+        execution.takeDefaultTransition();
+    }
+
+    public void signal(ActivityExecution execution, String signalName, Map<String, ?> parameters) throws Exception {
+    }
+
+}

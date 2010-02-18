fb771e67d5b5b96316f292c9ea58813d9388326e
Thomas Draier <tdraier@jahia.com>
2010-02-18 19:46:38 +0000

    allows write permission to lock
    git-svn-id: svn://localhost/jahia/trunk@5018 69c369ed-ed09-0410-a4a1-e19a46c23626

--- jackrabbit-ext/core/src/main/java/org/apache/jackrabbit/core/security/JahiaAccessManager.java
+++ jackrabbit-ext/core/src/main/java/org/apache/jackrabbit/core/security/JahiaAccessManager.java
@@ -147,7 +147,7 @@ public class JahiaAccessManager implements AccessManager, AccessControlManager {
         permissions.put("jcr:setProperties", Permission.SET_PROPERTY);
         permissions.put("jcr:addChildNodes", Permission.ADD_NODE);
         permissions.put("jcr:removeChildNodes", Permission.REMOVE_NODE);
-        permissions.put(Constants.JCR_WRITE_RIGHTS, Permission.SET_PROPERTY + Permission.REMOVE_PROPERTY + Permission.ADD_NODE + Permission.REMOVE_NODE + Permission.NODE_TYPE_MNGMT + Permission.VERSION_MNGMT);
+        permissions.put(Constants.JCR_WRITE_RIGHTS, Permission.SET_PROPERTY + Permission.REMOVE_PROPERTY + Permission.ADD_NODE + Permission.REMOVE_NODE + Permission.NODE_TYPE_MNGMT + Permission.LOCK_MNGMT + Permission.VERSION_MNGMT);
         permissions.put("jcr:getAccessControlPolicy", Permission.READ);
         permissions.put("jcr:setAccessControlPolicy", Permission.SET_PROPERTY + Permission.REMOVE_PROPERTY);
         permissions.put("jcr:all", Permission.READ + Permission.SET_PROPERTY + Permission.REMOVE_PROPERTY + Permission.ADD_NODE + Permission.REMOVE_NODE + Permission.NODE_TYPE_MNGMT);

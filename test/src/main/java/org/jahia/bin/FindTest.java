d52e1c6e28ff81fd6be8ea350034cfd9dbb1cd21
Serge Huber <shuber@jahia.com>
2010-02-25 18:18:48 +0000

    Working on SQL2 test, but not yet working.
    git-svn-id: svn://localhost/jahia/trunk@5070 69c369ed-ed09-0410-a4a1-e19a46c23626

--- test/src/main/java/org/jahia/bin/FindTest.java
+++ test/src/main/java/org/jahia/bin/FindTest.java
@@ -22,9 +22,13 @@ import java.io.IOException;
  */
 public class FindTest extends TestCase {
 
-    public void testFind() throws IOException, JSONException, JahiaException {
+    HttpClient client;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();    //To change body of overridden methods use File | Settings | File Templates.
         // Create an instance of HttpClient.
-        HttpClient client = new HttpClient();
+        client = new HttpClient();
 
         // todo we should really insert content to test the find.
 
@@ -39,6 +43,24 @@ public class FindTest extends TestCase {
         if (statusCode != HttpStatus.SC_OK) {
             System.err.println("Method failed: " + loginMethod.getStatusLine());
         }
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();    //To change body of overridden methods use File | Settings | File Templates.
+
+        PostMethod logoutMethod = new PostMethod("http://localhost:8080/cms/logout");
+        logoutMethod.addParameter("redirectActive", "false");
+
+        int statusCode = client.executeMethod(logoutMethod);
+        if (statusCode != HttpStatus.SC_OK) {
+            System.err.println("Method failed: " + logoutMethod.getStatusLine());
+        }
+
+        logoutMethod.releaseConnection();
+    }
+
+    public void testFindWithXPath() throws IOException, JSONException, JahiaException {
 
         PostMethod method = new PostMethod("http://localhost:8080/cms/find/default/en");
         method.addParameter("query", "/jcr:root/sites/mySite//element(*, nt:base)[jcr:contains(.,'{$q}*')]");
@@ -52,34 +74,54 @@ public class FindTest extends TestCase {
         method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
                 new DefaultHttpMethodRetryHandler(3, false));
 
-        try {
-            // Execute the method.
-            statusCode = client.executeMethod(method);
+        // Execute the method.
+        int statusCode = client.executeMethod(method);
 
-            if (statusCode != HttpStatus.SC_OK) {
-                System.err.println("Method failed: " + method.getStatusLine());
-            }
-
-            // Read the response body.
-            String responseBody = method.getResponseBodyAsString();
-
-            JSONArray jsonResults = new JSONArray(responseBody);
-
-            assertNotNull("A proper JSONObject instance was expected, got null instead", jsonResults);
-
-            // @todo we need to add more tests to validate depthLimit implementation.
-
-            PostMethod logoutMethod = new PostMethod("http://localhost:8080/cms/logout");
-            logoutMethod.addParameter("redirectActive", "false");
-
-            statusCode = client.executeMethod(logoutMethod);
-            if (statusCode != HttpStatus.SC_OK) {
-                System.err.println("Method failed: " + loginMethod.getStatusLine());
-            }
-
-        } finally {
-            // Release the connection.
-            method.releaseConnection();
+        if (statusCode != HttpStatus.SC_OK) {
+            System.err.println("Method failed: " + method.getStatusLine());
         }
+
+        // Read the response body.
+        String responseBody = method.getResponseBodyAsString();
+
+        JSONArray jsonResults = new JSONArray(responseBody);
+
+        assertNotNull("A proper JSONObject instance was expected, got null instead", jsonResults);
+
+        // @todo we need to add more tests to validate results.
+
     }
+
+    public void testFindWithSQL2() throws IOException, JSONException {
+
+        PostMethod method = new PostMethod("http://localhost:8080/cms/find/default/en");
+        method.addParameter("query", "select * from [nt:base] where isdescendantnode([/jcr:root/sites/mySite/]) and contains(.,'{$q}*')");
+        method.addParameter("q", "a:+-*\"&()[]{}$/\\%\'"); // to test if the reserved characters work correctly.
+        method.addParameter("language", javax.jcr.query.Query.JCR_SQL2);
+        method.addParameter("propertyMatchRegexp", "{$q}.*");
+        method.addParameter("removeDuplicatePropValues", "true");
+        method.addParameter("depthLimit", "1");
+
+        // Provide custom retry handler is necessary
+        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+                new DefaultHttpMethodRetryHandler(3, false));
+
+        // Execute the method.
+        int statusCode = client.executeMethod(method);
+
+        if (statusCode != HttpStatus.SC_OK) {
+            System.err.println("Method failed: " + method.getStatusLine());
+        }
+
+        // Read the response body.
+        String responseBody = method.getResponseBodyAsString();
+
+        JSONArray jsonResults = new JSONArray(responseBody);
+
+        assertNotNull("A proper JSONObject instance was expected, got null instead", jsonResults);
+
+        // @todo we need to add more tests to validate results.
+
+    }
+
 }

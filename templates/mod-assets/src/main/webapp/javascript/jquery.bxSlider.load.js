5fe1d4d237c36224a4f1c9b5b824efb8389fdc1d
Sergiy Shyrkov <sshyrkov@jahia.com>
2010-04-06 11:20:47 +0000

    BACKLOG-583: Clean-up Resources Management http://jira.jahia.org/browse/BACKLOG-583 Resolution: added mod-assets module with common static assets (CSS, JavaScript and related image files). Dropped duplicate resources.
    git-svn-id: svn://localhost/jahia/trunk@5774 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ templates/mod-assets/src/main/webapp/javascript/jquery.bxSlider.load.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+			$('#example1').bxSlider({
+				mode: 'slide',
+				speed: 250,
+				wrapper_class: 'example1_container'
+			});
+
+			$('#example2').bxSlider({
+				mode: 'slide',
+				auto: 'true',
+				controls: false,
+				speed: 1500,
+				pause: 3500,
+				width: 869,
+				wrapper_class: 'example2_container'
+			});
+
+			$('#example3').bxSlider({
+				mode: 'fade',
+				speed: 1000,
+				pause: 5000,
+				auto: true,
+				controls: true,
+				auto_direction: 'right',
+				next_text: 'next image',
+				prev_text: 'previous image',
+				width: 307,
+				wrapper_class: 'example3_container'
+			});
+
+			$('#example4').bxSlider({
+				mode: 'ticker',
+				speed: 7000,
+				width: 500,
+				wrapper_class: 'example4_container'
+			});
+
+		});

cce2c092be2e5dab38ca0361ccea227cbf6450fd
Sergiy Shyrkov <sshyrkov@jahia.com>
2010-04-23 07:13:44 +0000

    Added configuration for three CKeditor toolbars (Full, Basic, Light). To be later used in conjunction with roles and permissions to show appropriate toolbar to the user.
    git-svn-id: svn://localhost/jahia/trunk@6059 69c369ed-ed09-0410-a4a1-e19a46c23626

--- gwt/src/main/resources/org/jahia/ajax/gwt/public/ckeditor-3.2.1/config.js
+++ gwt/src/main/resources/org/jahia/ajax/gwt/public/ckeditor-3.2.1/config.js
@@ -7,7 +7,7 @@ CKEDITOR.editorConfig = function( config )
 {
     config.contextPath = (typeof jahiaGWTParameters != 'undefined') ? jahiaGWTParameters.contextPath : '';
     config.language = (typeof jahiaGWTParameters != 'undefined') ? jahiaGWTParameters.uilang : 'en';
-	config.skin = 'jahia';
+    config.skin = 'jahia';
     
     config.filebrowserWindowWidth = '810';
     config.filebrowserWindowHeight = '650';
@@ -18,36 +18,55 @@ CKEDITOR.editorConfig = function( config )
     config.filebrowserLinkBrowseUrl = config.contextPath + '/engines/gwtcontentpicker/contentpicker.jsp?type=linkpicker';
     config.image_previewText = '';
 
-    config.toolbar = 'Engine';
-    config.toolbar_Engine =
-        [
-            ['Source','-','Preview','-','Templates'],
-            ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print','SpellChecker'],
-            ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-            '/',
-            ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-            ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-            ['Link','Unlink','Anchor'],
-            ['Image','Flash','Table','HorizontalRule','SpecialChar'],
-            '/',
-            ['Styles','Format','Font','FontSize'],
-            ['TextColor','BGColor'],
-            ['Maximize','ShowBlocks','-','About']
-        ];
-    config.toolbar_User =
-        [
-         	['Cut','Copy','Paste','PasteText','PasteFromWord','-', 'SpellChecker', 'Scayt'],
-         	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-         	['Link','Unlink','Anchor', 'Image','LinkFile'],
-         	['HorizontalRule','Smiley','SpecialChar','PageBreak'],
-         	'/',
-            ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-            ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-            '/',
-            ['Styles','Format','Font','FontSize'],
-            ['TextColor','BGColor'],
-            ['Maximize', 'ShowBlocks']
-        ];
+    config.toolbar = 'Full';
+    config.toolbar_Full = [
+        ['Source','-',/*'Save',*/'NewPage','Preview','-','Templates'],
+        ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        //['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+        '/',
+        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        ['Link','Unlink','Anchor'],
+        ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+        '/',
+        ['Styles','Format','Font','FontSize'],
+        ['TextColor','BGColor'],
+        ['Maximize', 'ShowBlocks','-','About']
+    ];
+    config.toolbar_Basic = [
+        ['Source','-',/*'Save',*/'NewPage','Preview','-','Templates'],
+        ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        //['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+        '/',
+        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+        ['NumberedList','BulletedList'/*,'-','Outdent','Indent','Blockquote','CreateDiv'*/],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        ['Link','Unlink','Anchor'],
+        ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar'/*,'PageBreak'*/],
+        '/',
+        //['Styles','Format','Font','FontSize'],
+        //['TextColor','BGColor'],
+        ['Maximize', 'ShowBlocks','-','About']
+    ];
+    config.toolbar_Light = [
+        ['Bold','Italic','Underline','Strike','-','NumberedList','BulletedList']
+    ];
+    config.toolbar_User = [
+        ['Cut','Copy','Paste','PasteText','PasteFromWord','-', 'SpellChecker', 'Scayt'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Link','Unlink','Anchor', 'Image','LinkFile'],
+        ['HorizontalRule','Smiley','SpecialChar','PageBreak'],
+        '/',
+        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        '/',
+        ['Styles','Format','Font','FontSize'],
+        ['TextColor','BGColor'],
+        ['Maximize', 'ShowBlocks']
+    ];
+    
 };

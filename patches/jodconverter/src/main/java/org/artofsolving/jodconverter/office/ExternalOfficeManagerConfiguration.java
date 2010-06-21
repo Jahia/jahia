f91e191155e364f1bdf3edb0b8169820693e06ae
Sergiy Shyrkov <sshyrkov@jahia.com>
2010-06-21 05:47:41 +0000

    JODConverter patch
    git-svn-id: svn://localhost/jahia/trunk@7196 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ patches/jodconverter/src/main/java/org/artofsolving/jodconverter/office/ExternalOfficeManagerConfiguration.java
@@ -0,0 +1,60 @@
+//
+// JODConverter - Java OpenDocument Converter
+// Copyright 2009 Art of Solving Ltd
+// Copyright 2004-2009 Mirko Nasato
+//
+// JODConverter is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// JODConverter is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General
+// Public License along with JODConverter.  If not, see
+// <http://www.gnu.org/licenses/>.
+//
+package org.artofsolving.jodconverter.office;
+
+public class ExternalOfficeManagerConfiguration {
+
+    private OfficeConnectionProtocol connectionProtocol = OfficeConnectionProtocol.SOCKET;
+    private int portNumber = 2002;
+    private String host = null;
+    private String pipeName = "office";
+    private boolean connectOnStart = true;
+
+    public ExternalOfficeManagerConfiguration setConnectionProtocol(OfficeConnectionProtocol connectionProtocol) {
+        this.connectionProtocol = connectionProtocol;
+        return this;
+    }
+
+    public ExternalOfficeManagerConfiguration setPortNumber(int portNumber) {
+        this.portNumber = portNumber;
+        return this;
+    }
+
+    public ExternalOfficeManagerConfiguration setHost(String host) {
+        this.host = host;
+        return this;
+    }
+
+    public ExternalOfficeManagerConfiguration setPipeName(String pipeName) {
+        this.pipeName = pipeName;
+        return this;
+    }
+
+    public ExternalOfficeManagerConfiguration setConnectOnStart(boolean connectOnStart) {
+        this.connectOnStart = connectOnStart;
+        return this;
+    }
+
+    public OfficeManager buildOfficeManager() {
+        UnoUrl unoUrl = connectionProtocol == OfficeConnectionProtocol.SOCKET ? (host != null ? UnoUrl.socket(host, portNumber) : UnoUrl.socket(portNumber)) : UnoUrl.pipe(pipeName);
+        return new ExternalOfficeManager(unoUrl, connectOnStart);
+    }
+    
+}

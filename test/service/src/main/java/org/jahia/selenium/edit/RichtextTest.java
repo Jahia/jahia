fc76e76fef65562a394be09bc2b77d86c9ed6e66
Kevan Jahanshahi <kevan@jahia.com>
2010-09-21 14:36:34 +0000

    final version of first selenium test
    git-svn-id: svn://localhost/jahia/trunk@8719 69c369ed-ed09-0410-a4a1-e19a46c23626

--- test/service/src/main/java/org/jahia/selenium/edit/RichtextTest.java
+++ test/service/src/main/java/org/jahia/selenium/edit/RichtextTest.java
@@ -11,8 +11,10 @@ import org.jahia.test.TestHelper;
 public class RichtextTest extends SeleneseTestCase {
 
     private static Logger logger = Logger.getLogger(RichtextTest.class);
+    private String[] ids;
     private JahiaSite site;
     private final static String TESTSITE_NAME = "mySite";
+    private final static int numberOfNodes = 10;
 
     /*  protected DefaultSelenium createSeleniumClient(String url) throws Exception {
     return new DefaultSelenium("localhost", 4444, "*firefox", url);
@@ -64,80 +66,72 @@ public class RichtextTest extends SeleneseTestCase {
                 }
             }
         }
-        for (int i = 0; i < 15; i++) {
+        for (int i = 1; i < numberOfNodes; i++) {
+            //click on "Any content"
             new Wait("wait") {
-                public boolean until() {
-                    return selenium.isTextPresent("Any content");
-                }
-            };
-            String[] buttonIds = selenium.getEval(GwtButtons()).split(",");
-            final String[] finalButtonIds = buttonIds;
-            new Wait("wait") {
-                public boolean until() {
-                    return selenium.isElementPresent(finalButtonIds[1]);
-                }
-            };
-            selenium.click(buttonIds[1]);
+                    public boolean until() {
+                        return selenium.isTextPresent("Any content");
+                    }
+                };
+            while(!selenium.isTextPresent("Editorial content")){
+                ids = selenium.getEval(GwtButtons()).split(",");
+                selenium.click(ids[1]);
+            }
 
-            //wait for the text "Editorial content appear" it's usefull to wait the pop of the content's choice
+            //click on the image for sort"
             new Wait("wait") {
                 public boolean until() {
                     return selenium.isTextPresent("Editorial content");
                 }
             };
-            //clic on the sort by img
-            buttonIds = selenium.getEval(sortImg()).split(",");
-            selenium.click(buttonIds[0]);
+            ids = selenium.getEval(sortImg()).split(",");
+            selenium.click(ids[0]);
 
-            //wait after the sort by
+            //Double click on "Editorial content"
             new Wait("wait") {
                 public boolean until() {
                     return selenium.isTextPresent("Editorial content");
                 }
             };
-            //clic on this button
-            buttonIds = selenium.getEval(TreeNodes()).split(",");
-            selenium.click("//div[@id='" + buttonIds[10] + "']/div/table/tbody/tr/td[3]/img");
+            ids = selenium.getEval(TreeNodes()).split(",");
+            selenium.doubleClick(ids[10]);
 
-            //new wait for the text "Rich text"
+            //Double click on "Rich Text"
             new Wait("wait") {
                 public boolean until() {
                     return selenium.isTextPresent("Rich text");
                 }
             };
-            //clic on Rich text
-            buttonIds = selenium.getEval(LabelComponent()).split(",");
-            selenium.doubleClick("//div[@id='" + buttonIds[18] + "']/div/table/tbody/tr/td[3]/img");
+            ids = selenium.getEval(TreeNodes()).split(",");
+            selenium.doubleClick(ids[18]);
 
-            //wait for the properties of rich text pop
+            //Click on "source"
             new Wait("wait") {
                 public boolean until() {
                     return selenium.isTextPresent("Source");
                 }
             };
-            //clic on "source"  button
-            buttonIds = selenium.getEval(sourceButton()).split(",");
-            selenium.waitForCondition("selenium.browserbot.getCurrentWindow().document.getElementById('" + buttonIds[0] + "')", "10000");
-            selenium.click(buttonIds[0]);
+            ids = selenium.getEval(sourceButton()).split(",");
+            selenium.waitForCondition("selenium.browserbot.getCurrentWindow().document.getElementById('" + ids[0] + "')", "10000");
+            selenium.click(ids[0]);
 
-            //wait for write into source
+            //Fill in source content
             new Wait("wait") {
                 public boolean until() {
                     return selenium.isTextPresent("Source");
                 }
             };
-            buttonIds = selenium.getEval(sourceContent()).split(",");
-            selenium.type("//td[@id='" + buttonIds[0] + "']/textarea", "<p>\\n\t<br />\\n\tJ&#39;ai essay&eacute; avec une autre fonction sp&eacute;cial pour firefox vu la struture du DOM avec mozilla mais ca ne marche pas.<br />\\n\t<br />\\n\tCe qui m&#39;&eacute;tonne le plus dans l&#39;affichage du alert dans firefox c&#39;est le fait qu&#39;il m&#39;affiche : object HTMLImageElement, object HTMLMapElement, object Text, object HTMLMapElement, ...<br />\\n\t<br />\\n\tA chaque clic il me recupere l&#39;arborescence petit a petit (car dans le div 2 il ya une image, une def de map, un espace, une autre def de map, ...) mais ne m&#39;affiche rien dans le div1 <img alt=\"\" border=\"0\" class=\"inlineimg\" src=\"http://www.developpez.net/forums/images/smilies/icon_sad.gif\" title=\":(\" /></p> ");
+            ids = selenium.getEval(sourceContent()).split(",");
+            selenium.type("//td[@id='" + ids[0] + "']/textarea", "<h2>Rich Text Example " + i + "</h2><p>BlaBlaBlaaa ... BlaBlaBlaaa ... BlaBlaBlaaa ... BlaBlaBlaaa ... BlaBlaBlaaa ... BlaBlaBlaaa ... BlaBlaBlaaa ... BlaBlaBlaaa ... </p>");
 
-            //wait for save button is ready
+            //click on "Save"
             new Wait("wait") {
                 public boolean until() {
                     return selenium.isTextPresent("Save");
                 }
             };
-            buttonIds = selenium.getEval(saveButton()).split(",");
-            selenium.click("//table[@id='" + buttonIds[31] + "']/tbody/tr[2]/td[2]/em/button");
-
+            ids = selenium.getEval(saveButton()).split(",");
+            selenium.click("//div[@id='" + ids[0] + "']/table/tbody/tr/td[1]/table/tbody/tr/td[1]/table");
         }
     }
 
@@ -173,30 +167,14 @@ public class RichtextTest extends SeleneseTestCase {
         return script;
     }
 
-    public String LabelComponent() {
+    public String saveButton() {
         String script = "var buttonId = new Array();";
         script += "var cnt = 0;";
         script += "var buttons = new Array();";
         script += "buttons = window.document.getElementsByTagName('div');";
         script += "for(var i=0; i<buttons.length; i++) {";
         script += "if(buttons[i].id !=null" +
-                "&& buttons[i].getAttribute('class') == 'x-tree3-node') {";
-        script += "buttonId[cnt]=buttons[i].id;" +
-                "cnt ++;" +
-                "}" +
-                "}";
-        script += "buttonId.toString();";
-        return script;
-    }
-
-    public String saveButton() {
-        String script = "var buttonId = new Array();";
-        script += "var cnt = 0;";
-        script += "var buttons = new Array();";
-        script += "buttons = window.document.getElementsByTagName('table');";
-        script += "for(var i=0; i<buttons.length; i++) {";
-        script += "if(buttons[i].id !=null" +
-                ") {";
+                "&& buttons[i].getAttribute('class') == ' x-small-editor x-panel-btns-center x-panel-fbar x-component x-toolbar-layout-ct') {";
         script += "buttonId[cnt]=buttons[i].id;" +
                 "cnt ++;" +
                 "}" +
@@ -252,5 +230,4 @@ public class RichtextTest extends SeleneseTestCase {
         script += "buttonId.toString();";
         return script;
     }
-
 }

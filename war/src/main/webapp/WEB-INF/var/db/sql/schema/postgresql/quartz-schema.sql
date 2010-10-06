b1adfc511fc05c44042cec17ba0c1e43c1ad609f
Sergiy Shyrkov <sshyrkov@jahia.com>
2010-10-06 14:30:33 +0000

    Use proper driver delegate class for Quartz scheduler Fixes http://jira.jahia.org/browse/QA-1209 and http://jira.jahia.org/browse/JAHIA-4931
    Update Quartz DDL (use the latest from 1.8.3 Quartz distribution)
    
    Minor upgrade of PostgreSQL driver
    
    git-svn-id: svn://localhost/jahia/trunk@8981 69c369ed-ed09-0410-a4a1-e19a46c23626

--- war/src/main/webapp/WEB-INF/var/db/sql/schema/postgresql/quartz-schema.sql
+++ war/src/main/webapp/WEB-INF/var/db/sql/schema/postgresql/quartz-schema.sql
@@ -1,27 +1,27 @@
 -- Thanks to Patrick Lightbody for submitting this...
 --
--- In your Quartz properties file, you'll need to set
+-- In your Quartz properties file, you'll need to set 
 -- org.quartz.jobStore.driverDelegateClass = org.quartz.impl.jdbcjobstore.PostgreSQLDelegate
 
-drop TABLE jahia_qrtz_job_listeners;
-drop TABLE jahia_qrtz_trigger_listeners;
-drop TABLE jahia_qrtz_fired_triggers;
-DROP TABLE jahia_qrtz_PAUSED_TRIGGER_GRPS;
-DROP TABLE jahia_qrtz_SCHEDULER_STATE;
-DROP TABLE jahia_qrtz_LOCKS;
-drop TABLE jahia_qrtz_simple_triggers;
-drop TABLE jahia_qrtz_cron_triggers;
-DROP TABLE jahia_qrtz_BLOB_TRIGGERS;
-drop TABLE jahia_qrtz_triggers;
-drop TABLE jahia_qrtz_job_details;
-drop TABLE jahia_qrtz_calendars;
+drop table jahia_qrtz_job_listeners;
+drop table jahia_qrtz_trigger_listeners;
+drop table jahia_qrtz_fired_triggers;
+drop table jahia_qrtz_paused_trigger_grps;
+drop table jahia_qrtz_scheduler_state;
+drop table jahia_qrtz_locks;
+drop table jahia_qrtz_simple_triggers;
+drop table jahia_qrtz_cron_triggers;
+drop table jahia_qrtz_blob_triggers;
+drop table jahia_qrtz_triggers;
+drop table jahia_qrtz_job_details;
+drop table jahia_qrtz_calendars;
 
 CREATE TABLE jahia_qrtz_job_details
   (
-    JOB_NAME  VARCHAR(80) NOT NULL,
-    JOB_GROUP VARCHAR(80) NOT NULL,
-    DESCRIPTION VARCHAR(120) NULL,
-    JOB_CLASS_NAME   VARCHAR(128) NOT NULL,
+    JOB_NAME  VARCHAR(200) NOT NULL,
+    JOB_GROUP VARCHAR(200) NOT NULL,
+    DESCRIPTION VARCHAR(250) NULL,
+    JOB_CLASS_NAME   VARCHAR(250) NOT NULL, 
     IS_DURABLE BOOL NOT NULL,
     IS_VOLATILE BOOL NOT NULL,
     IS_STATEFUL BOOL NOT NULL,
@@ -32,22 +32,22 @@ CREATE TABLE jahia_qrtz_job_details
 
 CREATE TABLE jahia_qrtz_job_listeners
   (
-    JOB_NAME  VARCHAR(80) NOT NULL,
-    JOB_GROUP VARCHAR(80) NOT NULL,
-    JOB_LISTENER VARCHAR(80) NOT NULL,
+    JOB_NAME  VARCHAR(200) NOT NULL, 
+    JOB_GROUP VARCHAR(200) NOT NULL,
+    JOB_LISTENER VARCHAR(200) NOT NULL,
     PRIMARY KEY (JOB_NAME,JOB_GROUP,JOB_LISTENER),
-    FOREIGN KEY (JOB_NAME,JOB_GROUP)
-	REFERENCES JAHIA_QRTZ_JOB_DETAILS(JOB_NAME,JOB_GROUP)
+    FOREIGN KEY (JOB_NAME,JOB_GROUP) 
+	REFERENCES JAHIA_QRTZ_JOB_DETAILS(JOB_NAME,JOB_GROUP) 
 );
 
 CREATE TABLE jahia_qrtz_triggers
   (
-    TRIGGER_NAME VARCHAR(80) NOT NULL,
-    TRIGGER_GROUP VARCHAR(80) NOT NULL,
-    JOB_NAME  VARCHAR(80) NOT NULL,
-    JOB_GROUP VARCHAR(80) NOT NULL,
+    TRIGGER_NAME VARCHAR(200) NOT NULL,
+    TRIGGER_GROUP VARCHAR(200) NOT NULL,
+    JOB_NAME  VARCHAR(200) NOT NULL, 
+    JOB_GROUP VARCHAR(200) NOT NULL,
     IS_VOLATILE BOOL NOT NULL,
-    DESCRIPTION VARCHAR(120) NULL,
+    DESCRIPTION VARCHAR(250) NULL,
     NEXT_FIRE_TIME BIGINT NULL,
     PREV_FIRE_TIME BIGINT NULL,
     PRIORITY INTEGER NULL,
@@ -55,61 +55,61 @@ CREATE TABLE jahia_qrtz_triggers
     TRIGGER_TYPE VARCHAR(8) NOT NULL,
     START_TIME BIGINT NOT NULL,
     END_TIME BIGINT NULL,
-    CALENDAR_NAME VARCHAR(80) NULL,
+    CALENDAR_NAME VARCHAR(200) NULL,
     MISFIRE_INSTR SMALLINT NULL,
     JOB_DATA BYTEA NULL,
     PRIMARY KEY (TRIGGER_NAME,TRIGGER_GROUP),
-    FOREIGN KEY (JOB_NAME,JOB_GROUP)
-	REFERENCES JAHIA_QRTZ_JOB_DETAILS(JOB_NAME,JOB_GROUP)
+    FOREIGN KEY (JOB_NAME,JOB_GROUP) 
+	REFERENCES JAHIA_QRTZ_JOB_DETAILS(JOB_NAME,JOB_GROUP) 
 );
 
 CREATE TABLE jahia_qrtz_simple_triggers
   (
-    TRIGGER_NAME VARCHAR(80) NOT NULL,
-    TRIGGER_GROUP VARCHAR(80) NOT NULL,
+    TRIGGER_NAME VARCHAR(200) NOT NULL,
+    TRIGGER_GROUP VARCHAR(200) NOT NULL,
     REPEAT_COUNT BIGINT NOT NULL,
     REPEAT_INTERVAL BIGINT NOT NULL,
     TIMES_TRIGGERED BIGINT NOT NULL,
     PRIMARY KEY (TRIGGER_NAME,TRIGGER_GROUP),
-    FOREIGN KEY (TRIGGER_NAME,TRIGGER_GROUP)
+    FOREIGN KEY (TRIGGER_NAME,TRIGGER_GROUP) 
 	REFERENCES JAHIA_QRTZ_TRIGGERS(TRIGGER_NAME,TRIGGER_GROUP)
 );
 
 CREATE TABLE jahia_qrtz_cron_triggers
   (
-    TRIGGER_NAME VARCHAR(80) NOT NULL,
-    TRIGGER_GROUP VARCHAR(80) NOT NULL,
-    CRON_EXPRESSION VARCHAR(80) NOT NULL,
+    TRIGGER_NAME VARCHAR(200) NOT NULL,
+    TRIGGER_GROUP VARCHAR(200) NOT NULL,
+    CRON_EXPRESSION VARCHAR(120) NOT NULL,
     TIME_ZONE_ID VARCHAR(80),
     PRIMARY KEY (TRIGGER_NAME,TRIGGER_GROUP),
-    FOREIGN KEY (TRIGGER_NAME,TRIGGER_GROUP)
+    FOREIGN KEY (TRIGGER_NAME,TRIGGER_GROUP) 
 	REFERENCES JAHIA_QRTZ_TRIGGERS(TRIGGER_NAME,TRIGGER_GROUP)
 );
 
 CREATE TABLE jahia_qrtz_blob_triggers
   (
-    TRIGGER_NAME VARCHAR(80) NOT NULL,
-    TRIGGER_GROUP VARCHAR(80) NOT NULL,
+    TRIGGER_NAME VARCHAR(200) NOT NULL,
+    TRIGGER_GROUP VARCHAR(200) NOT NULL,
     BLOB_DATA BYTEA NULL,
     PRIMARY KEY (TRIGGER_NAME,TRIGGER_GROUP),
-    FOREIGN KEY (TRIGGER_NAME,TRIGGER_GROUP)
+    FOREIGN KEY (TRIGGER_NAME,TRIGGER_GROUP) 
         REFERENCES JAHIA_QRTZ_TRIGGERS(TRIGGER_NAME,TRIGGER_GROUP)
 );
 
 CREATE TABLE jahia_qrtz_trigger_listeners
   (
-    TRIGGER_NAME  VARCHAR(80) NOT NULL,
-    TRIGGER_GROUP VARCHAR(80) NOT NULL,
-    TRIGGER_LISTENER VARCHAR(80) NOT NULL,
+    TRIGGER_NAME  VARCHAR(200) NOT NULL, 
+    TRIGGER_GROUP VARCHAR(200) NOT NULL,
+    TRIGGER_LISTENER VARCHAR(200) NOT NULL,
     PRIMARY KEY (TRIGGER_NAME,TRIGGER_GROUP,TRIGGER_LISTENER),
-    FOREIGN KEY (TRIGGER_NAME,TRIGGER_GROUP)
+    FOREIGN KEY (TRIGGER_NAME,TRIGGER_GROUP) 
 	REFERENCES JAHIA_QRTZ_TRIGGERS(TRIGGER_NAME,TRIGGER_GROUP)
 );
 
 
 CREATE TABLE jahia_qrtz_calendars
   (
-    CALENDAR_NAME  VARCHAR(80) NOT NULL,
+    CALENDAR_NAME  VARCHAR(200) NOT NULL, 
     CALENDAR BYTEA NOT NULL,
     PRIMARY KEY (CALENDAR_NAME)
 );
@@ -117,30 +117,30 @@ CREATE TABLE jahia_qrtz_calendars
 
 CREATE TABLE jahia_qrtz_paused_trigger_grps
   (
-    TRIGGER_GROUP  VARCHAR(80) NOT NULL,
+    TRIGGER_GROUP  VARCHAR(200) NOT NULL, 
     PRIMARY KEY (TRIGGER_GROUP)
 );
 
-CREATE TABLE jahia_qrtz_fired_triggers
+CREATE TABLE jahia_qrtz_fired_triggers 
   (
     ENTRY_ID VARCHAR(95) NOT NULL,
-    TRIGGER_NAME VARCHAR(80) NOT NULL,
-    TRIGGER_GROUP VARCHAR(80) NOT NULL,
+    TRIGGER_NAME VARCHAR(200) NOT NULL,
+    TRIGGER_GROUP VARCHAR(200) NOT NULL,
     IS_VOLATILE BOOL NOT NULL,
-    INSTANCE_NAME VARCHAR(80) NOT NULL,
+    INSTANCE_NAME VARCHAR(200) NOT NULL,
     FIRED_TIME BIGINT NOT NULL,
     PRIORITY INTEGER NOT NULL,
     STATE VARCHAR(16) NOT NULL,
-    JOB_NAME VARCHAR(80) NULL,
-    JOB_GROUP VARCHAR(80) NULL,
+    JOB_NAME VARCHAR(200) NULL,
+    JOB_GROUP VARCHAR(200) NULL,
     IS_STATEFUL BOOL NULL,
     REQUESTS_RECOVERY BOOL NULL,
     PRIMARY KEY (ENTRY_ID)
 );
 
-CREATE TABLE jahia_qrtz_scheduler_state
+CREATE TABLE jahia_qrtz_scheduler_state 
   (
-    INSTANCE_NAME VARCHAR(80) NOT NULL,
+    INSTANCE_NAME VARCHAR(200) NOT NULL,
     LAST_CHECKIN_TIME BIGINT NOT NULL,
     CHECKIN_INTERVAL BIGINT NOT NULL,
     PRIMARY KEY (INSTANCE_NAME)
@@ -148,7 +148,7 @@ CREATE TABLE jahia_qrtz_scheduler_state
 
 CREATE TABLE jahia_qrtz_locks
   (
-    LOCK_NAME  VARCHAR(40) NOT NULL,
+    LOCK_NAME  VARCHAR(40) NOT NULL, 
     PRIMARY KEY (LOCK_NAME)
 );
 
@@ -158,5 +158,20 @@ INSERT INTO jahia_qrtz_locks values('JOB_ACCESS');
 INSERT INTO jahia_qrtz_locks values('CALENDAR_ACCESS');
 INSERT INTO jahia_qrtz_locks values('STATE_ACCESS');
 INSERT INTO jahia_qrtz_locks values('MISFIRE_ACCESS');
+
+create index idx_jahia_qrtz_j_req_recovery on jahia_qrtz_job_details(REQUESTS_RECOVERY);
+create index idx_jahia_qrtz_t_next_fire_time on jahia_qrtz_triggers(NEXT_FIRE_TIME);
+create index idx_jahia_qrtz_t_state on jahia_qrtz_triggers(TRIGGER_STATE);
+create index idx_jahia_qrtz_t_nft_st on jahia_qrtz_triggers(NEXT_FIRE_TIME,TRIGGER_STATE);
+create index idx_jahia_qrtz_t_volatile on jahia_qrtz_triggers(IS_VOLATILE);
+create index idx_jahia_qrtz_ft_trig_name on jahia_qrtz_fired_triggers(TRIGGER_NAME);
+create index idx_jahia_qrtz_ft_trig_group on jahia_qrtz_fired_triggers(TRIGGER_GROUP);
+create index idx_jahia_qrtz_ft_trig_nm_gp on jahia_qrtz_fired_triggers(TRIGGER_NAME,TRIGGER_GROUP);
+create index idx_jahia_qrtz_ft_trig_volatile on jahia_qrtz_fired_triggers(IS_VOLATILE);
+create index idx_jahia_qrtz_ft_trig_inst_name on jahia_qrtz_fired_triggers(INSTANCE_NAME);
+create index idx_jahia_qrtz_ft_job_name on jahia_qrtz_fired_triggers(JOB_NAME);
+create index idx_jahia_qrtz_ft_job_group on jahia_qrtz_fired_triggers(JOB_GROUP);
+create index idx_jahia_qrtz_ft_job_stateful on jahia_qrtz_fired_triggers(IS_STATEFUL);
+create index idx_jahia_qrtz_ft_job_req_recovery on jahia_qrtz_fired_triggers(REQUESTS_RECOVERY);
 
 commit;

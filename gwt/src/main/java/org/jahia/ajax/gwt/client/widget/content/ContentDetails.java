96001fe47f586252e617cf0eced39afdc4ed5bf8
David Griffon <dgriffon@jahia.com>
2011-01-03 12:34:34 +0000

    QA-1686 - fix display and save properties in managers
    git-svn-id: svn://localhost/jahia/trunk@10336 69c369ed-ed09-0410-a4a1-e19a46c23626

--- gwt/src/main/java/org/jahia/ajax/gwt/client/widget/content/ContentDetails.java
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/widget/content/ContentDetails.java
@@ -133,7 +133,7 @@ public class ContentDetails extends BottomRightComponent implements NodeHolder {
      * Creates and initializes all window tabs.
      */
     protected void initTabs() {
-        for (GWTEngineTab tabConfig : ((GWTManagerConfiguration)config).getManagerEngineTabs()) {
+        for (GWTEngineTab tabConfig : ((GWTManagerConfiguration)config).getEngineTabs()) {
             EditEngineTabItem tabItem = tabConfig.getTabItem();
             tabs.add(tabItem.create(tabConfig, this));
         }

de52b2854e282d45ece1319f6fce64938e199e36
Thomas Draier <tdraier@jahia.com>
2011-02-04 13:46:31 +0000

    test for acls ( BACKLOG-1499 )
    git-svn-id: svn://localhost/jahia/trunk@10791 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ test/service/src/main/java/org/jahia/services/acl/AclTest.java
@@ -0,0 +1,204 @@
+package org.jahia.services.acl;
+
+import org.jahia.registries.ServicesRegistry;
+import org.jahia.services.content.*;
+import org.jahia.services.sites.JahiaSite;
+import org.jahia.services.usermanager.JahiaGroup;
+import org.jahia.services.usermanager.JahiaGroupManagerService;
+import org.jahia.services.usermanager.JahiaUser;
+import org.jahia.services.usermanager.JahiaUserManagerService;
+import org.jahia.test.TestHelper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.Set;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class AclTest {
+    private static final transient Logger logger = org.slf4j.LoggerFactory.getLogger(ContentTest.class);
+
+    private final static String TESTSITE_NAME = "aclTestSite";
+
+    private static JahiaUser user1;
+    private static JahiaUser user2;
+    private static JahiaUser user3;
+    private static JahiaUser user4;
+
+    private static JahiaGroup group1;
+    private static JahiaGroup group2;
+    public static final String HOMEPATH = "/sites/"+TESTSITE_NAME+"/home";
+
+    public static JCRPublicationService jcrService;
+
+    private static JCRNodeWrapper home;
+    private static JCRNodeWrapper content1;
+    private static JCRNodeWrapper content11;
+    private static JCRNodeWrapper content12;
+    private static JCRNodeWrapper content2;
+    private static JCRNodeWrapper content21;
+    private static JCRNodeWrapper content22;
+    private static JCRSessionWrapper session;
+
+    public AclTest() {
+    }
+
+    @BeforeClass
+    public static void oneTimeSetUp() throws Exception {
+        JahiaSite site = TestHelper.createSite(TESTSITE_NAME);
+
+        jcrService = ServicesRegistry.getInstance().getJCRPublicationService();
+
+        session = jcrService.getSessionFactory().getCurrentUserSession();
+
+        Set<String> languages = null;
+
+        home = session
+                .getNode(HOMEPATH);
+
+        content1 = home.addNode("content1", "jnt:contentList");
+        content11 = content1.addNode("content1.1", "jnt:contentList");
+        content12 = content1.addNode("content1.2", "jnt:contentList");
+        content2 = home.addNode("content2", "jnt:contentList");
+        content21 = content2.addNode("content2.1", "jnt:contentList");
+        content22 = content2.addNode("content2.2", "jnt:contentList");
+
+        session.save();
+
+        final JahiaUserManagerService userMgr = ServicesRegistry
+                .getInstance().getJahiaUserManagerService();
+
+        JahiaUserManagerService userManager = ServicesRegistry.getInstance().getJahiaUserManagerService();
+        assertNotNull("JahiaUserManagerService cannot be retrieved", userManager);
+
+        user1 = userManager.createUser("user1", "password", new Properties());
+        user2 = userManager.createUser("user2", "password", new Properties());
+        user3 = userManager.createUser("user3", "password", new Properties());
+        user4 = userManager.createUser("user4", "password", new Properties());
+
+        JahiaGroupManagerService groupManager = ServicesRegistry.getInstance().getJahiaGroupManagerService();
+        assertNotNull("JahiaGroupManagerService cannot be retrieved", groupManager);
+
+        group1 = groupManager.createGroup(site.getID(), "group1", new Properties(), false);
+        group2 = groupManager.createGroup(site.getID(), "group2", new Properties(), false);
+
+        group1.addMember(user1);
+        group1.addMember(user2);
+
+        group2.addMember(user3);
+        group2.addMember(user4);
+    }
+
+    @AfterClass
+    public static void oneTimeTearDown() throws Exception {
+        try {
+            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession();
+            if (session.nodeExists("/sites/"+TESTSITE_NAME)) {
+                TestHelper.deleteSite(TESTSITE_NAME);
+            }
+
+            JahiaGroupManagerService groupManager = ServicesRegistry.getInstance().getJahiaGroupManagerService();
+            groupManager.deleteGroup(group1);
+            groupManager.deleteGroup(group2);
+
+            JahiaUserManagerService userManager = ServicesRegistry.getInstance().getJahiaUserManagerService();
+            userManager.deleteUser(user1);
+            userManager.deleteUser(user2);
+            userManager.deleteUser(user3);
+            userManager.deleteUser(user4);
+
+        } catch (Exception ex) {
+            logger.warn("Exception during test tearDown", ex);
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        home.revokeAllRoles();
+        content1.revokeAllRoles();
+        content11.revokeAllRoles();
+        content12.revokeAllRoles();
+        content2.revokeAllRoles();
+        content21.revokeAllRoles();
+        content21.revokeAllRoles();
+        session.save();
+    }
+
+    @Test
+    public void testDefaultReadRight() throws Exception {
+        assertTrue((JCRTemplate.getInstance().doExecuteWithUserSession("user1", null, new CheckPermission(HOMEPATH, "jcr:read"))));
+    }
+
+    @Test
+    public void testGrantUser() throws Exception {
+        content11.grantRoles("u:user1", Collections.singleton("owner"));
+        session.save();
+
+        assertTrue((JCRTemplate.getInstance().doExecuteWithUserSession("user1", null, new CheckPermission(content11.getPath(), "jcr:write"))));
+        assertFalse((JCRTemplate.getInstance().doExecuteWithUserSession("user2", null, new CheckPermission(content11.getPath(), "jcr:write"))));
+    }
+
+    @Test
+    public void testGrantGroup() throws Exception {
+        content11.grantRoles("g:group1", Collections.singleton("owner"));
+        session.save();
+
+        assertTrue((JCRTemplate.getInstance().doExecuteWithUserSession("user1", null, new CheckPermission(content11.getPath(), "jcr:write"))));
+        assertTrue((JCRTemplate.getInstance().doExecuteWithUserSession("user2", null, new CheckPermission(content11.getPath(), "jcr:write"))));
+        assertFalse((JCRTemplate.getInstance().doExecuteWithUserSession("user3", null, new CheckPermission(content11.getPath(), "jcr:write"))));
+        assertFalse((JCRTemplate.getInstance().doExecuteWithUserSession("user4", null, new CheckPermission(content11.getPath(), "jcr:write"))));
+    }
+
+    @Test
+    public void testDenyUser() throws Exception {
+        content1.grantRoles("u:user1", Collections.singleton("owner"));
+        content11.denyRoles("u:user1", Collections.singleton("owner"));
+        session.save();
+
+        assertTrue((JCRTemplate.getInstance().doExecuteWithUserSession("user1", null, new CheckPermission(content1.getPath(), "jcr:write"))));
+        assertFalse((JCRTemplate.getInstance().doExecuteWithUserSession("user1", null, new CheckPermission(content11.getPath(), "jcr:write"))));
+    }
+
+    @Test
+    public void testAclBreak() throws Exception {
+        content1.setAclInheritanceBreak(true);
+
+        content11.grantRoles("u:user1", Collections.singleton("owner"));
+        session.save();
+        assertTrue((JCRTemplate.getInstance().doExecuteWithUserSession("user1", null, new CheckPermission(home.getPath(), "jcr:read"))));
+        assertFalse((JCRTemplate.getInstance().doExecuteWithUserSession("user1", null, new CheckPermission(content1.getPath(), "jcr:read"))));
+        assertTrue((JCRTemplate.getInstance().doExecuteWithUserSession("user1", null, new CheckPermission(content11.getPath(), "jcr:read"))));
+        assertFalse((JCRTemplate.getInstance().doExecuteWithUserSession("user1", null, new CheckPermission(content12.getPath(), "jcr:read"))));
+    }
+
+
+
+    class CheckPermission implements JCRCallback<Boolean> {
+        private String path;
+        private String permission;
+
+        CheckPermission(String path, String permission) {
+            this.path = path;
+            this.permission = permission;
+        }
+
+        public Boolean doInJCR(JCRSessionWrapper session) throws RepositoryException {
+            try {
+                return session.getNode(path).hasPermission(permission);
+            } catch (PathNotFoundException e) {
+                return false;
+            }
+        }
+    }
+
+
+}

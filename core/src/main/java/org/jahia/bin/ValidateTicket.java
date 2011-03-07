244f27ce2f4babbe168108bb7ced2b3e7751f989
Sergiy Shyrkov <sshyrkov@jahia.com>
2011-03-07 17:56:44 +0000

    Do not execute the logic if request parameters are empty
    git-svn-id: svn://localhost/jahia/trunk@11237 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/bin/ValidateTicket.java
+++ core/src/main/java/org/jahia/bin/ValidateTicket.java
@@ -32,6 +32,7 @@
 
 package org.jahia.bin;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.jackrabbit.core.security.JahiaLoginModule;
 
 import javax.servlet.ServletException;
@@ -41,73 +42,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * TODO Comment me
+ * Validates the submitted user token.
  *
  * @author toto
  */
 public class ValidateTicket extends HttpServlet {
-    /**
-     * Called by the server (via the <code>service</code> method) to
-     * allow a servlet to handle a GET request.
-     * <p/>
-     * <p>Overriding this method to support a GET request also
-     * automatically supports an HTTP HEAD request. A HEAD
-     * request is a GET request that returns no body in the
-     * response, only the request header fields.
-     * <p/>
-     * <p>When overriding this method, read the request data,
-     * write the response headers, get the response's writer or
-     * output stream object, and finally, write the response data.
-     * It's best to include content type and encoding. When using
-     * a <code>PrintWriter</code> object to return the response,
-     * set the content type before accessing the
-     * <code>PrintWriter</code> object.
-     * <p/>
-     * <p>The servlet container must write the headers before
-     * committing the response, because in HTTP the headers must be sent
-     * before the response body.
-     * <p/>
-     * <p>Where possible, set the Content-Length header (with the
-     * {@link javax.servlet.ServletResponse#setContentLength} method),
-     * to allow the servlet container to use a persistent connection
-     * to return its response to the client, improving performance.
-     * The content length is automatically set if the entire response fits
-     * inside the response buffer.
-     * <p/>
-     * <p>When using HTTP 1.1 chunked encoding (which means that the response
-     * has a Transfer-Encoding header), do not set the Content-Length header.
-     * <p/>
-     * <p>The GET method should be safe, that is, without
-     * any side effects for which users are held responsible.
-     * For example, most form queries have no side effects.
-     * If a client request is intended to change stored data,
-     * the request should use some other HTTP method.
-     * <p/>
-     * <p>The GET method should also be idempotent, meaning
-     * that it can be safely repeated. Sometimes making a
-     * method safe also makes it idempotent. For example,
-     * repeating queries is both safe and idempotent, but
-     * buying a product online or modifying data is neither
-     * safe nor idempotent.
-     * <p/>
-     * <p>If the request is incorrectly formatted, <code>doGet</code>
-     * returns an HTTP "Bad Request" message.
-     *
-     * @param req  an {@link javax.servlet.http.HttpServletRequest} object that
-     *             contains the request the client has made of the servlet
-     * @param resp an {@link javax.servlet.http.HttpServletResponse} object that
-     *             contains the response the servlet sends to the client
-     * @throws java.io.IOException            if an input or output error is
-     *                                        detected when the servlet handles the GET request
-     * @throws javax.servlet.ServletException if the request for the GET
-     *                                        could not be handled
-     * @see javax.servlet.ServletResponse#setContentType
-     */
+
+    private static final long serialVersionUID = -7524215255135608239L;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String user = req.getParameter("user");
         String pass = req.getParameter("pass");
-        if (JahiaLoginModule.getToken(user, pass) == null) {
+        if (StringUtils.isNotEmpty(user) && StringUtils.isNotEmpty(pass) && JahiaLoginModule.getToken(user, pass) == null) {
             resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
         } else {
             resp.setStatus(HttpServletResponse.SC_OK);

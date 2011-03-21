be7e03fbbf824bc75a125d4304cf0f010c377c54
Serge Huber <shuber@jahia.com>
2011-03-21 15:54:55 +0000

    Implement action to add principals to roles
    git-svn-id: svn://localhost/jahia/trunk@11558 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/default/src/main/java/org/jahia/modules/defaultmodule/actions/AddPrincipalsInRolesAction.java
+++ modules/default/src/main/java/org/jahia/modules/defaultmodule/actions/AddPrincipalsInRolesAction.java
@@ -6,8 +6,15 @@ import org.jahia.services.content.JCRSessionWrapper;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.URLResolver;
+import org.jahia.services.sites.JahiaSitesService;
+import org.jahia.services.usermanager.JahiaGroup;
+import org.jahia.services.usermanager.JahiaGroupManagerService;
+import org.jahia.services.usermanager.JahiaUser;
+import org.jahia.services.usermanager.JahiaUserManagerService;
+import org.slf4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -16,8 +23,52 @@ import java.util.Map;
  * URL. Of course proper permissions must be given to execute this action.
  */
 public class AddPrincipalsInRolesAction extends Action {
+
+    private transient static Logger logger = org.slf4j.LoggerFactory.getLogger(AddPrincipalsInRolesAction.class);
+
+    private JahiaGroupManagerService jahiaGroupManagerService;
+    private JahiaUserManagerService jahiaUserManagerService;
+    private JahiaSitesService jahiaSitesService;
+
+    public void setJahiaGroupManagerService(JahiaGroupManagerService jahiaGroupManagerService) {
+        this.jahiaGroupManagerService = jahiaGroupManagerService;
+    }
+
+    public void setJahiaUserManagerService(JahiaUserManagerService jahiaUserManagerService) {
+        this.jahiaUserManagerService = jahiaUserManagerService;
+    }
+
     @Override
     public ActionResult doExecute(HttpServletRequest req, RenderContext renderContext, Resource resource, JCRSessionWrapper session, Map<String, List<String>> parameters, URLResolver urlResolver) throws Exception {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+
+        if ((parameters.get("principals") != null) &&
+            (parameters.get("roles") != null)) {
+            List<String> principals = parameters.get("principals");
+            List<String> roles = parameters.get("roles");
+            for (String principalKey : principals) {
+                if (principalKey.startsWith("u:")) {
+                    String userKey = principalKey.substring("u:".length());
+                    JahiaUser jahiaUser = jahiaUserManagerService.lookupUserByKey(userKey);
+                    if (jahiaUser == null) {
+                        logger.warn("User " + userKey + " could not be found, will not add to roles");
+                        return ActionResult.BAD_REQUEST;
+                    }
+                    resource.getNode().grantRoles(principalKey, new HashSet(roles));
+                    session.save();
+                } else if (principalKey.startsWith("g:")) {
+                    String groupKey = principalKey.substring("g:".length());
+                    JahiaGroup jahiaGroup = jahiaGroupManagerService.lookupGroup(groupKey);
+                    if (jahiaGroup == null) {
+                        logger.warn("Group " + groupKey + " could not be found, will not add to roles");
+                        return ActionResult.BAD_REQUEST;
+                    }
+                    resource.getNode().grantRoles(principalKey, new HashSet(roles));
+                    session.save();
+                }
+            }
+        } else {
+            return ActionResult.BAD_REQUEST;
+        }
+        return ActionResult.OK_JSON;
     }
 }

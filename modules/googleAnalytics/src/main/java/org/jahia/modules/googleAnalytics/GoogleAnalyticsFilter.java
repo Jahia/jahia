a5a880fb00f85869476242c367824eaf49bd4b65
Sergiy Shyrkov <sshyrkov@jahia.com>
2011-03-23 09:00:58 +0000

    Google Analytics small optimization: read content of the script once and use servlet context for reading it. Do not create OutputDocument if not modifications will be done. Move script to WEB-INF/ folder
    git-svn-id: svn://localhost/jahia/trunk@11612 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/googleAnalytics/src/main/java/org/jahia/modules/googleAnalytics/GoogleAnalyticsFilter.java
+++ modules/googleAnalytics/src/main/java/org/jahia/modules/googleAnalytics/GoogleAnalyticsFilter.java
@@ -1,23 +1,25 @@
 package org.jahia.modules.googleAnalytics;
 
 import net.htmlparser.jericho.*;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
-import org.jahia.bin.listeners.JahiaContextLoaderListener;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.filter.AbstractFilter;
 import org.jahia.services.render.filter.RenderChain;
 import org.jahia.services.render.filter.cache.AggregateCacheFilter;
+import org.jahia.services.templates.JahiaTemplateManagerService.TemplatePackageRedeployedEvent;
 import org.jahia.utils.ScriptEngineUtils;
+import org.jahia.utils.WebUtils;
 import org.slf4j.*;
 import org.slf4j.Logger;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 
 import javax.script.Bindings;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.SimpleScriptContext;
-import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
@@ -28,40 +30,39 @@ import java.util.List;
  * Date: 2/25/11
  * Time: 11:28 AM
  */
-public class GoogleAnalyticsFilter extends AbstractFilter{
+public class GoogleAnalyticsFilter extends AbstractFilter implements ApplicationListener<ApplicationEvent> {
 
     private static Logger logger = LoggerFactory.getLogger(GoogleAnalyticsFilter.class);
 
     private ScriptEngineUtils scriptEngineUtils;
+    
     private String template;
+    
+    private String resolvedTemplate;
+    
     @Override
     public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
-        Source source = new Source(previousOut);
-        OutputDocument outputDocument = new OutputDocument(source);
-        if (renderContext.getSite().hasProperty("webPropertyID") && !renderContext.getSite().getProperty("webPropertyID").getString().equals("")) {
-            List<Element> bodyElementList = source.getAllElements(HTMLElementName.BODY);
-            for (Element element : bodyElementList) {
-                final EndTag bodyEndTag = element.getEndTag();
-                String extension = StringUtils.substringAfterLast(template, ".");
-                ScriptEngine scriptEngine = scriptEngineUtils.scriptEngine(extension);
-                ScriptContext scriptContext = new GoogleScriptContext();
-                final Bindings bindings = scriptEngine.createBindings();
-                bindings.put("webPropertyID", renderContext.getSite().getProperty("webPropertyID").getString());
-                bindings.put("resource", resource);
-                bindings.put("gaMap",renderContext.getRequest().getAttribute("gaMap"));
-                scriptContext.setBindings(bindings, ScriptContext.GLOBAL_SCOPE);
-                String resolvedTemplate = null;
-                try {
-                resolvedTemplate = FileUtils.readFileToString(new File(
-
-                            JahiaContextLoaderListener.getServletContext().getRealPath(template)));
-                } catch (java.io.FileNotFoundException e) {
-                    logger.warn("GoogleAnalyticsFilter : Script " + template + "is not found");
-                }
-                if (resolvedTemplate != null) {
+        String out = previousOut;
+        String webPropertyID = renderContext.getSite().hasProperty("webPropertyID") ? renderContext.getSite().getProperty("webPropertyID").getString() : null;
+        if (StringUtils.isNotEmpty(webPropertyID)) {
+            String script = getResolvedTemplate();
+            if (script != null) {
+                Source source = new Source(previousOut);
+                OutputDocument outputDocument = new OutputDocument(source);
+                List<Element> bodyElementList = source.getAllElements(HTMLElementName.BODY);
+                for (Element element : bodyElementList) {
+                    final EndTag bodyEndTag = element.getEndTag();
+                    String extension = StringUtils.substringAfterLast(template, ".");
+                    ScriptEngine scriptEngine = scriptEngineUtils.scriptEngine(extension);
+                    ScriptContext scriptContext = new GoogleScriptContext();
+                    final Bindings bindings = scriptEngine.createBindings();
+                    bindings.put("webPropertyID", webPropertyID);
+                    bindings.put("resource", resource);
+                    bindings.put("gaMap",renderContext.getRequest().getAttribute("gaMap"));
+                    scriptContext.setBindings(bindings, ScriptContext.GLOBAL_SCOPE);
                     // The following binding is necessary for Javascript, which doesn't offer a console by default.
                     bindings.put("out", new PrintWriter(scriptContext.getWriter()));
-                    scriptEngine.eval(resolvedTemplate, scriptContext);
+                    scriptEngine.eval(script, scriptContext);
                     StringWriter writer = (StringWriter) scriptContext.getWriter();
                     final String googleAnalyticsScript = writer.toString();
                     if (StringUtils.isNotBlank(googleAnalyticsScript)) {
@@ -70,17 +71,36 @@ public class GoogleAnalyticsFilter extends AbstractFilter{
                     }
                     break; // avoid to loop if for any reasons multiple body in the page
                 }
+                out = outputDocument.toString().trim();
             }
         }
-        return outputDocument.toString().trim();
+        
+        return out;
     }
+    
+    protected String getResolvedTemplate() throws IOException {
+        if (resolvedTemplate == null) {
+            resolvedTemplate = WebUtils.getResourceAsString(template);
+            if (resolvedTemplate == null) {
+                logger.warn("Unable to lookup template at {}", template);
+            }
+        }
+        return resolvedTemplate;
+    }
+    
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof TemplatePackageRedeployedEvent) {
+            resolvedTemplate = null;
+        }
+    }
+    
     public void setScriptEngineUtils(ScriptEngineUtils scriptEngineUtils) {
         this.scriptEngineUtils = scriptEngineUtils;
     }
     public void setTemplate(String template) {
         this.template = template;
     }
-
+    
     class GoogleScriptContext extends SimpleScriptContext {
         private Writer writer = null;
 

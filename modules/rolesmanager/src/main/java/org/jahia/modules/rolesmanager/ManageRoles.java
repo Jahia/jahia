1e2dde3513e9d1759c299790e89e8bc2d89a3f67
Sergiy Shyrkov <sshyrkov@jahia.com>
2011-06-01 15:31:51 +0000

    QA-2615: License headers http://jira.jahia.org/browse/QA-2615
    Updated license header
    
    git-svn-id: svn://localhost/jahia/trunk@23927 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/rolesmanager/src/main/java/org/jahia/modules/rolesmanager/ManageRoles.java
+++ modules/rolesmanager/src/main/java/org/jahia/modules/rolesmanager/ManageRoles.java
@@ -1,19 +1,21 @@
 /**
- * Enterprise Jahia xCM v6.5
+ * This file is part of the Enterprise Jahia software.
+ *
+ * Jahia's next-generation, open source CMS stems from a widely acknowledged vision
+ * of enterprise application convergence - web, search, document, social and portal -
+ * unified by the simplicity of web content management.
+ *
+ * For more information, please visit http://www.jahia.com.
  *
  * Copyright (C) 2002-2011 Jahia Solutions Group SA. All rights reserved.
  *
- * Jahia's next-generation, open source CMS stems from a widely acknowledged vision of enterprise application
- * convergence - web, search, document, social and portal - unified by the simplicity of web content management.
- * Structured around Jahia Extended Content Management (xCM), the Jahia Studio, Jahiapps social and collaboration
- * capabilities, and based on the powerful Jahia Composite Content Management platform, Jahia brings a unique modularity
- * to the Java development arena. It is the tool of choice for building innovative user experiences for virtually any type
- * of web application. For more information, please visit http://www.jahia.com.
- *
- * This Enterprise Jahia instance must be used in accordance with the terms contained in the
- * Jahia Solutions Group Terms & Conditions as well as the Jahia Sustainable Enterprise License (JSEL).
- *
- * For questions regarding licensing, support, production usage... please contact our team at sales@jahia.com.
+ * This Enteprise Jahia software must be used in accordance with the terms contained in the
+ * Jahia Solutions Group Terms & Conditions as well as the
+ * Jahia Sustainable Enterprise License (JSEL). You may not use this software except
+ * in compliance with the Jahia Solutions Group Terms & Conditions and the JSEL.
+ * See the license for the rights, obligations and limitations governing use
+ * of the contents of the software. For questions regarding licensing, support, production usage,
+ * please contact our team at sales@jahia.com or go to: http://www.jahia.com/license
  */
 
 package org.jahia.modules.rolesmanager;

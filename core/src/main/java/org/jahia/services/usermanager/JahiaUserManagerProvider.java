2d391093aeae238064abfcc0101ab3cf7995c15e
Thomas Draier <tdraier@jahia.com>
2011-06-01 16:11:16 +0000

    update license headers
    git-svn-id: svn://localhost/jahia/trunk@12943 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/usermanager/JahiaUserManagerProvider.java
+++ core/src/main/java/org/jahia/services/usermanager/JahiaUserManagerProvider.java
@@ -1,5 +1,11 @@
 /**
- * This file is part of Jahia: An integrated WCM, DMS and Portal Solution
+ * This file is part of Jahia, next-generation open source CMS:
+ * Jahia's next-generation, open source CMS stems from a widely acknowledged vision
+ * of enterprise application convergence - web, search, document, social and portal -
+ * unified by the simplicity of web content management.
+ *
+ * For more information, please visit http://www.jahia.com.
+ *
  * Copyright (C) 2002-2011 Jahia Solutions Group SA. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
@@ -23,11 +29,13 @@
  * describing the FLOSS exception, and it is also available here:
  * http://www.jahia.com/license
  *
- * Commercial and Supported Versions of the program
- * Alternatively, commercial and supported versions of the program may be used
- * in accordance with the terms contained in a separate written agreement
- * between you and Jahia Solutions Group SA. If you are unsure which license is appropriate
- * for your use, please contact the sales department at sales@jahia.com.
+ * Commercial and Supported Versions of the program (dual licensing):
+ * alternatively, commercial and supported versions of the program may be used
+ * in accordance with the terms and conditions contained in a separate
+ * written agreement between you and Jahia Solutions Group SA.
+ *
+ * If you are unsure which license is appropriate for your use,
+ * please contact the sales department at sales@jahia.com.
  */
 
 package org.jahia.services.usermanager;

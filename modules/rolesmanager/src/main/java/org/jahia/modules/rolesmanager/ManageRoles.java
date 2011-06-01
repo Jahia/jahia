c9a9fad07b47a6e3d3ea9fe8c5c75d2c16940538
Serge Huber <shuber@jahia.com>
2011-06-01 13:23:58 +0000

    working on license headers
    git-svn-id: svn://localhost/jahia/trunk@23926 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/rolesmanager/src/main/java/org/jahia/modules/rolesmanager/ManageRoles.java
+++ modules/rolesmanager/src/main/java/org/jahia/modules/rolesmanager/ManageRoles.java
@@ -1,34 +1,21 @@
 /**
- * This file is part of Jahia: An integrated WCM, DMS and Portal Solution
+ * Enterprise Jahia xCM v6.5
+ *
  * Copyright (C) 2002-2011 Jahia Solutions Group SA. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * Jahia's next-generation, open source CMS stems from a widely acknowledged vision of enterprise application
+ * convergence - web, search, document, social and portal - unified by the simplicity of web content management.
+ * Structured around Jahia Extended Content Management (xCM), the Jahia Studio, Jahiapps social and collaboration
+ * capabilities, and based on the powerful Jahia Composite Content Management platform, Jahia brings a unique modularity
+ * to the Java development arena. It is the tool of choice for building innovative user experiences for virtually any type
+ * of web application. For more information, please visit http://www.jahia.com.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This Enterprise Jahia instance must be used in accordance with the terms contained in the
+ * Jahia Solutions Group Terms & Conditions as well as the Jahia Sustainable Enterprise License (JSEL).
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * As a special exception to the terms and conditions of version 2.0 of
- * the GPL (or any later version), you may redistribute this Program in connection
- * with Free/Libre and Open Source Software ("FLOSS") applications as described
- * in Jahia's FLOSS exception. You should have received a copy of the text
- * describing the FLOSS exception, and it is also available here:
- * http://www.jahia.com/license
- *
- * Commercial and Supported Versions of the program
- * Alternatively, commercial and supported versions of the program may be used
- * in accordance with the terms contained in a separate written agreement
- * between you and Jahia Solutions Group SA. If you are unsure which license is appropriate
- * for your use, please contact the sales department at sales@jahia.com.
+ * For questions regarding licensing, support, production usage... please contact our team at sales@jahia.com.
  */
+
 package org.jahia.modules.rolesmanager;
 
 import org.jahia.admin.AbstractAdministrationModule;

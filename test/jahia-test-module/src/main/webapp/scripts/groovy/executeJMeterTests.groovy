f631930367bfee936609a29333f94390dcf87365
Thomas Draier <tdraier@jahia.com>
2011-06-16 14:35:05 +0000

    fixed jmeter scripts execution
    git-svn-id: svn://localhost/jahia/trunk@13095 69c369ed-ed09-0410-a4a1-e19a46c23626

--- test/jahia-test-module/src/main/webapp/scripts/groovy/executeJMeterTests.groovy
+++ test/jahia-test-module/src/main/webapp/scripts/groovy/executeJMeterTests.groovy
@@ -3,31 +3,34 @@ def test = project.properties['test']
 def p = ~/.*\.jmx/
 
 new File(project.properties['testsPath']).eachDir {
-  d ->
-  if ((test != null && d.getName().equals(test)) || test == null) {
-    new File(d.getAbsolutePath()).eachFileMatch(p) {
-      f ->
-      final def file = new File(d.getAbsolutePath() + "/test.properties");
-      def jmeterExe;
-      def jmeterPath;
-      //if not windows
-      jmeterPath = project.properties['path'];
-      if (jmeterPath.toString().count("/") != 0) {
-        jmeterPath = project.properties['path'] + "/bin/jmeter.sh -n -t "
-      } else {
-        // if windows
-        jmeterPath = project.properties['path'] + "\\bin\\jmeter.bat -n -t "
-      }
-      if (file.exists()) jmeterExe = jmeterPath + f + " " + evaluate(file) else
-        jmeterExe = jmeterPath + f
-      println "Executing test : " + jmeterExe
-      def proc = jmeterExe.execute()
-      proc.waitFor()
-      // Obtain status and output
-      println "return code: ${ proc.exitValue()}"
-      println "stderr: ${proc.err.text}"
-      println "stdout: ${proc.in.text}"
-      println "End of test : " + jmeterExe
+    moduleDir ->
+    new File(moduleDir,"scripts/jmeter").eachDir {
+        d ->
+        if ((test != null && d.getName().equals(test)) || test == null) {
+            new File(d.getAbsolutePath()).eachFileMatch(p) {
+                f ->
+                final def file = new File(d.getAbsolutePath() + "/test.properties");
+                def jmeterExe;
+                def jmeterPath;
+                //if not windows
+                jmeterPath = project.properties['path'];
+                if (jmeterPath.toString().count("/") != 0) {
+                    jmeterPath = project.properties['path'] + "/bin/jmeter.sh -n -t "
+                } else {
+                    // if windows
+                    jmeterPath = project.properties['path'] + "\\bin\\jmeter.bat -n -t "
+                }
+                if (file.exists()) jmeterExe = jmeterPath + f + " " + evaluate(file) else
+                    jmeterExe = jmeterPath + f
+                println "Executing test : " + jmeterExe
+                def proc = jmeterExe.execute()
+                proc.waitFor()
+                // Obtain status and output
+                println "return code: ${ proc.exitValue()}"
+                println "stderr: ${proc.err.text}"
+                println "stdout: ${proc.in.text}"
+                println "End of test : " + jmeterExe
+            }
+        }
     }
-  }
 }

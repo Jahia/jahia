4bb543cfd859a0fb1b0c043f9f16bdca2e1e33e8
Thomas Draier <tdraier@jahia.com>
2011-06-21 13:35:54 +0000

    moved static assets handling away from cache and rendercontext, habdle everything in the assets filter to get the same behavior everytime ( QA-2670 )
    git-svn-id: svn://localhost/jahia/trunk@13142 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/assets/src/main/webapp/WEB-INF/scripts/ajaxResources.groovy
+++ modules/assets/src/main/webapp/WEB-INF/scripts/ajaxResources.groovy
@@ -2,9 +2,9 @@
  * @author rincevent
  * @since JAHIA 6.5
  */
-renderContext.staticAssets.css.eachWithIndex { resource, i ->
+renderContext.request.getAttribute("staticAssets").css.eachWithIndex { resource, i ->
     println "<link id=\"staticAssetCSS${i}\" rel=\"stylesheet\" href=\"${resource}\" media=\"screen\" type=\"text/css\"/>";
 }
-renderContext.staticAssets.inline.eachWithIndex { resource, i ->
+renderContext.request.getAttribute("staticAssets").inline.eachWithIndex { resource, i ->
     println "${resource}";
 }

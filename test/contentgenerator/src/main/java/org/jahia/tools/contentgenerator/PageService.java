134adc1b16ad03aa4fa0397633b1b15730c0a5a9
Guillaume Lucazeau <glucazeau@jahia.com>
2011-06-27 15:49:45 +0000

    Remove useless import that cause compilation errors
    git-svn-id: svn://localhost/jahia/trunk@13196 69c369ed-ed09-0410-a4a1-e19a46c23626

--- test/contentgenerator/src/main/java/org/jahia/tools/contentgenerator/PageService.java
+++ test/contentgenerator/src/main/java/org/jahia/tools/contentgenerator/PageService.java
@@ -1,10 +1,14 @@
 package org.jahia.tools.contentgenerator;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
-import com.sun.tools.internal.xjc.generator.bean.MethodWriter;
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.jahia.tools.contentgenerator.bo.ArticleBO;
 import org.jahia.tools.contentgenerator.bo.ExportBO;

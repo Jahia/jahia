93e465dbe535582c10b6c68016fe39f45c5c2585
Cédric Mailleux <cmailleux@jahia.com>
2011-08-03 09:40:06 +0000

    Fix BACKLOG-1805 Issue when playing with active languages in live mode as the site can not be resolved due to the checkValidity method in JCRNWI
    git-svn-id: svn://localhost/jahia/trunk@13487 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/bin/Render.java
+++ core/src/main/java/org/jahia/bin/Render.java
@@ -738,12 +738,12 @@ public class Render extends HttpServlet implements Controller, ServletConfigAwar
                     renderContext.setMainResource(resource);
 
                     JCRSiteNode site = resource.getNode().getResolveSite();
-                    if (site != null
+                    if ((site == null && resource.getNode().getPath().startsWith("/sites/")) || (site != null
                             && (renderContext.getEditModeConfigName() == null
                             || !renderContext.getEditModeConfigName().equals(Studio.STUDIO_MODE))
                             && !(renderContext.isLiveMode() ? site.getActiveLanguagesAsLocales()
                                     .contains(urlResolver.getLocale()) : site
-                                    .getLanguagesAsLocales().contains(urlResolver.getLocale()))) {
+                                    .getLanguagesAsLocales().contains(urlResolver.getLocale())))) {
                         throw new PathNotFoundException("This language does not exist on this site");
                     }
                     renderContext.setSite(site);

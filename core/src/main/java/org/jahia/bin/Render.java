6761a03c5a4282379df24aa32ba9c8eb8a47e42d
Sergiy Shyrkov <sshyrkov@jahia.com>
2011-09-09 09:40:31 +0000

    Use constant for jcrCaptcha
    git-svn-id: svn://localhost/jahia/trunk@13786 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/bin/Render.java
+++ core/src/main/java/org/jahia/bin/Render.java
@@ -893,7 +893,7 @@ public class Render extends HttpServlet implements Controller, ServletConfigAwar
                     List<String> stringList = entry.getValue();
                     List<String> parameterValues = parameters.get(entry.getKey());
                     if (!CollectionUtils.isEqualCollection(stringList, parameterValues)) {
-                        if (entry.getKey().equals("jcrCaptcha")) {
+                        if (entry.getKey().equals(CAPTCHA)) {
                             Map<String, String[]> formDatas = new HashMap<String, String[]>();
                             Set<Map.Entry<String, List<String>>> set = parameters.entrySet();
                             for (Map.Entry<String, List<String>> params : set) {

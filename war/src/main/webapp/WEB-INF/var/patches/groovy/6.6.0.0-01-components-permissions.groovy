4293eba3473de793e9bc8bee529d055677a75bc8
Sergiy Shyrkov <sshyrkov@jahia.com>
2011-10-13 10:33:47 +0000

    BACKLOG-1964 - Migration from Jahia 6.5 http://jira.jahia.org/browse/BACKLOG-1964
    Added migration script (Groovy) for granting new /permissions/editMode/useComponent permission to the privileged role
    
    git-svn-id: svn://localhost/jahia/trunk@14143 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ war/src/main/webapp/WEB-INF/var/patches/groovy/6.6.0.0-01-components-permissions.groovy
@@ -0,0 +1,22 @@
+import org.jahia.services.content.*
+
+import javax.jcr.*
+
+def sysout = out;
+
+sysout << "Start granting component usage permission to privileged role\n"
+
+JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Object>() {
+	public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
+		if (RBACUtils.grantPermissionToRole("/permissions/editMode/useComponent", "privileged", session)) {
+			session.save();
+			sysout << "Permission granted\n"
+		} else {
+			sysout << "Role already has the permission\n"
+		}
+				
+		return null;
+    }
+});
+
+sysout << "... done granting component usage permission to privileged role.\n"

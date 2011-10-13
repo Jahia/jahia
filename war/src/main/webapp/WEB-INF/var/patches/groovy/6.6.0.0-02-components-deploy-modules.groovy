e19c3a6f65c9abe76c79fcdf2897e9c670a9a7ee
Sergiy Shyrkov <sshyrkov@jahia.com>
2011-10-13 13:11:21 +0000

    BACKLOG-1964 - Migration from Jahia 6.5 http://jira.jahia.org/browse/BACKLOG-1964
    Added migration script (Groovy) for checking the "used" components of the sites and deploying corresponding modules
    
    git-svn-id: svn://localhost/jahia/trunk@14146 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ war/src/main/webapp/WEB-INF/var/patches/groovy/6.6.0.0-02-components-deploy-modules.groovy
@@ -0,0 +1,112 @@
+import java.util.*
+
+import javax.jcr.*
+import javax.jcr.query.*
+
+import org.jahia.services.content.*
+import org.jahia.registries.ServicesRegistry
+import org.jahia.services.templates.JahiaTemplateManagerService
+
+def sysout = out;
+
+sysout << "Start analyzing sites and deploying modules\n"
+
+        JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Object>() {
+            public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
+            	JahiaTemplateManagerService templateService = ServicesRegistry.getInstance().getJahiaTemplateManagerService();
+                for (NodeIterator siteIterator = session
+                        .getWorkspace()
+                        .getQueryManager()
+                        .createQuery("select * from [jnt:virtualsite] where ischildnode('/sites')",
+                                Query.JCR_SQL2).execute().getNodes(); siteIterator.hasNext();) {
+                    JCRNodeWrapper site = (JCRNodeWrapper) siteIterator.nextNode();
+                    if ("systemsite".equals(site.getName())) {
+                        // skip system site
+                        continue;
+                    }
+                    
+                    if (site.hasNode("components")) {
+                        sysout << "\n  Components are already deployed to site " + site.getName() + "\n"
+                        continue;
+                    }
+                    
+                    sysout << "\nProcessing site " + site.getName() + "\n";
+                    Set<String> alreadyDeployed = new TreeSet<String>();
+                    if (site.hasProperty("j:installedModules")) {
+                    	for (Value val : site.getProperty("j:installedModules").getValues()) {
+                    		alreadyDeployed.add(val.getString());
+                    	}
+                    }
+                    sysout << "  Modules, already deployed on site " + site.getName() + ": " + alreadyDeployed + "\n";
+                    
+                    Set<String> toDeploy = new LinkedHashSet<String>();
+                    if (!alreadyDeployed.contains("default")) {
+                    	toDeploy.add("default");
+                    }
+                    
+                    for (NodeIterator moduleIterator  = session
+                            .getWorkspace()
+                            .getQueryManager()
+                            .createQuery("select * from [jnt:virtualsite] where ischildnode('/templateSets')"+" and [j:siteType] = 'module' order by localname()",
+                                    Query.JCR_SQL2).execute().getNodes(); moduleIterator.hasNext();) {
+                        JCRNodeWrapper module = (JCRNodeWrapper) moduleIterator.nextNode();
+                        if (alreadyDeployed.contains(module.getName())) {
+                            sysout << "  Module " + module.getName() +" already deployed on -> skipping\n";
+                            continue;
+                        }
+                        if (!module.hasNode("components")) {
+                            sysout << "  Module " + module.getName() +" does not provide any components -> skipping\n";
+                            continue;
+                        }
+                        sysout << "  Checking components of module " + module.getName() + "\n";
+                        for (NodeIterator coponentIterator  = session
+                                .getWorkspace()
+                                .getQueryManager()
+                                .createQuery("select * from [jnt:component] where isdescendantnode('/templateSets/" + module.getName() + "/components')",
+                                        Query.JCR_SQL2).execute().getNodes(); coponentIterator.hasNext();) {
+                            JCRNodeWrapper component = (JCRNodeWrapper) coponentIterator.nextNode();
+                            if (component.isNodeType("jmix:studioOnly")) {
+                            	// skip studio only component
+                            	continue;
+                            }
+                            //sysout << "    Checking component " + component.getName() + " for site " + site.getName() + "\n";
+                            if (session
+                            .getWorkspace()
+                            .getQueryManager()
+                            .createQuery("select * from ["+component.getName()+"] where isdescendantnode('/sites/"+site.getName()+"/home')",
+                                    Query.JCR_SQL2).execute().getNodes().hasNext()
+                                || session
+                            .getWorkspace()
+                            .getQueryManager()
+                            .createQuery("select * from ["+component.getName()+"] where isdescendantnode('/sites/"+site.getName()+"/contents')",
+                                    Query.JCR_SQL2).execute().getNodes().hasNext()) {
+                                //sysout << "    Site " + site.getName() + " contains component " + component.getName() + ". Module " + module.getName() + " will be deployed\n";
+                                toDeploy.add(module.getName());
+                                break;
+                            }
+                        }
+                        if (toDeploy.contains(module.getName())) {
+							sysout << "    + Site " + site.getName() + " contains components of module " + module.getName() + ". It will be deployed to the site.\n";
+						} else {
+							sysout << "    - Site " + site.getName() + " does not contain components of module " + module.getName() + ". It won't be deployed to the site.\n";
+						}
+                    }
+                    
+                    if (toDeploy.isEmpty()) {
+                    	sysout << "  No new modules will need to be deployed on the site " + site.getName() + "\n";
+                    } else {
+                    	sysout << "  Following modules will deployed to the site " + site.getName() + ": " + toDeploy + "\n";
+	                    for (String deployedModule : toDeploy) {
+	                    	sysout << "    Deploying module " + deployedModule + " to site " + site.getName() + "\n";
+	                    	templateService.deployModule("/templateSets/" + deployedModule, site.getPath(), session); 
+	                    }
+	                    session.save();
+                    }
+                }
+                
+                return null;
+            }
+
+        });
+
+sysout << "\n... done analyzing sites and deploying modules.\n"

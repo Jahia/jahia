588769caf6ab436db95430eba9f8e1cc47c762a7
Sergiy Shyrkov <sshyrkov@jahia.com>
2011-10-26 15:24:40 +0000

    Correct whitespaces
    git-svn-id: svn://localhost/jahia/trunk@14325 69c369ed-ed09-0410-a4a1-e19a46c23626

--- war/src/main/webapp/WEB-INF/var/patches/groovy/6.6.0.0-04-visibility-permissions.groovy
+++ war/src/main/webapp/WEB-INF/var/patches/groovy/6.6.0.0-04-visibility-permissions.groovy
@@ -6,15 +6,15 @@ def log = log;
 log.info("Start granting viewVisibilityTab permission to editor role")
 
 JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Object>() {
-	public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
-		if (RBACUtils.grantPermissionToRole(RBACUtils.getOrCreatePermission("/permissions/editMode/engineTabs/viewVisibilityTab", session).getPath(), "editor", session)) {
-			session.save();
-			log.info("Permission granted")
-		} else {
-			log.info("Role already has the permission")
-		}
-				
-		return null;
+    public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
+        if (RBACUtils.grantPermissionToRole(RBACUtils.getOrCreatePermission("/permissions/editMode/engineTabs/viewVisibilityTab", session).getPath(), "editor", session)) {
+            session.save();
+            log.info("Permission granted")
+        } else {
+            log.info("Role already has the permission")
+        }
+
+        return null;
     }
 });
 
--- war/src/main/webapp/WEB-INF/var/patches/groovy/6.6.0.0-05-templates-permissions.groovy
+++ war/src/main/webapp/WEB-INF/var/patches/groovy/6.6.0.0-05-templates-permissions.groovy
@@ -6,15 +6,15 @@ def log = log;
 log.info("Start granting adminSiteTemplates permission to site-administrator role")
 
 JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Object>() {
-	public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
-		if (RBACUtils.grantPermissionToRole(RBACUtils.getOrCreatePermission("/permissions/admin/adminSiteTemplates", session).getPath(), "site-administrator", session)) {
-			session.save();
-			log.info("Permission granted")
-		} else {
-			log.info("Role already has the permission")
-		}
-				
-		return null;
+    public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
+        if (RBACUtils.grantPermissionToRole(RBACUtils.getOrCreatePermission("/permissions/admin/adminSiteTemplates", session).getPath(), "site-administrator", session)) {
+            session.save();
+            log.info("Permission granted")
+        } else {
+            log.info("Role already has the permission")
+        }
+
+        return null;
     }
 });
 

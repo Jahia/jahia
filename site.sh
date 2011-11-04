b6bbe21e0d996e955b9dc8ba3446d9652a10178d
Sergiy Shyrkov <sshyrkov@jahia.com>
2011-11-04 13:08:32 +0000

    Allow overriding maven repository path
    git-svn-id: svn://localhost/jahia/trunk@14436 69c369ed-ed09-0410-a4a1-e19a46c23626

--- site.sh
+++ site.sh
@@ -3,6 +3,11 @@ if [ "$1" = "" ] ; then
 else
   MAVEN_SITE_PATH="$1"
 fi
-mvn clean
-mvn -DgenerateReports=false site:site
-mvn -Djahia.site.path=$MAVEN_SITE_PATH site:deploy
+if [ "$2" = "" ] ; then
+  USE_MAVEN_REPO=""
+else
+  USE_MAVEN_REPO="-Dmaven.repo.local=$2"
+fi
+mvn clean $USE_MAVEN_REPO
+mvn -DgenerateReports=false site:site $USE_MAVEN_REPO
+mvn -Djahia.site.path=$MAVEN_SITE_PATH site:deploy $USE_MAVEN_REPO

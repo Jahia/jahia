944f68ae97bcbd1c086b564649be8f4175ea5ea1
Benjamin Papež <bpapez@jahia.com>
2012-02-09 08:28:33 +0000

    Prevent NPE (manual merge from before)
    git-svn-id: svn://localhost/jahia/trunk@15118 69c369ed-ed09-0410-a4a1-e19a46c23626

--- gwt/src/main/java/org/jahia/ajax/gwt/client/widget/content/AbstractView.java
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/widget/content/AbstractView.java
@@ -82,6 +82,10 @@ public abstract class AbstractView extends TopRightComponent {
             public boolean equals(Object obj) {
                 Object o1 = get(GWTJahiaNode.PRIMARY_TYPE_LABEL);
                 Object o2 = ((ModelData) obj).get(GWTJahiaNode.PRIMARY_TYPE_LABEL);
+                if (o1 == o2)
+                    return true;
+                if (o1 == null || o2 == null)
+                    return false;
                 return o1.equals(o2);
             }
         };

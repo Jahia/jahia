09ba9060b558494016da7af7426dc7831c9a45a9
bamboo <bamboo@jahia.com>
2012-02-15 13:03:32 +0000

    added comparator to jbpm user ( QA-3483 ) (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 40423)
    git-svn-id: svn://localhost/jahia/trunk@15189 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/workflow/jbpm/JBPMIdentitySession.java
+++ core/src/main/java/org/jahia/services/workflow/jbpm/JBPMIdentitySession.java
@@ -204,7 +204,7 @@ public class JBPMIdentitySession implements IdentitySession {
         throw new UnsupportedOperationException();
     }
 
-    class UserImpl implements User {
+    class UserImpl implements User,Comparable<User> {
         private String id;
         private String givenName;
         private String familyName;
@@ -231,6 +231,67 @@ public class JBPMIdentitySession implements IdentitySession {
 
         public String getBusinessEmail() {
             return businessEmail;
+        }
+
+        /**
+         * Compares this object with the specified object for order.  Returns a
+         * negative integer, zero, or a positive integer as this object is less
+         * than, equal to, or greater than the specified object.
+         * <p/>
+         * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
+         * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+         * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
+         * <tt>y.compareTo(x)</tt> throws an exception.)
+         * <p/>
+         * <p>The implementor must also ensure that the relation is transitive:
+         * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
+         * <tt>x.compareTo(z)&gt;0</tt>.
+         * <p/>
+         * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
+         * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
+         * all <tt>z</tt>.
+         * <p/>
+         * <p>It is strongly recommended, but <i>not</i> strictly required that
+         * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
+         * class that implements the <tt>Comparable</tt> interface and violates
+         * this condition should clearly indicate this fact.  The recommended
+         * language is "Note: this class has a natural ordering that is
+         * inconsistent with equals."
+         * <p/>
+         * <p>In the foregoing description, the notation
+         * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+         * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+         * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+         * <i>expression</i> is negative, zero or positive.
+         *
+         * @param o the object to be compared.
+         * @return a negative integer, zero, or a positive integer as this object
+         *         is less than, equal to, or greater than the specified object.
+         * @throws ClassCastException if the specified object's type prevents it
+         *                            from being compared to this object.
+         */
+        public int compareTo(User o) {
+            return (getFamilyName()+getGivenName()).compareTo(o.getFamilyName()+o.getGivenName());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            UserImpl user = (UserImpl) o;
+
+            return id.equals(user.id);
+
+        }
+
+        @Override
+        public int hashCode() {
+            return id.hashCode();
         }
     }
 

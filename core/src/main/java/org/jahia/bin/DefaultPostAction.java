9fdb2ce0b3690946f4ffa6de69c4e37b44b0833f
bamboo <bamboo@jahia.com>
2012-03-07 16:56:18 +0000

    check mandatory properties for all property types ( including references ) when using rest calls ( BOUYGUES-9 ) (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 40557)
    git-svn-id: svn://localhost/jahia/trunk@15323 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/bin/DefaultPostAction.java
+++ core/src/main/java/org/jahia/bin/DefaultPostAction.java
@@ -43,6 +43,8 @@ package org.jahia.bin;
 import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.apache.commons.lang.StringUtils;
 import org.jahia.services.content.*;
+import org.jahia.services.content.nodetypes.ExtendedPropertyDefinition;
+import org.jahia.services.content.nodetypes.NodeTypeRegistry;
 import org.jahia.services.logging.MetricsLoggingService;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
@@ -52,7 +54,9 @@ import org.jahia.tools.files.FileUpload;
 import org.jahia.utils.Patterns;
 import org.json.JSONObject;
 
+import javax.jcr.InvalidItemStateException;
 import javax.jcr.PathNotFoundException;
+import javax.jcr.PropertyType;
 import javax.jcr.RepositoryException;
 import javax.jcr.nodetype.ConstraintViolationException;
 import javax.servlet.http.HttpServletRequest;
@@ -161,6 +165,16 @@ public class DefaultPostAction extends Action {
                                 itemEntry.getValue().getContentType());
                     }
                 }
+
+                for (String s : newNode.getNodeTypes()) {
+                    ExtendedPropertyDefinition[] propDefs = NodeTypeRegistry.getInstance().getNodeType(s).getPropertyDefinitions();
+                    for (ExtendedPropertyDefinition propDef : propDefs) {
+                        if (propDef.isMandatory() && !propDef.isProtected() && !newNode.hasProperty(propDef.getName())) {
+                            throw new ConstraintViolationException("Mandatory field : "+propDef.getName());
+                        }
+                    }
+                }
+
                 session.save();
             } catch (ConstraintViolationException e) {
                 return new ActionResult(HttpServletResponse.SC_BAD_REQUEST);

3a01aab3eedc6f438e02c992f4eccbfe43938929
Benjamin Papež <bpapez@jahia.com>
2012-03-13 08:36:12 +0000

    BACKLOG-2458 resourceBundle macro
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-6-X-X-BRANCH@15363 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ modules/macros/src/main/webapp/WEB-INF/modules-macros/resourceBundle.groovy
@@ -0,0 +1,12 @@
+import javax.servlet.jsp.jstl.core.Config
+import javax.servlet.jsp.jstl.fmt.LocalizationContext
+import org.jahia.utils.i18n.JahiaResourceBundle
+
+// LocalizationContext locCtxt = renderContext.getRequest().getAttribute(Config.FMT_LOCALIZATION_CONTEXT + ".request")
+// ResourceBundle bundle = null
+// if (locCtxt != null) 
+//    bundle = locCtxt.getResourceBundle()
+// if (bundle != null) 
+//    print bundle.getString(param1)
+
+print JahiaResourceBundle.getString(binding.variables.containsKey("param2") ? param2 : null, param1, renderContext.getMainResourceLocale(), renderContext.getSite().getTemplatePackageName())

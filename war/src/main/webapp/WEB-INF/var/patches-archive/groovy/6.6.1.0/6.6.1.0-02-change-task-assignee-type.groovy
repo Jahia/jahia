34cbbd89751e9a99b1a21ad3b954a8f448d248d9
Quentin Lamérand <qlamerand@jahia.com>
2012-03-21 18:06:08 +0000

    BACKLOG-2541: Add task definition migration script
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-6-X-X-BRANCH@15499 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ war/src/main/webapp/WEB-INF/var/patches-archive/groovy/6.6.1.0/6.6.1.0-02-change-task-assignee-type.groovy
@@ -0,0 +1,82 @@
+import org.jahia.services.content.*
+
+import javax.jcr.*
+import javax.jcr.nodetype.NoSuchNodeTypeException
+import javax.jcr.query.InvalidQueryException
+import javax.jcr.query.Query
+
+import org.apache.log4j.Level
+import org.apache.log4j.LogManager
+import org.apache.log4j.Logger
+
+def log = log;
+log.info("Start patch for task nodes")
+
+JCRCallback callback = new JCRCallback<Integer>() {
+    public Integer doInJCR(JCRSessionWrapper session) throws RepositoryException {
+        Query q = session.getWorkspace().getQueryManager().createQuery("select * from [jnt:task]", Query.JCR_SQL2);
+        int count = 0;
+        try {
+            NodeIterator ni = q.execute().getNodes();
+            while (ni.hasNext()) {
+                JCRNodeWrapper next = (JCRNodeWrapper) ni.next();
+                log.debug("  processing  {}", next.path);
+                if (!next.hasProperty("assignee")) {
+                    log.debug("    no assignee property found -> skipped");
+                    continue;
+                }
+                String assigneeUuid = next.getProperty("assignee").getString();
+                log.debug("    assignee value: {}", assigneeUuid);
+                if (JCRContentUtils.isNotJcrUuid(assigneeUuid)) {
+                    log.debug("    assignee value is not a valid UUID -> skipped");
+                    continue;
+                }
+
+                try {
+                    String assigneeUserKey = session.getNodeByIdentifier(assigneeUuid).getName();
+                    log.debug("    setting assigneeUserKey to: {}", assigneeUserKey)
+                    next.setProperty("assigneeUserKey", assigneeUserKey);
+                    count++;
+                    log.debug("updated ${next.path} node, set assigneeUserKey value to ${assigneeUserKey} based on assignee ${assigneeUuid}");
+                } catch (ItemNotFoundException e) {
+                    log.debug("    target no longer exists -> skipped");
+                }
+                if (count > 0 && (count % 1000 == 0)) {
+                    session.save();
+                }
+            }
+        } catch (InvalidQueryException e) {
+            if (e.getCause() == null || !(e.getCause() instanceof NoSuchNodeTypeException)) {
+                log.warn("Unable to execute the query. Cause: " + e.getMessage(), e);
+            }
+
+        }
+
+        if (count > 0) {
+            session.save();
+        }
+        return count;
+    }
+};
+
+Logger imLogger = LogManager.getLogger("org.apache.jackrabbit.core.ItemManager");
+Level oldLevel = imLogger.getLevel();
+imLogger.setLevel(Level.ERROR);
+
+try {
+    log.info("---------------------------------------------------");
+    log.info("  Looking in default workspace");
+    int defaulCount = JCRTemplate.getInstance().doExecuteWithSystemSession(callback);
+    log.info("  updated {} tasks in default workspace", defaulCount);
+
+    log.info("---------------------------------------------------");
+    log.info("Looking in live workspace");
+    int liveCount = JCRTemplate.getInstance().doExecuteWithSystemSession(null, "live", callback);
+    log.info("  updated {} tasks in live workspace", liveCount);
+    log.info("---------------------------------------------------");
+
+
+    log.info("...done patch for task nodes.");
+} finally {
+    imLogger.setLevel(oldLevel);
+}

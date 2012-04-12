e7bfc553324d5187e684f71e167d44d0a348aab7
Cédric Mailleux <cmailleux@jahia.com>
2012-04-12 20:20:53 +0000

    BACKLOG-2723 Add tabs permissions for previous versions on contributor role
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-6-0-X-BRANCH@15796 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ war/src/main/webapp/WEB-INF/var/patches-archive/groovy/6.6.0.1/6.6.0.1-01-tabs-permissions.groovy
@@ -0,0 +1,28 @@
+import javax.jcr.RepositoryException
+import org.jahia.services.content.JCRCallback
+import org.jahia.services.content.JCRSessionWrapper
+import org.jahia.services.content.JCRTemplate
+import org.jahia.services.content.RBACUtils
+
+def log = log;
+
+log.info("Start granting some tabs permission to contributor role")
+
+def permissions = ["/permissions/editMode/engineTabs/viewContentTab", "/permissions/editMode/engineTabs/viewMetadataTab",
+        "/permissions/editMode/engineTabs/viewCategoriesTab", "/permissions/editMode/engineTabs/viewTagsTab"]
+
+JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Object>() {
+    public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
+        permissions.each() { permission ->
+            if (RBACUtils.grantPermissionToRole(RBACUtils.getOrCreatePermission(permission, session).getPath(), "contributor", session)) {
+                log.info("Permission ${permission} granted to contributor")
+            } else {
+                log.info("Role contributor already has the permission ${permission}")
+            }
+        }
+        session.save();
+        return null;
+    }
+});
+
+log.info("... done granting tabs permission to contributor role.")

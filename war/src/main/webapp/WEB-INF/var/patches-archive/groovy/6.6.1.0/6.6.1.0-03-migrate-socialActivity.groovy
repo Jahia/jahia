768d48f6a885b8c19b0ac093c4ef7e4ad18a4a86
bamboo <bamboo@jahia.com>
2012-04-26 10:08:00 +0000

    migration script for social activities ( BACKLOG-2684 ) (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 41227)
    git-svn-id: svn://localhost/jahia/trunk@15993 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ war/src/main/webapp/WEB-INF/var/patches-archive/groovy/6.6.1.0/6.6.1.0-03-migrate-socialActivity.groovy
@@ -0,0 +1,69 @@
+import org.jahia.services.content.*
+
+import javax.jcr.*
+import javax.jcr.nodetype.NoSuchNodeTypeException
+import javax.jcr.query.InvalidQueryException
+import javax.jcr.query.Query
+
+import org.apache.log4j.Level
+import org.apache.log4j.LogManager
+import org.apache.log4j.Logger
+
+def log = log;
+log.info("Start patch for social activity nodes")
+
+JCRCallback callback = new JCRCallback<Integer>() {
+    public Integer doInJCR(JCRSessionWrapper session) throws RepositoryException {
+        Query q = session.getWorkspace().getQueryManager().createQuery("select * from [jnt:socialActivity]", Query.JCR_SQL2);
+        int count = 0;
+        try {
+            NodeIterator ni = q.execute().getNodes();
+            while (ni.hasNext()) {
+                JCRNodeWrapper next = (JCRNodeWrapper) ni.next();
+                log.debug("  processing  {}", next.path);
+                if (!next.hasProperty("j:message")) {
+                    next.remove();
+                    log.debug("removed ${next.path} node");
+                } else {
+                    next.setProperty("j:activityType","text");
+                    log.debug("updated ${next.path}");
+                }
+
+                count++;
+                log.debug("updated ${next.path} node, converted j:targetNode value from ${targetUuid} to ${path}");
+            }
+        } catch (InvalidQueryException e) {
+            if (e.getCause() == null || !(e.getCause() instanceof NoSuchNodeTypeException)) {
+                log.warn("Unable to execute the query. Cause: " + e.getMessage(), e);
+            }
+            
+        }
+        
+        if (count > 0) {
+            session.save();
+        }
+        return count;
+    }
+};
+
+Logger imLogger = LogManager.getLogger("org.apache.jackrabbit.core.ItemManager");
+Level oldLevel = imLogger.getLevel();
+imLogger.setLevel(Level.ERROR);
+
+try {
+log.info("---------------------------------------------------");
+log.info("  Looking in default workspace");
+int defaulCount = JCRTemplate.getInstance().doExecuteWithSystemSession(callback);
+log.info("  updated {} activities in default workspace", defaulCount);
+
+log.info("---------------------------------------------------");
+log.info("Looking in live workspace");
+int liveCount = JCRTemplate.getInstance().doExecuteWithSystemSession(null, "live", callback);
+log.info("  updated {} activities in live workspace", liveCount);
+log.info("---------------------------------------------------");
+
+
+log.info("...done patch for social activity nodes.");
+} finally {
+imLogger.setLevel(oldLevel);
+}

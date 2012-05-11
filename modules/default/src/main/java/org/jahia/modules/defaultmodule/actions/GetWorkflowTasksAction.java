2f7ea761b7c3c54aadc0f0dcb55c2196e07b1147
bamboo <bamboo@jahia.com>
2012-05-11 15:46:40 +0000

    moved generic components from docspace (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 41476)
    git-svn-id: svn://localhost/jahia/trunk@16242 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ modules/default/src/main/java/org/jahia/modules/defaultmodule/actions/GetWorkflowTasksAction.java
@@ -0,0 +1,113 @@
+/**
+ * This file is part of Jahia, next-generation open source CMS:
+ * Jahia's next-generation, open source CMS stems from a widely acknowledged vision
+ * of enterprise application convergence - web, search, document, social and portal -
+ * unified by the simplicity of web content management.
+ *
+ * For more information, please visit http://www.jahia.com.
+ *
+ * Copyright (C) 2002-2012 Jahia Solutions Group SA. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * As a special exception to the terms and conditions of version 2.0 of
+ * the GPL (or any later version), you may redistribute this Program in connection
+ * with Free/Libre and Open Source Software ("FLOSS") applications as described
+ * in Jahia's FLOSS exception. You should have received a copy of the text
+ * describing the FLOSS exception, and it is also available here:
+ * http://www.jahia.com/license
+ *
+ * Commercial and Supported Versions of the program (dual licensing):
+ * alternatively, commercial and supported versions of the program may be used
+ * in accordance with the terms and conditions contained in a separate
+ * written agreement between you and Jahia Solutions Group SA.
+ *
+ * If you are unsure which license is appropriate for your use,
+ * please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.modules.defaultmodule.actions;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.xerces.jaxp.SAXParserFactoryImpl;
+import org.jahia.bin.Action;
+import org.jahia.bin.ActionResult;
+import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.Resource;
+import org.jahia.services.render.URLResolver;
+import org.jahia.utils.Patterns;
+import org.json.JSONObject;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Retrieves the workflow task location.
+ *
+ * @author Thomas Draier
+ */
+public class GetWorkflowTasksAction extends Action {
+
+    @Override
+    public ActionResult doExecute(HttpServletRequest req, RenderContext renderContext, Resource resource, JCRSessionWrapper session, Map<String, List<String>> parameters, URLResolver urlResolver) throws Exception {
+        String wfKey = parameters.get("workflowKey").get(0);
+        wfKey = StringUtils.substringAfter(wfKey,":");
+        InputStream in = getClass().getResourceAsStream("/org/jahia/services/workflow/"+wfKey+".jpdl.xml");
+        if (in != null) {
+            InputSource is = new InputSource(in);
+            SAXParserFactory factory;
+            factory = new SAXParserFactoryImpl();
+
+            factory.setNamespaceAware(true);
+
+            factory.setValidating(false);
+            factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
+            SAXParser parser = factory.newSAXParser();
+
+            JpdlHandler handler = new JpdlHandler();
+            parser.parse(is, handler);
+            Map<String, String[]> c = handler.getCoords();
+
+            return new ActionResult(200, null, new JSONObject(c));
+        }
+        return ActionResult.OK_JSON;
+    }
+
+    class JpdlHandler extends DefaultHandler {
+        Map <String, String[]> coords = new HashMap<String, String[]>();
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            if (!StringUtils.isEmpty(attributes.getValue("name")) && !StringUtils.isEmpty(attributes.getValue("g")) && localName.equals("task") ) {
+                coords.put(attributes.getValue("name"), Patterns.COMMA.split(attributes.getValue("g")));
+            }
+        }
+
+        public Map<String, String[]> getCoords() {
+            return coords;
+        }
+    }
+
+}

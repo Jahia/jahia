5423b8f373e1d09258236b7f444f7659b98f321f
bamboo <bamboo@jahia.com>
2012-05-11 16:51:53 +0000

    BACKLOG-2491
    Introduce store history servlet to allow storage of viewed element from javascript (use when element are only displayed in pages not by themselves) (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 41484)
    
    git-svn-id: svn://localhost/jahia/trunk@16250 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ core/src/main/java/org/jahia/bin/StoreHistory.java
@@ -0,0 +1,80 @@
+/**
+ *
+ * This file is part of Jahia: An integrated WCM, DMS and Portal Solution
+ * Copyright (C) 2002-2009 Jahia Limited. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * As a special exception to the terms and conditions of version 2.0 of
+ * the GPL (or any later version), you may redistribute this Program in connection
+ * with Free/Libre and Open Source Software ("FLOSS") applications as described
+ * in Jahia's FLOSS exception. You should have recieved a copy of the text
+ * describing the FLOSS exception, and it is also available here:
+ * http://www.jahia.com/license"
+ *
+ * Commercial and Supported Versions of the program
+ * Alternatively, commercial and supported versions of the program may be used
+ * in accordance with the terms contained in a separate written agreement
+ * between you and Jahia Limited. If you are unsure which license is appropriate
+ * for your use, please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.bin;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.LinkedList;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author : rincevent
+ * @since : JAHIA 6.1
+ *        Created : 11/05/12
+ */
+public class StoreHistory extends JahiaController {
+    private transient static Logger logger = Logger.getLogger(StoreHistory.class);
+
+    /**
+     * Process the request and return a ModelAndView object which the DispatcherServlet
+     * will render. A <code>null</code> return value is not an error: It indicates that
+     * this object completed request processing itself, thus there is no ModelAndView
+     * to render.
+     *
+     * @param request  current HTTP request
+     * @param response current HTTP response
+     * @return a ModelAndView to render, or <code>null</code> if handled directly
+     * @throws Exception in case of errors
+     */
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+        String name = getParameter(request, "h", "historyTracker");
+        LinkedList<String> historyTracker = (LinkedList<String>) session.getAttribute(name);
+        if (historyTracker == null) {
+            session.setAttribute(name, historyTracker = new LinkedList<String>());
+        }
+        String identifier = getParameter(request, "i");
+        historyTracker.remove(identifier);
+        historyTracker.addFirst(identifier);
+        int historySize = getIntParameter(request, "s",10);
+        if (historyTracker.size() > historySize) {
+            historyTracker.removeLast();
+        }
+        return null;
+    }
+}

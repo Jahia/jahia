0b8f9e86a785bcb40fab052c91c9f8dc87a67385
bamboo <bamboo@jahia.com>
2012-06-04 18:16:14 +0000

    catch missing resource exception (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 41804)
    git-svn-id: svn://localhost/jahia/trunk@16570 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/macros/src/main/webapp/WEB-INF/modules-macros/resourceBundle.groovy
+++ modules/macros/src/main/webapp/WEB-INF/modules-macros/resourceBundle.groovy
@@ -1,5 +1,8 @@
 import java.util.Locale
 import org.jahia.utils.i18n.JahiaResourceBundle
-
-Locale loc = renderContext.getRequest().getAttribute(org.jahia.services.render.filter.ForceUILocaleFilter.RENDERING_FORCED_LOCALE)
-print JahiaResourceBundle.getString(binding.variables.containsKey("param2") ? param2 : null, param1, loc != null ? loc : renderContext.getMainResourceLocale(), renderContext.getSite().getTemplatePackageName())
+try {
+    Locale loc = renderContext.getRequest().getAttribute(org.jahia.services.render.filter.ForceUILocaleFilter.RENDERING_FORCED_LOCALE)
+    print JahiaResourceBundle.getString(binding.variables.containsKey("param2") ? param2 : null, param1, loc != null ? loc : renderContext.getMainResourceLocale(), renderContext.getSite().getTemplatePackageName())
+} catch (java.util.MissingResourceException e) {
+    print param1
+}

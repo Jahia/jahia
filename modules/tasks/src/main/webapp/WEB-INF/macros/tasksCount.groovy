7fcf69906c218968bc6bb32b46d9bc30bec22ddf
bamboo <bamboo@jahia.com>
2012-06-13 12:51:28 +0000

    BACKLOG-2975: SQL 'encode' node path (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 41928)
    git-svn-id: svn://localhost/jahia/trunk@16694 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/tasks/src/main/webapp/WEB-INF/macros/tasksCount.groovy
+++ modules/tasks/src/main/webapp/WEB-INF/macros/tasksCount.groovy
@@ -1,16 +1,20 @@
 import org.jahia.services.content.JCRNodeWrapper
 import javax.jcr.query.Query
 import org.jahia.services.query.QueryResultWrapper
-import javax.jcr.NodeIterator
 import javax.jcr.RepositoryException
+import org.jahia.utils.Patterns
+
+public String sqlEncode(String s) {
+    return s != null && s.contains("'") ? Patterns.SINGLE_QUOTE.matcher(s).replaceAll("''") : s;
+}
 
 JCRNodeWrapper n = renderContext.getMainResource().getNode();
 try {
     String query;
     if (param1 != null) {
-        query = "select * from [jnt:task] as t where isdescendantnode(t,['" + n.getPath() + "']) and t.type='" + param1 + "' order by [jcr:created] desc";
+        query = "select * from [jnt:task] as t where isdescendantnode(t,['" + sqlEncode(n.getPath()) + "']) and t.type='" + param1 + "' order by [jcr:created] desc";
     } else {
-        query = "select * from [jnt:task] as t where isdescendantnode(t,['" + n.getPath() + "']) order by [jcr:created] desc";
+        query = "select * from [jnt:task] as t where isdescendantnode(t,['" + sqlEncode(n.getPath()) + "']) order by [jcr:created] desc";
     }
     Query q = n.getSession().getWorkspace().getQueryManager().createQuery(query, Query.JCR_SQL2);
     QueryResultWrapper queryResult = (QueryResultWrapper) q.execute();

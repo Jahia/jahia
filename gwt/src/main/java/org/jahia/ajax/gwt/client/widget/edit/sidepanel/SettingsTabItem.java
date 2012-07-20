79721c1362d5d08ce388a2dc73ec22927a2a99b8
Cédric Mailleux <cmailleux@jahia.com>
2012-07-20 12:09:11 +0000

    Make settings tab redirect to the correct page when cliecked
    git-svn-id: svn://localhost/jahia/trunk@17062 69c369ed-ed09-0410-a4a1-e19a46c23626

--- gwt/src/main/java/org/jahia/ajax/gwt/client/widget/edit/sidepanel/SettingsTabItem.java
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/widget/edit/sidepanel/SettingsTabItem.java
@@ -1,17 +1,23 @@
 package org.jahia.ajax.gwt.client.widget.edit.sidepanel;
 
+import com.extjs.gxt.ui.client.Style;
 import com.extjs.gxt.ui.client.data.*;
+import com.extjs.gxt.ui.client.event.GridEvent;
 import com.extjs.gxt.ui.client.event.LoadListener;
+import com.extjs.gxt.ui.client.event.SelectionChangedEvent;
+import com.extjs.gxt.ui.client.event.SelectionChangedListener;
 import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.store.StoreSorter;
 import com.extjs.gxt.ui.client.widget.TabItem;
 import com.extjs.gxt.ui.client.widget.TabPanel;
 import com.extjs.gxt.ui.client.widget.treegrid.TreeGrid;
+import com.extjs.gxt.ui.client.widget.treegrid.TreeGridSelectionModel;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import org.jahia.ajax.gwt.client.data.node.GWTJahiaNode;
 import org.jahia.ajax.gwt.client.data.toolbar.GWTSidePanelTab;
 import org.jahia.ajax.gwt.client.service.content.JahiaContentManagementService;
 import org.jahia.ajax.gwt.client.util.Collator;
+import org.jahia.ajax.gwt.client.widget.edit.mainarea.MainModule;
 
 import java.util.Comparator;
 
@@ -38,13 +44,50 @@ public class SettingsTabItem extends BrowseTabItem {
     @Override
     public TabItem create(GWTSidePanelTab config) {
         super.create(config);
-        TabItem tabItem = new TabItem();
-        TabPanel tabPanel = new TabPanel();
-        tabItem.add(tabPanel);
 
         // get List of site settings
+        this.tree.setSelectionModel(new TreeGridSelectionModel<GWTJahiaNode>() {
+            @Override
+            protected void handleMouseClick(GridEvent<GWTJahiaNode> e) {
+                super.handleMouseClick(e);
+                if (!getSelectedItem().getPath().equals(editLinker.getMainModule().getPath())) {
+                    if (!getSelectedItem().getNodeTypes().contains("jnt:virtualsite") && !getSelectedItem().getNodeTypes().contains("jnt:settingsFolder")) {
+                        MainModule.staticGoTo(getSelectedItem().getPath(), null);
+                    }
+                }
+            }
+        });
+        this.tree.getSelectionModel().setSelectionMode(Style.SelectionMode.SINGLE);
+        this.tree.getSelectionModel().addSelectionChangedListener(new SelectionChangedListener<GWTJahiaNode>() {
+            @Override public void selectionChanged(SelectionChangedEvent<GWTJahiaNode> se) {
+                listLoader.load(se.getSelectedItem());
+            }
+        });
 
-        return super.create(config);
+        // data proxy
+        RpcProxy<ListLoadResult<GWTJahiaNode>> listProxy = new RpcProxy<ListLoadResult<GWTJahiaNode>>() {
+            @Override
+            protected void load(final Object gwtJahiaFolder, final AsyncCallback<ListLoadResult<GWTJahiaNode>> listAsyncCallback) {
+            }
+        };
+
+        listLoader = new BaseListLoader<ListLoadResult<GWTJahiaNode>>(listProxy);
+
+        contentStore = new ListStore<GWTJahiaNode>(listLoader);
+        contentStore.setStoreSorter(new StoreSorter<GWTJahiaNode>(new Comparator<Object>() {
+            public int compare(Object o1, Object o2) {
+                if (o1 instanceof String && o2 instanceof String) {
+                    String s1 = (String) o1;
+                    String s2 = (String) o2;
+                    return Collator.getInstance().localeCompare(s1,s2);
+                } else if (o1 instanceof Comparable && o2 instanceof Comparable) {
+                    return ((Comparable) o1).compareTo(o2);
+                }
+                return 0;
+            }
+        }));
+        contentStore.setSortField("display");
+        return tab;
     }
 
     public void setServerSettingMixin(String serverSettingMixin) {

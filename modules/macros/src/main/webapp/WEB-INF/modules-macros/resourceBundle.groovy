8475d764d840f5ae0b2d419f200fc392d44b1afe
bamboo <bamboo@jahia.com>
2012-07-30 13:38:39 +0000

    DOCSPACE-144: Fix resourceBundle macro (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 42389)
    git-svn-id: svn://localhost/jahia/trunk@17155 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/macros/src/main/webapp/WEB-INF/modules-macros/resourceBundle.groovy
+++ modules/macros/src/main/webapp/WEB-INF/modules-macros/resourceBundle.groovy
@@ -1,8 +1,7 @@
-import java.util.Locale
 import org.jahia.utils.i18n.JahiaResourceBundle
+
 try {
-    Locale loc = renderContext.getRequest().getAttribute(org.jahia.services.render.filter.ForceUILocaleFilter.RENDERING_FORCED_LOCALE)
-    print JahiaResourceBundle.getString(binding.variables.containsKey("param2") ? param2 : null, param1, loc != null ? loc : renderContext.getMainResourceLocale(), renderContext.getSite().getTemplatePackageName())
+    print JahiaResourceBundle.getString(binding.variables.containsKey("param2") ? param2 : null, param1, renderContext.getMainResourceLocale(), renderContext.getSite().getTemplatePackageName())
 } catch (java.util.MissingResourceException e) {
     print param1
 }

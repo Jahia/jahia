0f59f4b40ab684d50c5e07fd8662a08e7d5694da
Benjamin Papež <bpapez@jahia.com>
2012-08-21 14:59:40 +0000

    check for mixin before setting lastModified on parent
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-6-X-X-BRANCH@17519 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/content/LastModifiedListener.java
+++ core/src/main/java/org/jahia/services/content/LastModifiedListener.java
@@ -42,15 +42,12 @@ package org.jahia.services.content;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.jackrabbit.core.security.JahiaLoginModule;
-import org.jahia.services.content.nodetypes.ExtendedPropertyDefinition;
-import org.jahia.services.content.nodetypes.NodeTypeRegistry;
 import org.slf4j.Logger;
 
 import javax.jcr.ItemNotFoundException;
 import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
-import javax.jcr.nodetype.ConstraintViolationException;
 import javax.jcr.observation.Event;
 import javax.jcr.observation.EventIterator;
 import java.util.*;
@@ -193,14 +190,16 @@ public class LastModifiedListener extends DefaultEventListener {
 
         if (type != JCRObservationManager.IMPORT || isAutoPublished) {
             if (!n.isCheckedOut()) {
-                n.checkout();
+                n.getSession().getWorkspace().getVersionManager().checkout(n.getPath());
             }
             n.setProperty(JCR_LASTMODIFIED,c);
             n.setProperty(JCR_LASTMODIFIEDBY, userId);
             if (n.isNodeType("nt:resource")) {
-                JCRNodeWrapper file = n.getParent();
-                file.setProperty(JCR_LASTMODIFIED, c);
-                file.setProperty(JCR_LASTMODIFIEDBY, userId);
+                JCRNodeWrapper parent = n.getParent();
+                if (parent.isNodeType(MIX_LAST_MODIFIED)) {
+                    parent.setProperty(JCR_LASTMODIFIED, c);
+                    parent.setProperty(JCR_LASTMODIFIEDBY, userId);
+                }
             }
         }
     }

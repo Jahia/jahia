96411020d62078121cd21b834243f50260759e64
Sergiy Shyrkov <sshyrkov@jahia.com>
2012-09-05 17:05:07 +0000

    QA-3797 - Broken links on pictures stored in wise (not specific to wise cf last comment) https://jira.jahia.org/browse/QA-3797
    Automatically handle cache dependencies for files.
    
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-6-X-X-BRANCH@17716 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/filter/cache/CacheUrlDependenciesParserFilter.java
+++ core/src/main/java/org/jahia/services/render/filter/cache/CacheUrlDependenciesParserFilter.java
@@ -33,21 +33,25 @@
 package org.jahia.services.render.filter.cache;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
+import org.jahia.bin.Jahia;
 import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.URLResolver;
 import org.jahia.services.render.URLResolverFactory;
 import org.jahia.services.render.filter.HtmlTagAttributeTraverser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  * @author rincevent
- *        Created : 12/15/11
+ * Created : 12/15/11
  */
-public class CacheUrlDependenciesParserFilter implements HtmlTagAttributeTraverser.HtmlTagAttributeVisitor {
-    private transient static Logger logger = Logger.getLogger(CacheUrlDependenciesParserFilter.class);
+public class CacheUrlDependenciesParserFilter implements HtmlTagAttributeTraverser.HtmlTagAttributeVisitor, InitializingBean {
+    private static Logger logger = LoggerFactory.getLogger(CacheUrlDependenciesParserFilter.class);
     private URLResolverFactory urlResolverFactory;
+    private String liveDmsContext;
 
     /**
      * Applies the required modifications to the specified attribute if
@@ -63,17 +67,27 @@ public class CacheUrlDependenciesParserFilter implements HtmlTagAttributeTravers
     public String visit(String value, RenderContext context, String tagName, String attrName, Resource resource) {
         String contextConfiguration = resource.getContextConfiguration();
         if (context.isLiveMode()
-                && StringUtils.isNotEmpty(value)
                 && contextConfiguration.equals(Resource.CONFIGURATION_MODULE)
-                && value.startsWith(context.getRequest().getContextPath().length() > 0 ? context
-                        .getRequest().getContextPath() + context.getServletPath() : context
-                        .getServletPath())) {
-            try {
-                URLResolver urlResolver = urlResolverFactory.createURLResolver(value, context);
-                JCRNodeWrapper nodeWrapper = urlResolver.getNode();
-                resource.getDependencies().add(nodeWrapper.getCanonicalPath());
-            } catch (Exception e) {
-                logger.debug(e.getMessage(), e);
+                && StringUtils.isNotEmpty(value)) {
+
+            if (value.startsWith(liveDmsContext)) {
+                resource.getDependencies().add(
+                        StringUtils.substringAfter(
+                                value.contains("?") ? StringUtils.substringBefore(value, "?")
+                                        : value, liveDmsContext));
+            } else if (value
+                    .startsWith(context.getRequest().getContextPath().length() > 0 ? context
+                            .getRequest().getContextPath() + context.getServletPath() : context
+                            .getServletPath())) {
+                if (!value.equals(resource.getNode().getUrl())) {
+                    try {
+                        URLResolver urlResolver = urlResolverFactory.createURLResolver(value, context);
+                        JCRNodeWrapper nodeWrapper = urlResolver.getNode();
+                        resource.getDependencies().add(nodeWrapper.getCanonicalPath());
+                    } catch (Exception e) {
+                        logger.debug(e.getMessage(), e);
+                    }
+                }
             }
         }
         return value;
@@ -81,5 +95,9 @@ public class CacheUrlDependenciesParserFilter implements HtmlTagAttributeTravers
 
     public void setUrlResolverFactory(URLResolverFactory urlResolverFactory) {
         this.urlResolverFactory = urlResolverFactory;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        liveDmsContext = Jahia.getContextPath() + "/files/live";
     }
 }

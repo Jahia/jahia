44e55346cec331161e61a95491d6d724e8deab74
bamboo <bamboo@jahia.com>
2012-09-21 10:10:33 +0000

    QA-3846 - Contribute CKeditor, missing button https://jira.jahia.org/browse/QA-3846
    Granted permission to use Basic CKEditor toolbar to contributor and editor-in-chief roles. (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 43174)
    
    git-svn-id: svn://localhost/jahia/trunk@17940 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ war/src/main/webapp/WEB-INF/var/patches-archive/groovy/6.6.1.0/6.6.1.0-07-ckeditor-toolbar-permissions.groovy
@@ -0,0 +1,56 @@
+import javax.jcr.*
+
+import org.jahia.services.content.*
+
+def log = log;
+
+log.info("Start granting CKEditor Basic toolbar permission to contributor and editor-in-chief roles...")
+
+JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Object>() {
+    public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
+        boolean doSave = false;
+        if (RBACUtils.revokePermissionFromRole("/permissions/wysiwyg-editor-toolbar/view-light-wysiwyg-editor", "editor-in-chief", session)) {
+            doSave = RBACUtils.grantPermissionToRole("/permissions/wysiwyg-editor-toolbar/view-basic-wysiwyg-editor", "editor-in-chief", session);
+            if (doSave) {
+                log.info("Permission granted")
+            }
+        } else {
+            JCRNodeWrapper role = session.getNode("/roles/editor-in-chief");
+            boolean found = false;
+            for (Value val : role.getProperty("j:permissions").getValues()) {
+                JCRNodeWrapper perm = ((JCRValueWrapper) val).getNode();
+                if (perm != null && perm.getPath().startsWith("/permissions/wysiwyg-editor-toolbar")) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                doSave = RBACUtils.grantPermissionToRole("/permissions/wysiwyg-editor-toolbar/view-basic-wysiwyg-editor", "editor-in-chief", session);
+            } else {
+                log.info("Role editor-in-chief already has toolbar permission");
+            }
+        }
+        
+        JCRNodeWrapper role = session.getNode("/roles/contributor");
+        boolean found = false;
+        for (Value val : role.getProperty("j:permissions").getValues()) {
+            JCRNodeWrapper perm = ((JCRValueWrapper) val).getNode();
+            if (perm != null && perm.getPath().startsWith("/permissions/wysiwyg-editor-toolbar")) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            doSave = RBACUtils.grantPermissionToRole("/permissions/wysiwyg-editor-toolbar/view-basic-wysiwyg-editor", "contributor", session);
+        } else {
+            log.info("Role contributor already has toolbar permission");
+        }
+
+        if (doSave) {
+            session.save();
+        }
+        return null;
+    }
+});
+
+log.info("... done granting CKEditor toolbar permissions.")

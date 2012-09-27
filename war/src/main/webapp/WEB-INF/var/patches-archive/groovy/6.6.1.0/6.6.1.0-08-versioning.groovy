154bd1cf96d9243d7fc41109adc3b459a8e72500
Quentin Lamérand <qlamerand@jahia.com>
2012-09-27 15:47:22 +0000

    QA-3862: Add 6.6.1.0 migration scripts
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-6-X-X-BRANCH@17986 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ war/src/main/webapp/WEB-INF/var/patches-archive/groovy/6.6.1.0/6.6.1.0-08-versioning.groovy
@@ -0,0 +1,45 @@
+import org.jahia.services.content.JCRTemplate
+import org.jahia.services.content.JCRCallback
+import org.jahia.services.content.JCRSessionWrapper
+import javax.jcr.RepositoryException
+import javax.jcr.query.QueryResult
+import javax.jcr.NodeIterator
+import org.jahia.services.content.JCRNodeWrapper
+import javax.jcr.query.Query
+import org.jahia.services.content.JCRObservationManager
+
+JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Object>() {
+    public Object doInJCR(JCRSessionWrapper jcrsession) throws RepositoryException {
+        JCRObservationManager.setEventsDisabled(Boolean.TRUE);
+
+        try {
+            removeVersionable(jcrsession, "jnt:component");
+        } finally {
+            JCRObservationManager.setEventsDisabled(Boolean.FALSE);
+        }
+
+        return null;
+    }
+
+    private void removeVersionable(JCRSessionWrapper jcrsession, String type) {
+        QueryResult result = jcrsession.getWorkspace().getQueryManager().createQuery("select * from ["+ type +"]", Query.JCR_SQL2).execute();
+        NodeIterator ni = result.getNodes();
+        while (ni.hasNext()) {
+            try {
+                JCRNodeWrapper next = (JCRNodeWrapper) ni.next();
+                next.getRealNode().addMixin("mix:versionable");
+                jcrsession.save();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        result = jcrsession.getWorkspace().getQueryManager().createQuery("select * from ["+ type +"]", Query.JCR_SQL2).execute();
+        ni = result.getNodes();
+        while (ni.hasNext()) {
+            JCRNodeWrapper next = (JCRNodeWrapper) ni.next();
+            next.getRealNode().removeMixin("mix:versionable");
+        }
+        jcrsession.save();
+    }
+});

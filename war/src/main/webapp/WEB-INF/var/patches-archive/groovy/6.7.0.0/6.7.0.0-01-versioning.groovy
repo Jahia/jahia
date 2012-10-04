5b141c21f7618848633a7bea9f845bf3161eb0f0
Sergiy Shyrkov <sshyrkov@jahia.com>
2012-10-04 12:50:51 +0000

    BACKLOG-3249 - Check migration from previous version https://jira.jahia.org/browse/BACKLOG-3249
    Refactor the migration script to prevent warnings. Execute on both workspaces. Correct removal of component folders.
    Launch background jobs for removal of the version history for processed nodes.
    Exclude jnt:portletDefinitions from versioning.
    Remove jmix:lastPublished from jnt:component as it won't be ever published.
    
    git-svn-id: svn://localhost/jahia/trunk@18064 69c369ed-ed09-0410-a4a1-e19a46c23626

--- war/src/main/webapp/WEB-INF/var/patches-archive/groovy/6.7.0.0/6.7.0.0-01-versioning.groovy
+++ war/src/main/webapp/WEB-INF/var/patches-archive/groovy/6.7.0.0/6.7.0.0-01-versioning.groovy
@@ -1,47 +1,110 @@
-import org.jahia.services.content.JCRTemplate
-import org.jahia.services.content.JCRCallback
-import org.jahia.services.content.JCRSessionWrapper
-import javax.jcr.RepositoryException
-import javax.jcr.query.QueryResult
 import javax.jcr.NodeIterator
-import org.jahia.services.content.JCRNodeWrapper
+import javax.jcr.RepositoryException
 import javax.jcr.query.Query
-import org.jahia.services.content.JCRObservationManager
+import javax.jcr.query.QueryResult
 
-JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Object>() {
+import org.apache.jackrabbit.core.ItemManager;
+import org.apache.jackrabbit.core.state.ItemStateReferenceCache
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.jahia.registries.ServicesRegistry
+import org.jahia.services.content.JCRCallback
+import org.jahia.services.content.JCRNodeWrapper
+import org.jahia.services.content.JCRObservationManager
+import org.jahia.services.content.JCRSessionWrapper
+import org.jahia.services.content.JCRTemplate
+import org.jahia.services.history.NodeVersionHistoryJob
+import org.jahia.services.scheduler.BackgroundJob
+import org.quartz.JobDataMap
+import org.quartz.JobDetail
+import org.quartz.SchedulerException
+
+final Logger log = Logger.getLogger("org.jahia.tools.groovyConsole");
+
+JCRTemplate.getInstance().doExecuteWithSystemSession(null, "live", new JCRCallback<Object>() {
     public Object doInJCR(JCRSessionWrapper jcrsession) throws RepositoryException {
         JCRObservationManager.setEventsDisabled(Boolean.TRUE);
+        Logger logger = Logger.getLogger(ItemManager.class);
+        Level previousLevel = logger.getLevel();
+        logger.setLevel(Level.ERROR);
 
         try {
-            removeVersionable(jcrsession, "jnt:user")
-            removeVersionable(jcrsession, "jnt:group")
-            removeVersionable(jcrsession, "jnt:virtualsitesFolder")
-            removeVersionable(jcrsession, "jnt:virtualsite")
-            removeVersionable(jcrsession, "jnt:tag")
-            removeVersionable(jcrsession, "jnt:category")
-            removeVersionable(jcrsession, "jnt:roles")
-            removeVersionable(jcrsession, "jnt:role")
-            removeVersionable(jcrsession, "jnt:permission")
-            removeVersionable(jcrsession, "jnt:usersFolder")
-            removeVersionable(jcrsession, "jnt:groupsFolder")
-            removeVersionable(jcrsession, "jnt:componentFolder")
-            removeVersionable(jcrsession, "jnt:templateSets")
-            removeVersionable(jcrsession, "jnt:portletDefinition");
+            log.info("Start removing components from live workspace");
+            QueryResult result = jcrsession.getWorkspace().getQueryManager().createQuery("select * from [jnt:componentFolder] where localname() = 'components'", Query.JCR_SQL2).execute();
+            NodeIterator ni = result.getNodes();
+            while (ni.hasNext()) {
+                try {
+                    JCRNodeWrapper next = (JCRNodeWrapper) ni.next();
+                    next.getRealNode().remove();
+                    jcrsession.save();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            log.info("...done removing live nodes");
         } finally {
+            logger.setLevel(previousLevel);
             JCRObservationManager.setEventsDisabled(Boolean.FALSE);
         }
 
         return null;
     }
+});
 
-    private void removeVersionable(JCRSessionWrapper jcrsession, String type) {
+JCRCallback<Object> callback = new JCRCallback<Object>() {
+    public Object doInJCR(JCRSessionWrapper jcrsession) throws RepositoryException {
+        log.info("Start removing versioning in workspace " + jcrsession.getWorkspace().getName());
+        JCRObservationManager.setEventsDisabled(Boolean.TRUE);
+        Logger logger = Logger.getLogger(ItemStateReferenceCache.class);
+        Level previousLevel = logger.getLevel();
+        logger.setLevel(Level.ERROR);
+
+        try {
+            scheduleJob(removeVersionable(jcrsession, "jnt:user"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:group"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:virtualsite"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:virtualsitesFolder"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:tag"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:tagList"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:category"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:roles"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:role"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:permission"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:usersFolder"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:groupsFolder"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:componentFolder"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:templateSets"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:portletDefinition"))
+            scheduleJob(removeVersionable(jcrsession, "jnt:portletDefinitions"))
+            removeMixin(jcrsession, "jnt:component", "jmix:lastPublished");
+        } finally {
+            logger.setLevel(previousLevel);
+            JCRObservationManager.setEventsDisabled(Boolean.FALSE);
+        }
+        log.info("...done removing versioning in workspace " + jcrsession.getWorkspace().getName());
+        
+        return null;
+    }
+
+    private Set<String> removeVersionable(JCRSessionWrapper jcrsession, String type) {
+        return removeMixin(jcrsession, type, "mix:versionable");
+    }
+    
+    private Set<String> removeMixin(JCRSessionWrapper jcrsession, String type, String mixin) {
+        log.info("Start processing node type " + type + " and removing mixin " + mixin);
+        Set<String> uuids = new HashSet<String>();
         QueryResult result = jcrsession.getWorkspace().getQueryManager().createQuery("select * from ["+ type +"]", Query.JCR_SQL2).execute();
         NodeIterator ni = result.getNodes();
+        int count = 0;
         while (ni.hasNext()) {
             try {
                 JCRNodeWrapper next = (JCRNodeWrapper) ni.next();
-                next.getRealNode().addMixin("mix:versionable");
+                next.getRealNode().addMixin(mixin);
                 jcrsession.save();
+                count++;
+                if (count > 0 && count % 100 == 0) {
+                    log.info("Preprocessed " + count + " nodes");
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -49,41 +112,43 @@ JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Object>() {
 
         result = jcrsession.getWorkspace().getQueryManager().createQuery("select * from ["+ type +"]", Query.JCR_SQL2).execute();
         ni = result.getNodes();
+        count = 0;
         while (ni.hasNext()) {
             JCRNodeWrapper next = (JCRNodeWrapper) ni.next();
-            next.getRealNode().removeMixin("mix:versionable");
-        }
-        jcrsession.save();
-    }
-});
-
-JCRTemplate.getInstance().doExecuteWithSystemSession(null, "live", new JCRCallback<Object>() {
-    public Object doInJCR(JCRSessionWrapper jcrsession) throws RepositoryException {
-        JCRObservationManager.setEventsDisabled(Boolean.TRUE);
-
-        try {
-            removeLive(jcrsession, "jnt:componentFolder")
-            removeLive(jcrsession, "jnt:component")
-        } finally {
-            JCRObservationManager.setEventsDisabled(Boolean.FALSE);
-        }
-
-        return null;
-    }
-
-    private void removeLive(JCRSessionWrapper jcrsession, String type) {
-        QueryResult result = jcrsession.getWorkspace().getQueryManager().createQuery("select * from ["+ type +"]", Query.JCR_SQL2).execute();
-        NodeIterator ni = result.getNodes();
-        while (ni.hasNext()) {
-            try {
-                JCRNodeWrapper next = (JCRNodeWrapper) ni.next();
-                next.getRealNode().remove();
-                jcrsession.save();
-            } catch (Exception e) {
-                e.printStackTrace();
+            next.getRealNode().removeMixin(mixin);
+            uuids.add(next.getIdentifier());
+            count++;
+            if (count > 0 && count % 100 == 0) {
+                log.info("Processed " + count + " nodes");
             }
         }
+        log.info("Saving changes for " + count + " nodes");
+        jcrsession.save();
+        
+        log.info("..done processing nodes for type " + type);
+        
+        return uuids;
+    }
+    
+    private void scheduleJob(Set<String> ids) {
+        if (ids.size() == 0) {
+            return;
+        }
+        JobDetail jobDetail = BackgroundJob.createJahiaJob("Node version history purge", NodeVersionHistoryJob.class);
+        jobDetail.setGroup("Maintenance");
+        JobDataMap jobDataMap = jobDetail.getJobDataMap();
+        jobDataMap.put(NodeVersionHistoryJob.JOB_NODE_IDS, ids);
+
+        log.info("Scheduling node version history purge job for " + ids.size() + " nodes");
+
+        try {
+            ServicesRegistry.getInstance().getSchedulerService().scheduleJobNow(jobDetail);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
     }
 
-});
+}
 
+JCRTemplate.getInstance().doExecuteWithSystemSession(callback);
+JCRTemplate.getInstance().doExecuteWithSystemSession(null, "live", callback);

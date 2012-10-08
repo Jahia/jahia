9aba10a7522959e4c72c333a15f7593122ce358d
Quentin Lamérand <qlamerand@jahia.com>
2012-10-08 16:53:11 +0000

    BACKLOG-3263: Use current locale instead of ui for validation messages in /cms context
    git-svn-id: svn://localhost/jahia/trunk@18092 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/multilang/CurrentLocaleResolver.java
+++ core/src/main/java/org/jahia/services/multilang/CurrentLocaleResolver.java
@@ -48,13 +48,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Locale;
 
-public class UILocaleResolver implements LocaleResolver {
+public class CurrentLocaleResolver implements LocaleResolver {
     public Locale resolveLocale(HttpServletRequest request) {
         HttpSession session = request.getSession();
         if (session != null) {
-            Locale uiLocale = (Locale) session.getAttribute(ProcessingContext.SESSION_UI_LOCALE);
-            if (uiLocale != null) {
-                return uiLocale;
+            Locale currentLocale = (Locale) session.getAttribute(ProcessingContext.SESSION_LOCALE);
+            if (currentLocale != null) {
+                return currentLocale;
             }
         }
         return request.getLocale();

293a0e837786b1b720128583dc0029f2773da269
Sergiy Shyrkov <sshyrkov@jahia.com>
2012-11-15 15:07:09 +0000

    Removed superfluous dot
    git-svn-id: svn://localhost/jahia/trunk@18448 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/templates/TemplatePackageRegistry.java
+++ core/src/main/java/org/jahia/services/templates/TemplatePackageRegistry.java
@@ -434,7 +434,7 @@ class TemplatePackageRegistry {
             }
         }
         if (!templatePackage.isDefault() && fileNameRegistry.containsKey("default")) {
-            	templatePackage.getResourceBundleHierarchy().add(MODULES_ROOT_PATH + ".default." + fileNameRegistry.get("default").getVersion().toString().replaceAll("\\.","___") + ".resources.DefaultJahiaTemplates");
+            	templatePackage.getResourceBundleHierarchy().add(MODULES_ROOT_PATH + "default." + fileNameRegistry.get("default").getVersion().toString().replaceAll("\\.","___") + ".resources.DefaultJahiaTemplates");
             templatePackage.getResourceBundleHierarchy().add("JahiaTypesResources");
             templatePackage.getResourceBundleHierarchy().add("JahiaInternalResources");
         } else {

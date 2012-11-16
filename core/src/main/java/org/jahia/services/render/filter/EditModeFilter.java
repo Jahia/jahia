67fc13950ae220ad6034c72064440be8150f9c49
Thomas Draier <tdraier@jahia.com>
2012-11-16 15:04:19 +0000

    handle links in edit mode ( BACKLOG-3299 )
    git-svn-id: svn://localhost/jahia/trunk@18468 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/filter/EditModeFilter.java
+++ core/src/main/java/org/jahia/services/render/filter/EditModeFilter.java
@@ -1,12 +1,38 @@
 package org.jahia.services.render.filter;
 
 
+import net.htmlparser.jericho.OutputDocument;
+import net.htmlparser.jericho.Source;
+import net.htmlparser.jericho.StartTag;
+import org.jahia.ajax.gwt.client.widget.edit.mainarea.ListModule;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.URLGenerator;
 import org.jahia.utils.StringResponseWrapper;
 
+import java.util.List;
+
 public class EditModeFilter extends AbstractFilter {
+
+    @Override
+    public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
+        String out = super.execute(previousOut, renderContext, resource, chain);
+        if (renderContext.getServletPath().endsWith("frame")) {
+            Source source = new Source(out);
+            OutputDocument document = new OutputDocument(source);
+            List<StartTag> tags = source.getAllStartTags("a");
+            for (StartTag tag : tags) {
+                String href = tag.getAttributeValue("href");
+                if (href != null && ((href.startsWith("/") && !href.startsWith(renderContext.getServletPath())) || href.contains("://"))) {
+                    if (tag.getAttributeValue("target") == null) {
+                        document.insert(tag.getEnd()-1, " target=\"_parent\"");
+                    }
+                }
+            }
+            return document.toString();
+        }
+        return out;
+    }
 
     @Override
     public String prepare(RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {

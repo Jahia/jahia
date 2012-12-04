11fb9c8fb2387d53ef94610f20cc9608ccd49a88
Quentin Lamérand <qlamerand@jahia.com>
2012-12-04 10:10:11 +0000

    BACKLOG-3459: Fix node validation tests + add test for translation of validation messages
    git-svn-id: svn://localhost/jahia/trunk@18633 69c369ed-ed09-0410-a4a1-e19a46c23626

--- test/jahia-test-module/src/main/java/org/jahia/services/validation/ValidationTest.java
+++ test/jahia-test-module/src/main/java/org/jahia/services/validation/ValidationTest.java
@@ -41,15 +41,16 @@
 package org.jahia.services.validation;
 
 import org.jahia.api.Constants;
-import org.jahia.services.content.JCRNodeWrapper;
-import org.jahia.services.content.JCRSessionFactory;
-import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.services.content.*;
 import org.jahia.test.TestHelper;
 import org.junit.*;
 import org.slf4j.Logger;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 import javax.jcr.nodetype.ConstraintViolationException;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 public class ValidationTest {
@@ -88,71 +89,82 @@ public class ValidationTest {
 
     @Test
     public void testNotNullConstraint() throws Exception {
-        String contraintViolationMessage = null;
+        List<ConstraintViolationException> errors = null;
         try {
-            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE, Locale.ENGLISH);
+            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE);
             JCRNodeWrapper testNotNull = session.getNode(SITE_CONTENT_ROOT_NODE).addNode("testNotNull", TEST_NODE_TYPE);
             session.save();
-        } catch (ConstraintViolationException e) {
-            contraintViolationMessage = e.getMessage();
+        } catch (CompositeConstraintViolationException e) {
+            errors = e.getErrors();
         }
-        Assert.assertEquals("Field not null: may not be null", contraintViolationMessage);
+        Assert.assertEquals("/sites/validationTest/testNotNull Field not null: may not be null",
+                errors.get(0).getLocalizedMessage());
     }
 
     @Test
     public void testSizeBetween6And20Constraint() throws Exception {
-        String contraintViolationMessage = null;
+        List<ConstraintViolationException> errors = null;
         try {
-            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE, Locale.ENGLISH);
+            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE);
             JCRNodeWrapper testSizeBetween6And20 = session.getNode(SITE_CONTENT_ROOT_NODE).addNode("testSizeBetween6And20", TEST_NODE_TYPE);
             testSizeBetween6And20.setProperty("test:notNull", "something");
             testSizeBetween6And20.setProperty("test:sizeBetween6And20", "abc");
             session.save();
-        } catch (ConstraintViolationException e) {
-            contraintViolationMessage = e.getMessage();
+        } catch (CompositeConstraintViolationException e) {
+            errors = e.getErrors();
         }
-        Assert.assertEquals("Field with size between 6 and 20: size must be between 6 and 20", contraintViolationMessage);
+        Assert.assertEquals("/sites/validationTest/testSizeBetween6And20 Field with size between 6 and 20: size must be between 6 and 20",
+                errors.get(0).getLocalizedMessage());
     }
 
     @Test
     public void testFieldMatchCustomConstraint() throws Exception {
-        String contraintViolationMessage = null;
+        List<ConstraintViolationException> errors = null;
         try {
-            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE, Locale.ENGLISH);
+            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE);
             JCRNodeWrapper testFieldMatch = session.getNode(SITE_CONTENT_ROOT_NODE).addNode("testFieldMatch", TEST_NODE_TYPE);
             testFieldMatch.setProperty("test:notNull", "something");
             testFieldMatch.setProperty("test:sizeBetween6And20", "abcdefghijklm");
             testFieldMatch.setProperty("test:email", "mail@one.com");
             testFieldMatch.setProperty("test:confirmEmail", "mail@two.com");
             session.save();
-        } catch (ConstraintViolationException e) {
-            contraintViolationMessage = e.getMessage();
+        } catch (CompositeConstraintViolationException e) {
+            errors = e.getErrors();
         }
-        Assert.assertEquals("Fields test_email and test_confirmEmail don't match", contraintViolationMessage);
+        Assert.assertEquals("/sites/validationTest/testFieldMatch Confirmation email field: Fields don't match",
+                errors.get(0).getLocalizedMessage());
     }
 
     @Test
     public void testEmailConstraint() throws Exception {
-        String contraintViolationMessage = null;
+        List<ConstraintViolationException> errors = null;
         try {
-            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE, Locale.ENGLISH);
+            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE);
             JCRNodeWrapper testEmail = session.getNode(SITE_CONTENT_ROOT_NODE).addNode("testEmail", TEST_NODE_TYPE);
             testEmail.setProperty("test:notNull", "something");
             testEmail.setProperty("test:sizeBetween6And20", "abcdefghijklm");
             testEmail.setProperty("test:email", "wrongmail");
             testEmail.setProperty("test:confirmEmail", "wrongmail");
             session.save();
-        } catch (ConstraintViolationException e) {
-            contraintViolationMessage = e.getMessage();
+        } catch (CompositeConstraintViolationException e) {
+            errors = e.getErrors();
         }
-        Assert.assertEquals("Email field: not a well-formed email address", contraintViolationMessage);
+        Assert.assertEquals(2, errors.size());
+        List<String> messages = Arrays.asList(
+                "/sites/validationTest/testEmail Email field: not a well-formed email address",
+                "/sites/validationTest/testEmail Confirmation email field: not a well-formed email address"
+        );
+        for (ConstraintViolationException error: errors) {
+            String localizedMessage = error.getLocalizedMessage();
+            Assert.assertTrue(messages.contains(localizedMessage));
+        }
     }
 
     @Test
     public void testFutureDateConstraint() throws Exception {
-        String contraintViolationMessage = null;
+        List<ConstraintViolationException> errors = null;
         try {
-            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE, Locale.ENGLISH);
+            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE);
             JCRNodeWrapper testFutureDate = session.getNode(SITE_CONTENT_ROOT_NODE).addNode("testFutureDate", TEST_NODE_TYPE);
             testFutureDate.setProperty("test:notNull", "something");
             testFutureDate.setProperty("test:sizeBetween6And20", "abcdefghijklm");
@@ -160,17 +172,18 @@ public class ValidationTest {
             testFutureDate.setProperty("test:confirmEmail", "good@mail.com");
             testFutureDate.setProperty("test:futureDate", Calendar.getInstance());
             session.save();
-        } catch (ConstraintViolationException e) {
-            contraintViolationMessage = e.getMessage();
+        } catch (CompositeConstraintViolationException e) {
+            errors = e.getErrors();
         }
-        Assert.assertEquals("Future date field: must be in the future", contraintViolationMessage);
+        Assert.assertEquals("/sites/validationTest/testFutureDate Future date field: must be in the future",
+                errors.get(0).getLocalizedMessage());
     }
 
     @Test
     public void testGreaterThan2Constraint() throws Exception {
-        String contraintViolationMessage = null;
+        List<ConstraintViolationException> errors = null;
         try {
-            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE, Locale.ENGLISH);
+            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE);
             JCRNodeWrapper testGreaterThan2 = session.getNode(SITE_CONTENT_ROOT_NODE).addNode("testGreaterThan2", TEST_NODE_TYPE);
             testGreaterThan2.setProperty("test:notNull", "something");
             testGreaterThan2.setProperty("test:sizeBetween6And20", "abcdefghijklm");
@@ -181,10 +194,43 @@ public class ValidationTest {
             testGreaterThan2.setProperty("test:futureDate", date);
             testGreaterThan2.setProperty("test:greaterThan2", 2);
             session.save();
-        } catch (ConstraintViolationException e) {
-            contraintViolationMessage = e.getMessage();
+        } catch (CompositeConstraintViolationException e) {
+            errors = e.getErrors();
         }
-        Assert.assertEquals("Field with value greater than 2: must be greater than or equal to 3", contraintViolationMessage);
+        Assert.assertEquals("/sites/validationTest/testGreaterThan2 Field with value greater than 2: must be greater than or equal to 3",
+                errors.get(0).getLocalizedMessage());
+    }
+
+    @Test
+    public void testMessageTranslation() throws Exception {
+        List<ConstraintViolationException> errors = null;
+        try {
+            JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE);
+            JCRNodeWrapper testGreaterThan2 = session.getNode(SITE_CONTENT_ROOT_NODE).addNode("testTranslation", TEST_NODE_TYPE);
+            testGreaterThan2.setProperty("test:sizeBetween6And20", "abc");
+            testGreaterThan2.setProperty("test:email", "badmail.com");
+            testGreaterThan2.setProperty("test:confirmEmail", "badmail2.com");
+            testGreaterThan2.setProperty("test:futureDate", Calendar.getInstance());
+            testGreaterThan2.setProperty("test:greaterThan2", 2);
+            LocaleContextHolder.setLocale(Locale.FRENCH, true);
+            session.save();
+        } catch (CompositeConstraintViolationException e) {
+            errors = e.getErrors();
+        }
+        Assert.assertEquals(7, errors.size());
+        List<String> messages = Arrays.asList(
+                "/sites/validationTest/testTranslation Champ non nul: ne peut pas être nul",
+                "/sites/validationTest/testTranslation Champ dont la taille est comprise entre 6 et 20: la taille doit être entre 6 et 20",
+                "/sites/validationTest/testTranslation Champ de confirmation d'e-mail: Les champs ne correspondent pas",
+                "/sites/validationTest/testTranslation Champ d'e-mail: Adresse email mal formée",
+                "/sites/validationTest/testTranslation Champ de confirmation d'e-mail: Adresse email mal formée",
+                "/sites/validationTest/testTranslation Champ de date dans le futur: doit être dans le futur",
+                "/sites/validationTest/testTranslation Champ avec valeur strictement supérieure à 2: doit être plus grand que 3"
+        );
+        for (ConstraintViolationException error: errors) {
+            String localizedMessage = error.getLocalizedMessage();
+            Assert.assertTrue(messages.contains(localizedMessage));
+        }
     }
 
     @Test

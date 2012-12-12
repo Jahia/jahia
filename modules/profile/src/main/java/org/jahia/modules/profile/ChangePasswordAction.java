b3f77c46b22fcabbfdffadb8d37608d97fb08420
Sergiy Shyrkov <sshyrkov@jahia.com>
2012-12-12 12:42:00 +0000

    BACKLOG-3302 - Optimize RB lookup https://jira.jahia.org/browse/BACKLOG-3302
    Replaced deprecated JahiaresourceBundle API with new one.
    
    
    git-svn-id: svn://localhost/jahia/trunk@18846 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/profile/src/main/java/org/jahia/modules/profile/ChangePasswordAction.java
+++ modules/profile/src/main/java/org/jahia/modules/profile/ChangePasswordAction.java
@@ -44,7 +44,6 @@ import org.jahia.bin.Action;
 import org.jahia.bin.ActionResult;
 import org.jahia.engines.EngineMessage;
 import org.jahia.engines.EngineMessages;
-import org.jahia.params.ParamBean;
 import org.jahia.registries.ServicesRegistry;
 import org.jahia.services.content.JCRSessionWrapper;
 import org.jahia.services.pwdpolicy.JahiaPasswordPolicyService;
@@ -53,12 +52,11 @@ import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.URLResolver;
 import org.jahia.services.usermanager.JahiaUser;
-import org.jahia.utils.i18n.JahiaResourceBundle;
+import org.jahia.utils.i18n.Messages;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.text.MessageFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -76,12 +74,12 @@ public class ChangePasswordAction extends Action {
 
         if ("".equals(passwd)) {
 
-            String userMessage = JahiaResourceBundle.getJahiaInternalResource("org.jahia.admin.userMessage.specifyPassword.label", renderContext.getUILocale());
+            String userMessage = Messages.getInternal("org.jahia.admin.userMessage.specifyPassword.label", renderContext.getUILocale());
             json.put("errorMessage", userMessage);
         } else {
             String passwdConfirm = req.getParameter("passwordconfirm").trim();
             if (!passwdConfirm.equals(passwd)) {
-                String userMessage = JahiaResourceBundle.getJahiaInternalResource("org.jahia.admin.userMessage.passwdNotMatch.label", renderContext.getUILocale());
+                String userMessage = Messages.getInternal("org.jahia.admin.userMessage.passwdNotMatch.label", renderContext.getUILocale());
                 json.put("errorMessage",userMessage);
             } else {
                 JahiaPasswordPolicyService pwdPolicyService = ServicesRegistry.getInstance().getJahiaPasswordPolicyService();
@@ -92,13 +90,13 @@ public class ChangePasswordAction extends Action {
                     EngineMessages policyMsgs = evalResult.getEngineMessages();
                     String res = "";
                     for (EngineMessage message : policyMsgs.getMessages()) {
-                        res += (message.isResource() ? MessageFormat.format(JahiaResourceBundle.getJahiaInternalResource(message.getKey(), renderContext.getUILocale()), message.getValues()) : message.getKey())+"\n";
+                        res += (message.isResource() ? Messages.format(Messages.getInternal(message.getKey(), renderContext.getUILocale()), message.getValues()) : message.getKey())+"\n";
                     }
                     json.put("errorMessage", res);
                 } else {
                     // change password
                     user.setPassword(passwd);
-                    json.put("errorMessage", JahiaResourceBundle.getJahiaInternalResource("org.jahia.admin.userMessage.passwordChanged.label", renderContext.getUILocale()));
+                    json.put("errorMessage", Messages.getInternal("org.jahia.admin.userMessage.passwordChanged.label", renderContext.getUILocale()));
                     json.put("result", "success");
                 }
             }

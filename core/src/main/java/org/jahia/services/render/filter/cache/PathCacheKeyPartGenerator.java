a277bc8d6fcc5b5c8642bdbc45240694cc0de40e
Sergiy Shyrkov <sshyrkov@jahia.com>
2012-12-19 16:21:59 +0000

    Cosmetics: use StringUtils.replace()
    git-svn-id: svn://localhost/jahia/trunk@18949 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/filter/cache/PathCacheKeyPartGenerator.java
+++ core/src/main/java/org/jahia/services/render/filter/cache/PathCacheKeyPartGenerator.java
@@ -40,15 +40,14 @@
 
 package org.jahia.services.render.filter.cache;
 
+import org.codehaus.plexus.util.StringUtils;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.regex.Pattern;
 
 public class PathCacheKeyPartGenerator implements CacheKeyPartGenerator {
     public static final String MAIN_RESOURCE_KEY = "_mr_";
-    public static final Pattern MAIN_RESOURCE_PATTERN = Pattern.compile(MAIN_RESOURCE_KEY);
 
     @Override
     public String getKey() {
@@ -66,12 +65,12 @@ public class PathCacheKeyPartGenerator implements CacheKeyPartGenerator {
     }
 
     public String getPath(String key) {
-        return MAIN_RESOURCE_PATTERN.matcher(key).replaceAll("");
+        return StringUtils.replace(key, MAIN_RESOURCE_KEY, "");
     }
 
     @Override
     public String replacePlaceholders(RenderContext renderContext, String keyPart) {
-        return MAIN_RESOURCE_PATTERN.matcher(keyPart).replaceAll(
+        return StringUtils.replace(keyPart, MAIN_RESOURCE_KEY,
                 renderContext.getMainResource().getNode().getCanonicalPath() + renderContext.getMainResource().getResolvedTemplate());
     }
 }

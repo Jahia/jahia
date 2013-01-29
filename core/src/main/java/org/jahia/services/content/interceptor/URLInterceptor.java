329ee65e04837457dbf05c3c3771a2db27410c6b
bamboo <bamboo@jahia.com>
2013-01-29 07:49:39 +0000

    QA-4049, SOC-199: {servername:localhost} in front of image url, when it is systemsite  (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 44525)
    git-svn-id: svn://localhost/jahia/trunk@19291 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/content/interceptor/URLInterceptor.java
+++ core/src/main/java/org/jahia/services/content/interceptor/URLInterceptor.java
@@ -531,7 +531,7 @@ public class URLInterceptor extends BaseInterceptor implements InitializingBean 
                     if (site != null) {
                         JCRSiteNode currentSite = parent.getResolveSite();
                         String serverName = site.getServerName();
-                        if (currentSite != null && !currentSite.getServerName().equals(serverName)) {
+                        if (currentSite != null && !currentSite.getServerName().equals(serverName) && !site.getSiteKey().equals("systemsite")) {
                             serverUrl = "{server:" + serverName + "}";
                         }
                     }

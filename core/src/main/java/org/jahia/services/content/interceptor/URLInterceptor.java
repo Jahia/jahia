9c08daa8b6f1bcb5924f91f95cfcb84b03cd752d
Quentin Lamérand <qlamerand@jahia.com>
2013-02-11 16:35:51 +0000

    QA-4067 + QA-4049: Use better method to check if a server name is localhost
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-6-X-X-BRANCH@19484 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/content/interceptor/URLInterceptor.java
+++ core/src/main/java/org/jahia/services/content/interceptor/URLInterceptor.java
@@ -518,7 +518,7 @@ public class URLInterceptor extends BaseInterceptor implements InitializingBean 
                     if (site != null) {
                         JCRSiteNode currentSite = parent.getResolveSite();
                         String serverName = site.getServerName();
-                        if (!"localhost".equals(serverName) && serverName != null && currentSite != null && !serverName.equals(currentSite.getServerName())) {
+                        if (!Url.isLocalhost(serverName) && serverName != null && currentSite != null && !serverName.equals(currentSite.getServerName())) {
                             serverPlaceHolder = "{server:" + serverName + "}";
                         }
                     }

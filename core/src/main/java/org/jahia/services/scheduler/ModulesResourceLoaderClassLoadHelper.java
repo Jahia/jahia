c9332252ba7ae4bed869f25098f30d8426ec7a25
Sergiy Shyrkov <sshyrkov@jahia.com>
2013-02-12 09:22:31 +0000

    Use ConcurrentHashMap instead of a HashMap. Get service instance once.
    git-svn-id: svn://localhost/jahia/trunk@19496 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/scheduler/ModulesResourceLoaderClassLoadHelper.java
+++ core/src/main/java/org/jahia/services/scheduler/ModulesResourceLoaderClassLoadHelper.java
@@ -1,34 +1,47 @@
 package org.jahia.services.scheduler;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.jahia.data.templates.JahiaTemplatesPackage;
 import org.jahia.registries.ServicesRegistry;
 import org.jahia.services.templates.JahiaTemplateManagerService;
 import org.springframework.scheduling.quartz.ResourceLoaderClassLoadHelper;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class ModulesResourceLoaderClassLoadHelper extends ResourceLoaderClassLoadHelper {
-    private Map<String, String> moduleForClass = new HashMap<String, String>();
+    private Map<String, String> moduleForClass = new ConcurrentHashMap<String, String>();
+
+    private JahiaTemplateManagerService service;
+
+    private JahiaTemplateManagerService getService() {
+        if (service == null) {
+            service = ServicesRegistry.getInstance().getJahiaTemplateManagerService();
+        }
+
+        return service;
+    }
 
     @Override
-    public Class loadClass(String name) throws ClassNotFoundException {
-        JahiaTemplateManagerService templateManagerService = ServicesRegistry.getInstance().getJahiaTemplateManagerService();
-
-        if (moduleForClass.containsKey(name)) {
-            return templateManagerService.getTemplatePackageByFileName(moduleForClass.get(name)).getClassLoader().loadClass(name);
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        String module = moduleForClass.get(name);
+        if (module != null) {
+            JahiaTemplatesPackage pkg = getService().getTemplatePackageByFileName(module);
+            if (pkg != null && pkg.getClassLoader() != null) {
+                return pkg.getClassLoader().loadClass(name);
+            } else {
+                moduleForClass.remove(name);
+            }
         }
 
         try {
             return super.loadClass(name);
         } catch (ClassNotFoundException e) {
-            List<JahiaTemplatesPackage> packageList = templateManagerService.getAvailableTemplatePackages();
+            List<JahiaTemplatesPackage> packageList = getService().getAvailableTemplatePackages();
             for (JahiaTemplatesPackage aPackage : packageList) {
                 try {
                     if (aPackage.getClassLoader() != null) {
-                        Class clazz = aPackage.getClassLoader().loadClass(name);
+                        Class<?> clazz = aPackage.getClassLoader().loadClass(name);
                         if (clazz != null) {
                             moduleForClass.put(name, aPackage.getRootFolder());
                             return clazz;

bc964e5cc9269e01a6d88677fef857dcf8267585
Benjamin Papež <bpapez@jahia.com>
2013-02-19 20:03:50 +0000

    DOCSPACE-408 Implement a cache filter, for caching only fragments based on expiration, which can be used in other modes than live
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-6-X-X-BRANCH@19592 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ core/src/main/java/org/jahia/services/render/filter/cache/ExpiringCacheFilter.java
@@ -0,0 +1,72 @@
+/**
+ * This file is part of Jahia, next-generation open source CMS:
+ * Jahia's next-generation, open source CMS stems from a widely acknowledged vision
+ * of enterprise application convergence - web, search, document, social and portal -
+ * unified by the simplicity of web content management.
+ *
+ * For more information, please visit http://www.jahia.com.
+ *
+ * Copyright (C) 2002-2012 Jahia Solutions Group SA. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * As a special exception to the terms and conditions of version 2.0 of
+ * the GPL (or any later version), you may redistribute this Program in connection
+ * with Free/Libre and Open Source Software ("FLOSS") applications as described
+ * in Jahia's FLOSS exception. You should have received a copy of the text
+ * describing the FLOSS exception, and it is also available here:
+ * http://www.jahia.com/license
+ *
+ * Commercial and Supported Versions of the program (dual licensing):
+ * alternatively, commercial and supported versions of the program may be used
+ * in accordance with the terms and conditions contained in a separate
+ * written agreement between you and Jahia Solutions Group SA.
+ *
+ * If you are unsure which license is appropriate for your use,
+ * please contact the sales department at sales@jahia.com.
+ */
+
+package org.jahia.services.render.filter.cache;
+
+import java.util.Properties;
+
+import javax.jcr.RepositoryException;
+
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.Resource;
+
+/**
+ * Module content caching filter, which only caches the modules having an expiration set.
+ *
+ * @author bpapez
+ * @since JAHIA 6.6.1.5
+ */
+public class ExpiringCacheFilter extends AggregateCacheFilter {
+
+    @Override
+    protected boolean isCacheable(RenderContext renderContext,
+            Resource resource, String key, Properties properties,
+            boolean isInPrepare) throws RepositoryException {
+        return !isInPrepare ? getExpiration(renderContext, resource, properties) > 0
+                : super.isCacheable(renderContext, resource, key, properties,
+                        isInPrepare);
+    }
+
+    @Override
+    protected boolean useDependencies() {
+        return false;
+    }
+
+}

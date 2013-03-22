2f7d0cb3717c45f1fd81a14215d98a026ef8763a
bamboo <bamboo@jahia.com>
2013-03-22 13:01:41 +0000

    Improved the log on some cnd parsing error (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 45198)
    git-svn-id: svn://localhost/jahia/trunk@19964 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/content/nodetypes/JahiaCndReader.java
+++ core/src/main/java/org/jahia/services/content/nodetypes/JahiaCndReader.java
@@ -134,7 +134,7 @@ import java.util.*;
  */
 public class JahiaCndReader {
     private static Logger logger = org.slf4j.LoggerFactory.getLogger(JahiaCndReader.class);
-    
+
     protected String systemId;
     protected String filename;
 
@@ -177,10 +177,10 @@ public class JahiaCndReader {
         }
         return false;
     }
-    
+
     /**
      * Returns the parsed property type or <code>-1</code> if the type is not recognized.
-     * 
+     *
      * @param token
      *            the token to parse property type from
      * @return the parsed property type or <code>-1</code> if the type is not recognized
@@ -219,7 +219,7 @@ public class JahiaCndReader {
 
     /**
      * Returns the parsed selector type or <code>-1</code> if the selector is not recognized.
-     * 
+     *
      * @param token
      *            the token to parse property type from
      * @return the parsed selector type or <code>-1</code> if the selector is not recognized
@@ -307,7 +307,7 @@ public class JahiaCndReader {
                 nodeTypesList.add(ntd);
             } catch (ParseException e) {
                 logger.error(e.getMessage(), e);
-                nextToken();    
+                nextToken();
                 while (!currentTokenEquals(Lexer.BEGIN_NODE_TYPE_NAME) && !currentTokenEquals(Lexer.EOF)) {
                     nextToken();
                 }
@@ -330,7 +330,7 @@ public class JahiaCndReader {
         }
 
     }
-    
+
     /**
      * processes the namespace declaration
      *
@@ -641,7 +641,7 @@ public class JahiaCndReader {
                 } else {
                     lexer.fail("Invalid value for tokenizer " + currentToken);
                 }
-                
+
             } else if (currentTokenEquals(Lexer.SORTABLE)) {
                 // deprecated , use NOQUERYORDER
                 pdi.setQueryOrderable(true);
@@ -893,7 +893,7 @@ public class JahiaCndReader {
             } else if (currentTokenEquals(Lexer.PROTECTED)) {
                 ndi.setProtected(true);
             } else if (currentTokenEquals(Lexer.MULTIPLE) /*|| currentTokenEquals(Lexer.SNS)*/) {
-                ndi.setAllowsSameNameSiblings(true);                
+                ndi.setAllowsSameNameSiblings(true);
             } else if (currentTokenEquals(Lexer.COPY)) {
                 ndi.setOnParentVersion(OnParentVersionAction.COPY);
             } else if (currentTokenEquals(Lexer.VERSION)) {
@@ -1012,7 +1012,7 @@ public class JahiaCndReader {
     protected Name parseName(String name) throws ParseException {
         Name res = new Name(name, registry.getNamespaces());
         if (!StringUtils.isEmpty(res.getPrefix()) && res.getUri() == null) {
-            lexer.fail("Cannot parse name");
+            lexer.fail("Cannot parse name: " + name);
         }
         return res;
     }

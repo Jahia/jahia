d3f4c1761f5b4f49b6daf1c6d5b305fe01724781
Cédric Mailleux <cmailleux@jahia.com>
2013-05-06 13:32:01 +0000

    BACKLOG-3847
    Fix cache key issue, javadocs, and finally clauses
    
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-6-X-X-BRANCH@20614 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/filter/cache/KeyCompressor.java
+++ core/src/main/java/org/jahia/services/render/filter/cache/KeyCompressor.java
@@ -9,8 +9,18 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.zip.*;
 
+/**
+ * Class of utility method for compressing/decompressing cache key.
+ */
+
 public class KeyCompressor {
     protected transient static Logger logger = org.slf4j.LoggerFactory.getLogger(KeyCompressor.class);
+
+    /**
+     * Encode a cacheKey.
+     * @param inputString The key to encode
+     * @return the encoded key as a Base-64 url safe string
+     */
     public static String encodeKey(String inputString) {
         if (StringUtils.isEmpty(inputString)) {
             return inputString;
@@ -27,16 +37,18 @@ public class KeyCompressor {
             return Base64.encodeBase64URLSafeString(copy);
         } catch (UnsupportedEncodingException e) {
             logger.warn("Not able to encode dependency: " + inputString, e);
+        } finally {
+            compresser.end();
         }
 
         return inputString;
     }
 
     /**
-     * Decode facet filter URL parameter
+     * Decode a base-64 url safe cache key.
      *
-     * @param inputString enocded facet filter URL query parameter
-     * @return decoded facet filter parameter
+     * @param inputString encoded key.
+     * @return decoded key
      */
     public static String decodeKey(String inputString) {
         if (StringUtils.isEmpty(inputString)) {
@@ -45,9 +57,9 @@ public class KeyCompressor {
         byte[] input = Base64.decodeBase64(inputString);
         // Decompress the bytes
         StringBuilder outputString = new StringBuilder();
+        Inflater inf = new Inflater();
         try {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream(2048);
-            Inflater inf = new Inflater();
             byte[] buf = new byte[512];
             int len = input.length;
             int off = 0;
@@ -91,6 +103,8 @@ public class KeyCompressor {
             logger.warn("Not able to decode dependency: " + inputString, e);
         } catch (DataFormatException e) {
             logger.warn("Not able to decode dependency: " + inputString, e);
+        } finally {
+            inf.end();
         }
         return outputString.toString();
     }

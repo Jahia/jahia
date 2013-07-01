2780c5a512110967cd518d32c91f2bb179eb5151
David Griffon <dgriffon@jahia.com>
2013-07-01 07:25:05 +0000

    fix merge issue of commit 46549 ( QA-4330 )
    git-svn-id: svn://localhost/jahia/trunk@21331 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/filter/cache/DefaultCacheKeyGenerator.java
+++ core/src/main/java/org/jahia/services/render/filter/cache/DefaultCacheKeyGenerator.java
@@ -40,27 +40,12 @@
 
 package org.jahia.services.render.filter.cache;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang.StringUtils;
-<<<<<<< .working
-import org.apache.jackrabbit.util.Base64;
-=======
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
-import org.apache.jackrabbit.core.security.JahiaAccessManager;
-import org.jahia.api.Constants;
-import org.jahia.services.cache.ehcache.EhCacheProvider;
-import org.jahia.services.content.*;
->>>>>>> .merge-right.r46553
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
-import org.jahia.settings.SettingsBean;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 /**
@@ -74,40 +59,8 @@ public class DefaultCacheKeyGenerator implements CacheKeyGenerator {
     private List<CacheKeyPartGenerator> partGenerators;
     private List<String> fields;
 
-<<<<<<< .working
     public List<CacheKeyPartGenerator> getPartGenerators() {
         return partGenerators;
-=======
-    private static transient Logger logger = org.slf4j.LoggerFactory.getLogger(DefaultCacheKeyGenerator.class);
-
-    private static final Set<String> KNOWN_FIELDS = new LinkedHashSet<String>(Arrays.asList("workspace", "language",
-            "path", "template", "templateType", "acls", "context", "wrapped", "custom", "queryString",
-            "templateNodes", "resourceID", "inArea", "site","moduleParams"));
-    private static final String CACHE_NAME = "HTMLNodeUsersACLs";
-    private static final String PROPERTY_CACHE_NAME = "HTMLRequiredPermissionsCache";
-    public static final String PER_USER = "_perUser_";
-    private static final String MAIN_RESOURCE_KEY = "_mr_";
-    private List<String> fields = new ArrayList<String>(KNOWN_FIELDS);
-
-    private MessageFormat format = new MessageFormat("#{0}#{1}#{2}#{3}#{4}#{5}#{6}#{7}#{8}#{9}#{10}#{11}#{12}#{13}#{14}");
-
-    private JahiaGroupManagerService groupManagerService;
-    private JahiaUserManagerService userManagerService;
-    private Map<String, Set<JahiaGroup>> aclGroups = new LinkedHashMap<String, Set<JahiaGroup>>();
-    private final Object objForSync = new Object();
-    private EhCacheProvider cacheProvider;
-    private Cache cache;
-    private JCRTemplate template;
-    private Cache permissionCache;
-    public static Pattern mainResourcePattern = Pattern.compile(MAIN_RESOURCE_KEY);
-    public static Pattern perUserPattern = Pattern.compile(PER_USER);
-    private static final Pattern depAclsPattern = Pattern.compile("_depacl_");
-    private static final Pattern aclsPathPattern = Pattern.compile("_p_");
-    public static Pattern mainResourceAclPattern = Pattern.compile("mraclmr");
-
-    public void setGroupManagerService(JahiaGroupManagerService groupManagerService) {
-        this.groupManagerService = groupManagerService;
->>>>>>> .merge-right.r46553
     }
 
     public void setPartGenerators(List<CacheKeyPartGenerator> partGenerators) {
@@ -124,76 +77,8 @@ public class DefaultCacheKeyGenerator implements CacheKeyGenerator {
 
     private Object[] getArguments(Resource resource, RenderContext renderContext, Properties properties) {
         List<String> args = new LinkedList<String>();
-<<<<<<< .working
         for (CacheKeyPartGenerator generator : partGenerators) {
             args.add(generator.getValue(resource, renderContext, properties));
-=======
-        for (String field : fields) {
-            if ("workspace".equals(field)) {
-                args.add(encodeString(resource.getWorkspace()));
-            } else if ("language".equals(field)) {
-                args.add(encodeString(resource.getLocale().toString()));
-            } else {
-                if ("path".equals(field)) {
-                    StringBuilder s = new StringBuilder(resource.getNode().getPath());
-                    if (Boolean.TRUE.equals(request.getAttribute("cache.mainResource"))) {
-                        s.append(MAIN_RESOURCE_KEY);
-                    }
-                    args.add(encodeString(s.toString()));
-                } else if ("template".equals(field)) {
-                    if (resource.getContextConfiguration().equals("page") && resource.getNode().getPath().equals(
-                            renderContext.getMainResource().getNode().getPath())) {
-                        args.add(encodeString(renderContext.getMainResource().getResolvedTemplate()));
-                    } else {
-                        args.add(encodeString(resource.getResolvedTemplate()));
-                    }
-                } else if ("templateType".equals(field)) {
-                    String templateType = resource.getTemplateType();
-                    if (renderContext.isAjaxRequest()) {
-                        templateType += ".ajax";
-                    }
-                    args.add(encodeString(templateType));
-                } else if ("queryString".equals(field)) {
-                    String[] params = (String[]) request.getAttribute("cache.requestParameters");
-                    if (params != null && params.length > 0) {
-                        args.add(encodeString("_qs" + Arrays.toString(params) + "_"));
-                    } else {
-                        args.add("");
-                    }
-                } else if ("acls".equals(field)) {
-                    args.add(encodeString(appendAcls(resource, renderContext, true)));
-                } else if ("wrapped".equals(field)) {
-                    args.add(encodeString(String.valueOf(resource.hasWrapper())));
-                } else if ("context".equals(field)) {
-                    args.add(encodeString(String.valueOf(resource.getContextConfiguration())));
-                } else if ("custom".equals(field)) {
-                    args.add((request.getAttribute("module.cache.additional.key") != null ? encodeString(request.getAttribute("module.cache.additional.key").toString()) : ""));
-                } else if ("templateNodes".equals(field)) {
-                    final Template t = (Template) request.getAttribute("previousTemplate");
-                    args.add(encodeString(t != null ? t.serialize() : ""));
-                } else if ("resourceID".equals(field)) {
-                    try {
-                        args.add(encodeString(resource.getNode().getIdentifier()));
-                    } catch (RepositoryException e) {
-                        logger.error(e.getMessage(), e);
-                    }
-                } else if ("inArea".equals(field)) {
-                    Object inArea = request.getAttribute("inArea");
-                    args.add(encodeString(inArea != null ? inArea.toString() : ""));
-                } else if ("site".equals(field)) {
-                    // Todo : Do we need to find another way of getting the urlresolver ?
-                    URLResolver urlResolver = (URLResolver) renderContext.getRequest().getAttribute("urlResolver");
-                    args.add(encodeString(urlResolver == null ||
-                            urlResolver.getSiteKeyByServerName() == null ? new StringBuilder().append(
-                            renderContext.getSite().getSiteKey()).append(":").append("virtualhost").append(":").append(
-                            request.getParameter("jsite")).toString() : new StringBuilder().append(
-                            renderContext.getSite().getSiteKey()).append(":").append(request.getParameter(
-                            "jsite")).toString()));
-                } else if ("moduleParams".equals(field)) {
-                    args.add(encodeString(new JSONObject(resource.getModuleParams()).toString()).replaceAll("\"","'"));
-                }
-            }
->>>>>>> .merge-right.r46549
         }
         return args.toArray(new String[args.size()]);
     }

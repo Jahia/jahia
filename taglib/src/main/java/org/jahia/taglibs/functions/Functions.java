85152a0d49ca7b1e52701e8eb17ebf52520ebdc1
Sergiy Shyrkov <sshyrkov@jahia.com>
2013-07-24 12:57:51 +0000

    Don not fail in removeHtmlTags() if called with null. Skip processing if the passed value is empty.
    git-svn-id: svn://localhost/jahia/branches/JAHIA-6-6-X-X-BRANCH@21554 69c369ed-ed09-0410-a4a1-e19a46c23626

--- taglib/src/main/java/org/jahia/taglibs/functions/Functions.java
+++ taglib/src/main/java/org/jahia/taglibs/functions/Functions.java
@@ -251,6 +251,9 @@ public class Functions {
     }
 
     public static String removeHtmlTags(String value) {
+        if (value == null || value.length() == 0) {
+            return value;
+        }
         Source source = new Source(value);
         TextExtractor textExtractor = source.getTextExtractor();
         textExtractor.setExcludeNonHTMLElements(true);

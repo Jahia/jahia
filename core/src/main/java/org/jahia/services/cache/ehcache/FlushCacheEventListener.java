91149f33a9f99d22760f8af4b71a0d82b30fe7bd
bamboo <bamboo@jahia.com>
2013-07-29 18:54:45 +0000

    fix BACKLOG-4125
    Add cache listener to flush local dependencies 
    when receiving remote remove event (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 46842)
    
    git-svn-id: svn://localhost/jahia/trunk@21608 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ core/src/main/java/org/jahia/services/cache/ehcache/FlushCacheEventListener.java
@@ -0,0 +1,111 @@
+/**
+ * This file is part of Jahia, next-generation open source CMS:
+ * Jahia's next-generation, open source CMS stems from a widely acknowledged vision
+ * of enterprise application convergence - web, search, document, social and portal -
+ * unified by the simplicity of web content management.
+ *
+ * For more information, please visit http://www.jahia.com.
+ *
+ * Copyright (C) 2002-2012 Jahia Solutions Group SA. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * As a special exception to the terms and conditions of version 2.0 of
+ * the GPL (or any later version), you may redistribute this Program in connection
+ * with Free/Libre and Open Source Software ("FLOSS") applications as described
+ * in Jahia's FLOSS exception. You should have received a copy of the text
+ * describing the FLOSS exception, and it is also available here:
+ * http://www.jahia.com/license
+ *
+ * Commercial and Supported Versions of the program (dual licensing):
+ * alternatively, commercial and supported versions of the program may be used
+ * in accordance with the terms and conditions contained in a separate
+ * written agreement between you and Jahia Solutions Group SA.
+ *
+ * If you are unsure which license is appropriate for your use,
+ * please contact the sales department at sales@jahia.com.
+ */
+
+package org.jahia.services.cache.ehcache;
+
+import net.sf.ehcache.CacheException;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.event.CacheEventListener;
+import org.jahia.services.render.filter.cache.ModuleCacheProvider;
+import org.slf4j.Logger;
+
+import java.text.ParseException;
+import java.util.Properties;
+
+/**
+ * A very simple EHCache logging cache listeners that logs all the
+ * cache events to Jahia's logs.
+ *
+ * @author loom
+ *         Date: Sep 14, 2009
+ *         Time: 3:54:36 PM
+ */
+public class FlushCacheEventListener implements CacheEventListener {
+    private static Logger logger = org.slf4j.LoggerFactory.getLogger(FlushCacheEventListener.class);
+
+    public FlushCacheEventListener(Properties properties) {
+    }
+
+    public void notifyElementRemoved(Ehcache ehcache, Element element) throws CacheException {
+        if (logger.isDebugEnabled()) {
+            logger.debug(ehcache.getName() + ": Element " + element.getKey() + " remotely removed.");
+        }
+
+        ModuleCacheProvider instance = ModuleCacheProvider.getInstance();
+        try {
+            String path = instance.getKeyGenerator().getPath((String) element.getKey());
+            if (logger.isDebugEnabled()) {
+                logger.debug("Calling invalidation on path : "+path);
+            }
+            instance.invalidate(path);
+        } catch (ParseException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    public void notifyElementPut(Ehcache ehcache, Element element) throws CacheException {
+
+    }
+
+    public void notifyElementUpdated(Ehcache ehcache, Element element) throws CacheException {
+
+    }
+
+    public void notifyElementExpired(Ehcache ehcache, Element element) {
+
+    }
+
+    public void notifyElementEvicted(Ehcache ehcache, Element element) {
+
+    }
+
+    public void notifyRemoveAll(Ehcache ehcache) {
+
+    }
+
+    public void dispose() {
+
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
+}

dc07397e4e5c3c3ae8f2fe2d5bd29ced612c208b
bamboo <bamboo@jahia.com>
2013-08-24 13:46:59 +0000

    QA-4391 URLs with vanity URL mapping defined get jsite parameter appended
    Originally discovered on jahia.com and reported by Cl?\195?\169ment. 
    
    On some pages (e.g. https://www.jahia.com/fr/tech/jahia-xcm) the links (also to sub-pages) get "jsite" parameter appended, i.e.: 
    
    https://www.jahia.com/fr/tech/jahia-xcm?jsite=7d813234-6e72-412b-a534-45c73b7fb75f 
    
    Test case locally (latest 6.6.x.x nightly). 
    1) Imported ACME-SPACE 
    2) For the Activities page in French assign the Vanity URL "/fr/activities" 
    3) Publish the changes 
    4) Flush HTML output caches 
    5) Access the site (another browser) using: 
    http://127.0.0.1:8080/ 
    6) On the home page of ACME switch to "FR" 
    7) The link to Activities page is: 
    http://127.0.0.1:8080/cms/fr/activities?jsite=3cc14679-9d07-485d-9ae4-8acd0a2c1877 
    
    The issue is that in the SiteParameterAdder we check if the URL does not have /sites/ in it we append jsite URL parameter. 
    In my test case the value which comes to SiteParameterAdder is "/cms/render/live/fr/activities", so without /sites/
    
    This commit fixes the issue by only generating the jsite parameter if really needed, for example : 
    - If the same vanity URL is used on multiple sites, it will generate the jsite parameter for ALL urls EXCEPT the one for the default site (this actually won't work because of another bug, but should work once the other bug is corrected)
    - If a vanity URL is only used on one site, we never put the jsite parameter. (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 47108)
    
    git-svn-id: svn://localhost/jahia/trunk@21874 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/filter/SiteParameterAdder.java
+++ core/src/main/java/org/jahia/services/render/filter/SiteParameterAdder.java
@@ -40,22 +40,84 @@
 
 package org.jahia.services.render.filter;
 
-import org.jahia.services.render.RenderContext;
-import org.jahia.services.render.Resource;
-import org.jahia.services.render.URLGenerator;
+import org.apache.commons.lang.StringUtils;
+import org.jahia.exceptions.JahiaNotFoundException;
+import org.jahia.services.content.JCRContentUtils;
+import org.jahia.services.render.*;
 import org.jahia.services.render.filter.HtmlTagAttributeTraverser.HtmlTagAttributeVisitor;
+import org.jahia.services.seo.VanityUrl;
+import org.jahia.services.seo.jcr.VanityUrlService;
+import org.jahia.services.sites.JahiaSitesBaseService;
+import org.jahia.utils.Url;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
+import java.util.List;
+
+import static org.jahia.api.Constants.LIVE_WORKSPACE;
 
 public class SiteParameterAdder implements HtmlTagAttributeVisitor {
+
+    private transient static Logger logger = LoggerFactory
+            .getLogger(SiteParameterAdder.class);
+
+    public final static String SITE_URL_PARAMETER_NAME = "jsite";
+
+    private static final String DEFAULT_WORKSPACE = LIVE_WORKSPACE;
+
+    private VanityUrlService vanityUrlService;
+
+    public void setVanityUrlService(VanityUrlService vanityUrlService) {
+        this.vanityUrlService = vanityUrlService;
+    }
 
     public String visit(String value, RenderContext context, String tagName, String attrName, Resource resource) {
         URLGenerator urlGenerator = context.getURLGenerator();
         if (value != null && value.startsWith(urlGenerator.getContext() + urlGenerator.getBase())) {
-                if (!value.startsWith(urlGenerator.getContext() + urlGenerator.getBase() + "/sites/") && !value.contains("jsite=")) {
-                    String jsite = context.getRequest().getParameter("jsite");
+                if (!value.startsWith(urlGenerator.getContext() + urlGenerator.getBase() + "/sites/") && !value.contains(SITE_URL_PARAMETER_NAME + "=")) {
+                    String servletPath = context.getRequest().getContextPath().length() > 0 ? context
+                                                                .getRequest().getContextPath() + context.getServletPath() : context
+                                                                .getServletPath();
+                    if (StringUtils.isNotEmpty(value)
+                            && !Url.isLocalhost(context.getRequest().getServerName())
+                            && value.startsWith(servletPath)) {
+                        try {
+                            String pathInfo = value.substring(servletPath.length());
+                            String tempWorkspace = verifyWorkspace(StringUtils.substringBefore(pathInfo.substring(1), "/"));
+                            String tempPath = StringUtils.substringAfter(pathInfo.substring(1), "/");
+                            List<VanityUrl> vanityUrls = vanityUrlService
+                                    .findExistingVanityUrls("/" + tempPath,
+                                            StringUtils.EMPTY, tempWorkspace);
+
+                            VanityUrl resolvedVanityUrl = null;
+                            String siteKey = context.getSite().getSiteKey();
+                            for (VanityUrl vanityUrl : vanityUrls) {
+                                if (vanityUrl.isActive()
+                                        && (StringUtils.isEmpty(siteKey) || siteKey.equals(
+                                        vanityUrl.getSite()))) {
+                                    resolvedVanityUrl = vanityUrl;
+                                    break;
+                                }
+                            }
+                            if (resolvedVanityUrl != null) {
+                                if (vanityUrls.size() == 1) {
+                                    return value;
+                                } else {
+                                    // multiple vanity URLs exist for the same URL, we need to understand if we need to
+                                    // generate a site parameter or not.
+                                    if (JahiaSitesBaseService.getInstance().getDefaultSite().getSiteKey().equals(resolvedVanityUrl.getSite())) {
+                                        return value;
+                                    }
+                                }
+                            }
+                        } catch (RepositoryException e) {
+                            logger.error("Error trying to resolve vanity URL " + value, e);
+                        }
+                    }
+                    String jsite = context.getRequest().getParameter(SITE_URL_PARAMETER_NAME);
                     if (jsite == null) {
-                        jsite = (String) context.getMainResource().getModuleParams().get("jsite");
+                        jsite = (String) context.getMainResource().getModuleParams().get(SITE_URL_PARAMETER_NAME);
                     }
                     if (jsite == null) {
                         try {
@@ -64,11 +126,24 @@ public class SiteParameterAdder implements HtmlTagAttributeVisitor {
                             return value;
                         }
                     }
-                    value += (!value.contains("?") ? "?jsite=" : "&jsite=") + jsite;
+                    value += (!value.contains("?") ? "?" + SITE_URL_PARAMETER_NAME + "=" : "&" + SITE_URL_PARAMETER_NAME + "=") + jsite;
                 }
         }
 
         return value;
+    }
+
+    protected String verifyWorkspace(String workspace) {
+        if (StringUtils.isEmpty(workspace)) {
+            if (workspace == null) {
+                workspace = DEFAULT_WORKSPACE;
+            }
+        } else {
+            if (!JCRContentUtils.isValidWorkspace(workspace)) {
+                return DEFAULT_WORKSPACE;
+            }
+        }
+        return workspace;
     }
 
 }

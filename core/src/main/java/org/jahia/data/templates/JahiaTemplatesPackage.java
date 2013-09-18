a690ec28c560d5d92ee6e5a61c5e3816fae8c7ec
Sergiy Shyrkov <sshyrkov@jahia.com>
2013-09-18 13:13:57 +0000

    BACKLOG-4194 - Regressions raised by code review https://jira.jahia.org/browse/BACKLOG-4194
    JavaDoc: https://devtools.jahia.com/fisheye/cru/AND-168#c6319
    
    git-svn-id: svn://localhost/jahia/trunk@22079 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/data/templates/JahiaTemplatesPackage.java
+++ core/src/main/java/org/jahia/data/templates/JahiaTemplatesPackage.java
@@ -157,18 +157,40 @@ public class JahiaTemplatesPackage {
 
     private String forgeUrl;
 
+    /**
+     * Initializes an instance of this class.
+     * 
+     * @param bundle
+     *            the backing OSGi bundle for this module
+     */
     public JahiaTemplatesPackage(Bundle bundle) {
         this.bundle = bundle;
     }
 
+    /**
+     * Returns the backing OSGi bundle for this module.
+     * 
+     * @return the backing OSGi bundle for this module
+     */
     public Bundle getBundle() {
         return bundle;
     }
 
+    /**
+     * Retrieves the module state information.
+     * 
+     * @return the module state information
+     */
     public ModuleState getState() {
         return state;
     }
 
+    /**
+     * Sets the module state information.
+     * 
+     * @param state
+     *            the module state information
+     */
     public void setState(ModuleState state) {
         this.state = state;
     }
@@ -318,6 +340,7 @@ public class JahiaTemplatesPackage {
         depends.add(dep);
     }
 
+    @Override
     public String toString() {
         return getRootFolder();
     }
@@ -332,45 +355,97 @@ public class JahiaTemplatesPackage {
     }
 
 
+    /**
+     * Returns the description of this module is available.
+     * 
+     * @return the description of this module is available
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description for this module.
+     * 
+     * @param description
+     *            the description text
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Returns the name (path) of the resource bundle for this module.
+     * 
+     * @return the name (path) of the resource bundle for this module
+     */
     public String getResourceBundleName() {
         return resourceBundleName;
     }
 
+    /**
+     * Sets the resource bundle name (path) for this module.
+     * 
+     * @param resourceBundleName
+     *            the resource bundle name (path) for this module
+     */
     public void setResourceBundleName(String resourceBundleName) {
         this.resourceBundleName = resourceBundleName;
     }
 
+    /**
+     * Returns a list of content node definition files (CND) available in the module.
+     * 
+     * @return a list of content node definition files (CND) available in the module
+     */
     public List<String> getDefinitionsFiles() {
         return definitionsFile;
     }
 
+    /**
+     * Sets the list of content node definition files (CND) available in the module.
+     * 
+     * @param definitionFile
+     *            the list of content node definition files (CND) available in the module
+     */
     public void setDefinitionsFile(String definitionFile) {
         definitionsFile.add(definitionFile);
     }
 
+    /**
+     * Returns a list of rule files available in the module.
+     * 
+     * @return a list of rule files available in the module
+     */
     public List<String> getRulesFiles() {
         return rulesFiles;
     }
 
+    /**
+     * Sets a list of rule files available in the module.
+     * 
+     * @param rulesFile
+     *            a list of rule files available in the module
+     */
     public void setRulesFile(String rulesFile) {
         rulesFiles.add(rulesFile);
     }
 
+    /**
+     * Returns a list with the resource bundle lookup chain for this module.
+     * 
+     * @return a list with the resource bundle lookup chain for this module
+     */
     public List<String> getResourceBundleHierarchy() {
         return resourceBundleHierarchy;
     }
 
-   public void clearHierarchy() {
-        getResourceBundleHierarchy().clear();
-    }
+    /**
+     * Resets the resource bundle lookup hierarchy.
+     */
+    public void clearHierarchy() {
+            getResourceBundleHierarchy().clear();
+        }
 
     @Override
     public boolean equals(Object o) {
@@ -392,28 +467,50 @@ public class JahiaTemplatesPackage {
         return result;
     }
 
+    /**
+     * Returns a list with the rule DSL files for this module.
+     * 
+     * @return a list with the rule DSL files for this module
+     */
     public List<String> getRulesDescriptorFiles() {
         return rulesDescriptorFiles;
     }
 
+    /**
+     * Sets the list with the rule DSL files for this module.
+     * 
+     * @param rulesDescriptorFiles
+     *            a list with the rule DSL files for this module
+     */
     public void setRulesDescriptorFile(String rulesDescriptorFiles) {
         this.rulesDescriptorFiles.add(rulesDescriptorFiles);
     }
 
 
     /**
-     * @return the dependencies
+     * Returns a list of modules which this module depends on.
+     * 
+     * @return a list of modules which this module depends on
      */
     public List<JahiaTemplatesPackage> getDependencies() {
         return Collections.unmodifiableList(dependencies);
     }
 
+    /**
+     * Add the provided module into the list of dependencies for this one.
+     * 
+     * @param dep
+     *            a module to add as a dependency for this module
+     */
     public void addDependency(JahiaTemplatesPackage dep) {
         if (!dependencies.contains(dep)) {
             dependencies.add(dep);
         }
     }
 
+    /**
+     * Reset the module dependencies.
+     */
     public void resetDependencies() {
         dependencies.clear();
     }
@@ -434,76 +531,182 @@ public class JahiaTemplatesPackage {
         this.buildNumber = buildNumber;
     }
 
+    /**
+     * Returns the version information for this module.
+     * 
+     * @return the version information for this module
+     */
     public ModuleVersion getVersion() {
         return version;
     }
 
+    /**
+     * Sets the version information for this module.
+     * 
+     * @param version
+     *            the version information for this module
+     */
     public void setVersion(ModuleVersion version) {
         this.version = version;
     }
 
+    /**
+     * Returns the type of this module.
+     * 
+     * @return the type of this module
+     */
     public String getModuleType() {
         return moduleType;
     }
 
+    /**
+     * Sets the type of this module.
+     * 
+     * @param moduleType
+     *            the type of this module
+     */
     public void setModuleType(String moduleType) {
         this.moduleType = moduleType;
     }
 
+    /**
+     * Sets the value for the <code>autoDeployOnSite</code> directive which defines on which sites this module should be automatically
+     * installed.
+     * 
+     * @param autoDeployOnSite
+     *            the value for the <code>autoDeployOnSite</code> directive which defines on which sites this module should be automatically
+     *            installed
+     */
     public void setAutoDeployOnSite(String autoDeployOnSite) {
         this.autoDeployOnSite = autoDeployOnSite;
     }
 
+    /**
+     * Returns the value for the <code>autoDeployOnSite</code> directive which defines on which sites this module should be automatically
+     * installed.
+     * 
+     * @return the value for the <code>autoDeployOnSite</code> directive which defines on which sites this module should be automatically
+     *         installed
+     */
     public String getAutoDeployOnSite() {
         return autoDeployOnSite;
     }
 
+    /**
+     * Returns the Spring application context instance for this module. <code>null</code> in case this module has no application context
+     * provided.
+     * 
+     * @return the Spring application context instance for this module. <code>null</code> in case this module has no application context
+     *         provided
+     */
     public AbstractApplicationContext getContext() {
         return context;
     }
 
+    /**
+     * Sets the application context instance for this module.
+     * 
+     * @param context
+     *            the application context instance for this module
+     */
     public void setContext(AbstractApplicationContext context) {
         this.context = context;
         // reset services state
         serviceInitialized = false;
     }
 
+    /**
+     * Checks if this is the version of the module which is currently active.
+     * 
+     * @return <code>true</code> if this version of the module is currently active.
+     */
     public boolean isActiveVersion() {
         return isActiveVersion;
     }
 
+    /**
+     * Sets the active state of this module version.
+     * 
+     * @param activeVersion
+     *            the active state of this module version
+     */
     public void setActiveVersion(boolean activeVersion) {
         isActiveVersion = activeVersion;
     }
 
+    /**
+     * Checks if this is the latest available version of this module.
+     * 
+     * @return <code>true</code> if this is the latest version of this module
+     */
     public boolean isLastVersion() {
         return isLastVersion;
     }
 
+    /**
+     * Sets the flag for the latest available module version.
+     * 
+     * @param lastVersion
+     *            the flag for the latest available module version
+     */
     public void setLastVersion(boolean lastVersion) {
         isLastVersion = lastVersion;
     }
 
+    /**
+     * Returns an SCM URI for this module if the source control is available for it.
+     * 
+     * @return an SCM URI for this module if the source control is available for it
+     */
     public String getScmURI() {
         return scmURI;
     }
 
+    /**
+     * Sets the SCM URI for this module if the source control is available for it.
+     * 
+     * @param scmURI the SCM URI for this module if the source control is available for it
+     */
     public void setScmURI(String scmURI) {
         this.scmURI = scmURI;
     }
 
+    /**
+     * Returns the file descriptor, representing the folder with the sources for this module if available.
+     * 
+     * @return the file descriptor, representing the folder with the sources for this module if available
+     */
     public File getSourcesFolder() {
         return sourcesFolder;
     }
 
+    /**
+     * Sets the file descriptor, representing the folder with the sources for this module if available.
+     * 
+     * @param sourcesFolder
+     *            the file descriptor, representing the folder with the sources for this module if available
+     */
     public void setSourcesFolder(File sourcesFolder) {
         this.sourcesFolder = sourcesFolder;
     }
 
+    /**
+     * Returns an instance of the {@link SourceControlManagement} for this module.
+     * 
+     * @return an instance of the {@link SourceControlManagement} for this module
+     */
     public SourceControlManagement getSourceControl() {
         return sourceControl;
     }
 
+    /**
+     * Sets an instance of the source control management for this module.
+     * 
+     * @param sourceControl
+     *            an instance of the source control management for this module
+     * @throws IOException
+     *             in case of an exception when retrieving SCM URL
+     */
     public void setSourceControl(SourceControlManagement sourceControl) throws IOException {
         this.sourceControl = sourceControl;
         if (sourceControl != null) {
@@ -511,6 +714,14 @@ public class JahiaTemplatesPackage {
         }
     }
 
+    /**
+     * Returns a resource from the module's bundle for the specified path.
+     * 
+     * @param relativePath
+     *            a path of the bundle resource
+     * @return a resource from the module's bundle for the specified path
+     * @see Bundle#getEntry(String)
+     */
     public Resource getResource(String relativePath) {
         if (relativePath == null) {
             return null;
@@ -523,6 +734,14 @@ public class JahiaTemplatesPackage {
         }
     }
 
+    /**
+     * Returns resources from the module's bundle and its attached fragments for the specified path.
+     * 
+     * @param relativePath
+     *            a path of the bundle resource
+     * @return resources from the module's bundle and its attached fragments for the specified path
+     * @see Bundle#findEntries(String, String, boolean)
+     */
     public Resource[] getResources(String relativePath) {
         Enumeration<URL> resourceEnum = bundle.findEntries(relativePath, null, false);
         List<Resource> resources = new ArrayList<Resource>();
@@ -536,6 +755,11 @@ public class JahiaTemplatesPackage {
         return resources.toArray(new Resource[resources.size()]);
     }
 
+    /**
+     * Returns the module bundle's class loader instance.
+     * 
+     * @return the module bundle's class loader instance
+     */
     public ClassLoader getClassLoader() {
         if (classLoader == null && bundle != null) {
             classLoader = BundleUtils.createBundleClassLoader(bundle);
@@ -543,10 +767,23 @@ public class JahiaTemplatesPackage {
         return classLoader;
     }
 
+    /**
+     * Sets the class loader instance for this module
+     * 
+     * @param classLoader
+     *            a class loader instance for this module
+     */
     public void setClassLoader(ClassLoader classLoader) {
         this.classLoader = classLoader;
     }
 
+    /**
+     * Returns a class loader which is a chain of class loaders, starting from the Web application one, than this modules class loader and
+     * at the end the list of class loaders of modules, this module depends on.
+     * 
+     * @return a class loader which is a chain of class loaders, starting from the Web application one, than this modules class loader and
+     *         at the end the list of class loaders of modules, this module depends on
+     */
     public ClassLoader getChainedClassLoader() {
         final List<ClassLoader> classLoaders = new ArrayList<ClassLoader>();
         classLoaders.add(Jahia.class.getClassLoader());
@@ -584,6 +821,12 @@ public class JahiaTemplatesPackage {
         };
     }
     
+    /**
+     * Sets the root path for the module folder.
+     * 
+     * @param rootFolderPath
+     *            the root path for the module folder
+     */
     public void setRootFolderPath(String rootFolderPath) {
         this.rootFolderPath = rootFolderPath;
     }
@@ -608,18 +851,40 @@ public class JahiaTemplatesPackage {
         this.serviceInitialized = serviceInitialized;
     }
 
+    /**
+     * Sets the flag for downloadable sources.
+     * 
+     * @param sourcesDownloadable
+     *            value of the flag for downloadable sources
+     */
     public void setSourcesDownloadable(boolean sourcesDownloadable) {
         this.sourcesDownloadable = sourcesDownloadable;
     }
 
+    /**
+     * Checks if the sources for this module can be downloaded.
+     * 
+     * @return <code>true</code> if the sources for this module can be downloaded; <code>false</code> otherwise
+     */
     public boolean isSourcesDownloadable() {
         return sourcesDownloadable;
     }
 
+    /**
+     * Returns the URL of the corresponding Jahia forge.
+     * 
+     * @return the URL of the corresponding Jahia forge
+     */
     public String getForgeUrl() {
         return forgeUrl;
     }
 
+    /**
+     * Sets the URL of the corresponding Jahia forge.
+     * 
+     * @param forgeUrl
+     *            the URL of the corresponding Jahia forge
+     */
     public void setForgeUrl(String forgeUrl) {
         this.forgeUrl = forgeUrl;
     }

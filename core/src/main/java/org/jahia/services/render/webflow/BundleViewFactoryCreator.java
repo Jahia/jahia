d51646523406980b4b1638e46e8e9bb865b495d3
Sergiy Shyrkov <sshyrkov@jahia.com>
2013-11-15 10:30:43 +0000

    Removed unused field
    git-svn-id: svn://localhost/jahia/trunk@22670 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/webflow/BundleViewFactoryCreator.java
+++ core/src/main/java/org/jahia/services/render/webflow/BundleViewFactoryCreator.java
@@ -45,12 +45,10 @@ import org.jahia.services.templates.JahiaModuleAware;
 import org.springframework.binding.convert.ConversionService;
 import org.springframework.binding.expression.Expression;
 import org.springframework.binding.expression.ExpressionParser;
-import org.springframework.validation.MessageCodesResolver;
 import org.springframework.webflow.engine.builder.BinderConfiguration;
 import org.springframework.webflow.mvc.builder.MvcViewFactoryCreator;
 import org.springframework.webflow.mvc.view.AbstractMvcViewFactory;
 import org.springframework.webflow.mvc.view.FlowViewResolver;
-import org.springframework.webflow.validation.WebFlowMessageCodesResolver;
 
 /**
  * Creates factory view for bundle views
@@ -59,8 +57,6 @@ public class BundleViewFactoryCreator extends MvcViewFactoryCreator implements J
     private JahiaTemplatesPackage module;
 
     private FlowViewResolver flowViewResolver = new BundleFlowViewResolver();
-
-    private MessageCodesResolver messageCodesResolver = new WebFlowMessageCodesResolver();
 
     @Override
     public void setJahiaModule(JahiaTemplatesPackage module) {

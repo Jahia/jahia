bb48f8d702b73b839e9b85c19d00081f43e3ae1e
Sergiy Shyrkov <sshyrkov@jahia.com>
2013-11-21 11:32:35 +0000

    Use proper WebFlowMessageCodesResolver instance to prevent NPE during form bindings in Web Flow
    git-svn-id: svn://localhost/jahia/trunk@22705 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/webflow/BundleViewFactoryCreator.java
+++ core/src/main/java/org/jahia/services/render/webflow/BundleViewFactoryCreator.java
@@ -45,10 +45,12 @@ import org.jahia.services.templates.JahiaModuleAware;
 import org.springframework.binding.convert.ConversionService;
 import org.springframework.binding.expression.Expression;
 import org.springframework.binding.expression.ExpressionParser;
+import org.springframework.validation.MessageCodesResolver;
 import org.springframework.webflow.engine.builder.BinderConfiguration;
 import org.springframework.webflow.mvc.builder.MvcViewFactoryCreator;
 import org.springframework.webflow.mvc.view.AbstractMvcViewFactory;
 import org.springframework.webflow.mvc.view.FlowViewResolver;
+import org.springframework.webflow.validation.WebFlowMessageCodesResolver;
 
 /**
  * Creates factory view for bundle views
@@ -57,17 +59,20 @@ public class BundleViewFactoryCreator extends MvcViewFactoryCreator implements J
     private JahiaTemplatesPackage module;
 
     private FlowViewResolver flowViewResolver = new BundleFlowViewResolver();
+    
+    private MessageCodesResolver messageCodesResolver = new WebFlowMessageCodesResolver();
 
     @Override
     public void setJahiaModule(JahiaTemplatesPackage module) {
         this.module = module;
     }
 
+    @Override
     protected AbstractMvcViewFactory createMvcViewFactory(Expression viewId, ExpressionParser expressionParser,
                                                           ConversionService conversionService,
                                                           BinderConfiguration binderConfiguration) {
         return new BundleMvcViewFactory(viewId, flowViewResolver, expressionParser, conversionService,
-                binderConfiguration, null, module);
+                binderConfiguration, messageCodesResolver, module);
     }
 
 }

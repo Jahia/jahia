10444d30c5aa9d94190cf4123f7cd5706c99ed27
Sergiy Shyrkov <sshyrkov@jahia.com>
2014-01-16 21:42:53 +0000

    QA-5246 - Changed year to 2014 (copyright and file headers)
    git-svn-id: svn://localhost/jahia/trunk@24009 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/rolesmanager/src/main/java/org/jahia/modules/rolesmanager/RolesAndPermissionsHandler.java
+++ modules/rolesmanager/src/main/java/org/jahia/modules/rolesmanager/RolesAndPermissionsHandler.java
@@ -1,3 +1,23 @@
+/**
+ * This file is part of the Enterprise Jahia software.
+ *
+ * Jahia's next-generation, open source CMS stems from a widely acknowledged vision
+ * of enterprise application convergence - web, search, document, social and portal -
+ * unified by the simplicity of web content management.
+ *
+ * For more information, please visit http://www.jahia.com.
+ *
+ * Copyright (C) 2002-2014 Jahia Solutions Group SA. All rights reserved.
+ *
+ * This Enteprise Jahia software must be used in accordance with the terms contained in the
+ * Jahia Solutions Group Terms & Conditions as well as the
+ * Jahia Sustainable Enterprise License (JSEL). You may not use this software except
+ * in compliance with the Jahia Solutions Group Terms & Conditions and the JSEL.
+ * See the license for the rights, obligations and limitations governing use
+ * of the contents of the software. For questions regarding licensing, support, production usage,
+ * please contact our team at sales@jahia.com or go to: http://www.jahia.com/license
+ */
+
 package org.jahia.modules.rolesmanager;
 
 import org.apache.commons.lang.StringUtils;

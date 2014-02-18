506c65e96d3887bb3afef11a900fd0125a1b5f8e
bamboo <bamboo@jahia.com>
2014-02-18 19:47:03 +0000

    UNIL-64
    Display name ( title ) for file only not for folder
    For folder keep the old behavior (merged from jahia/branches/JAHIA-6-6-X-X-BRANCH revision 48758)
    
    git-svn-id: svn://localhost/jahia/trunk@23524 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/content/decorator/JCRFileNode.java
+++ core/src/main/java/org/jahia/services/content/decorator/JCRFileNode.java
@@ -40,20 +40,43 @@
 
 package org.jahia.services.content.decorator;
 
+import org.jahia.api.Constants;
 import org.jahia.services.content.JCRNodeWrapper;
+import org.slf4j.Logger;
 
 import javax.jcr.RepositoryException;
 
 /**
- * 
+ *
  * User: toto
  * Date: Dec 3, 2008
  * Time: 5:55:11 PM
- * 
+ *
  */
 public class JCRFileNode extends JCRNodeDecorator {
+    protected static final Logger logger = org.slf4j.LoggerFactory.getLogger(JCRFileNode.class);
+
     public JCRFileNode(JCRNodeWrapper node) throws RepositoryException {
         super(node);
     }
 
+    @Override
+    public String getDisplayableName() {
+        try {
+            if(isNodeType(Constants.NT_FOLDER)) {
+                return super.getDisplayableName();
+            }
+        } catch (RepositoryException e) {
+            logger.error(e.getMessage(), e);
+        }
+        String title = null;
+        String name = null;
+        try {
+            title = getProperty(Constants.JCR_TITLE).getValue().getString();
+        } catch (RepositoryException e) {
+            logger.debug("could not retrieve jcr:title of " + this.getPath());
+        }
+        name = getName();
+        return title != null ? name+" ("+title+")" : name;
+    }
 }

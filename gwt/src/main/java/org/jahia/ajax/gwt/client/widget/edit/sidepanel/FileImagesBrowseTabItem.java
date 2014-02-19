7cc7320feb45b7708b30eba47b97672f0cc5634f
David Griffon <dgriffon@jahia.com>
2014-02-19 16:54:12 +0000

    QA-5464 : allow to download files from content side panel - change view of items
    git-svn-id: svn://localhost/jahia/trunk@23547 69c369ed-ed09-0410-a4a1-e19a46c23626

--- gwt/src/main/java/org/jahia/ajax/gwt/client/widget/edit/sidepanel/FileImagesBrowseTabItem.java
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/widget/edit/sidepanel/FileImagesBrowseTabItem.java
@@ -186,60 +186,6 @@ class FileImagesBrowseTabItem extends BrowseTabItem {
         }
     }
 
-    public class ImageDragSource extends ListViewDragSource {
-        public ImageDragSource(ListView<GWTJahiaNode> listView) {
-            super(listView);
-            DragListener listener = new DragListener() {
-                public void dragEnd(DragEvent de) {
-                    DNDEvent e = new DNDEvent(ImageDragSource.this, de.getEvent());
-                    e.setData(data);
-                    e.setDragEvent(de);
-                    e.setComponent(component);
-                    e.setStatus(statusProxy);
-
-                    onDragEnd(e);
-                }
-            };
-            draggable.addDragListener(listener);
-        }
-
-        @Override
-        protected void onDragStart(DNDEvent e) {
-            e.getStatus().setData(EditModeDNDListener.SOURCE_TYPE, EditModeDNDListener.CONTENT_SOURCE_TYPE);
-            List<GWTJahiaNode> nodes = new ArrayList<GWTJahiaNode>(1);
-            nodes.add((GWTJahiaNode) listView.getSelectionModel().getSelectedItem());
-            e.setData(nodes);
-            List<GWTJahiaNode> list = new ArrayList<GWTJahiaNode>(1);
-            list.add((GWTJahiaNode) listView.getSelectionModel().getSelectedItem());
-            e.getStatus().setData("size", list.size());
-            e.getStatus().setData(EditModeDNDListener.SOURCE_NODES, list);
-            e.setOperation(DND.Operation.COPY);
-            super.onDragStart(e);
-        }
-
-        @Override
-        protected void onDragCancelled(DNDEvent dndEvent) {
-            super.onDragCancelled(dndEvent);
-            onDragEnd(dndEvent);
-        }
-
-        protected void onDragEnd(DNDEvent e) {
-            StatusProxy sp = e.getStatus();
-            sp.setData(EditModeDNDListener.SOURCE_TYPE, null);
-            sp.setData(EditModeDNDListener.CONTENT_SOURCE_TYPE, null);
-            sp.setData(EditModeDNDListener.TARGET_TYPE, null);
-            sp.setData(EditModeDNDListener.TARGET_NODE, null);
-            sp.setData(EditModeDNDListener.TARGET_PATH, null);
-            sp.setData(EditModeDNDListener.SOURCE_NODES, null);
-            sp.setData(EditModeDNDListener.SOURCE_QUERY, null);
-            sp.setData(EditModeDNDListener.SOURCE_TEMPLATE, null);
-            sp.setData(EditModeDNDListener.OPERATION_CALLED, null);
-            e.setData(null);
-        }
-
-
-    }
-
     @Override
     protected boolean acceptNode(GWTJahiaNode node) {
         return node.getInheritedNodeTypes().contains("nt:file");

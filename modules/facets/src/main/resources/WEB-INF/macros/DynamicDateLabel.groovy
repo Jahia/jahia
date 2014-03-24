8445fdbac6d57ef12449ae413cde00a41afd818c
Benjamin Papež <bpapez@jahia.com>
2014-03-24 16:09:32 +0000

    QA-4744: Wrong months displayed in facets - this was the case on US computers due to a missing timezone setting in the MessageFormatter of our DynamicDateLabel macro
    git-svn-id: svn://localhost/jahia/trunk@24206 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/facets/src/main/resources/WEB-INF/macros/DynamicDateLabel.groovy
+++ modules/facets/src/main/resources/WEB-INF/macros/DynamicDateLabel.groovy
@@ -1,6 +1,19 @@
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+
+import org.jahia.services.search.facets.JahiaQueryParser;
+
 String dateString = param1;
-java.util.Date date = org.jahia.services.search.facets.JahiaQueryParser.DATE_TYPE.parseMath(null, param1); 
+Date date = JahiaQueryParser.DATE_TYPE.parseMath(null, param1); 
 if (date != null) {
-   dateString = new java.text.MessageFormat("{0" + (binding.variables.containsKey("param2") ? "," + param2 : "") + (binding.variables.containsKey("param3") ? "," + param3 : "") + "}", renderContext.getMainResourceLocale()).format([date] as Object[]);
+   MessageFormat mf = new MessageFormat("{0" + (binding.variables.containsKey("param2") ? "," + param2 : "") + (binding.variables.containsKey("param3") ? "," + param3 : "") + "}", renderContext.getMainResourceLocale());
+   TimeZone tz = TimeZone.getTimeZone("UTC");
+   Object [] formats = mf.getFormats();
+   for (int i = 0; i < formats.length; i++) {
+       if (formats[i] instanceof SimpleDateFormat) {
+           ((SimpleDateFormat)formats[i]).setTimeZone(tz);
+       }
+   }
+   dateString = mf.format([date] as Object[]);
 }
 print dateString;

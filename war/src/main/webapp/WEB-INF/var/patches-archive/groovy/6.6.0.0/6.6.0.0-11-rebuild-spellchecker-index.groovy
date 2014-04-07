a065ba568c108b9771bfa9e334e0e9d9bfc96fbc
Sergiy Shyrkov <sshyrkov@jahia.com>
2014-04-07 21:13:28 +0000

    Moved Groovy and SQL patches from Jahia to Fix Creator projects
    git-svn-id: svn://localhost/jahia/trunk@24292 69c369ed-ed09-0410-a4a1-e19a46c23626

--- war/src/main/webapp/WEB-INF/var/patches-archive/groovy/6.6.0.0/6.6.0.0-11-rebuild-spellchecker-index.groovy
+++
@@ -1,9 +0,0 @@
-import org.jahia.services.search.spell.CompositeSpellChecker;
-
-def log = log;
-
-log.info("Force rebuilding spellchecker indexes...");
-
-CompositeSpellChecker.updateSpellCheckerIndex();
-
-log.info("... done rebuilding spellchecker indexes.");
--- war/src/main/webapp/WEB-INF/var/patches-archive/groovy/6.6.1.5/6.6.1.5-01-deactivate-datastoreGC-job.groovy
+++
@@ -1,3 +0,0 @@
-import org.jahia.registries.ServicesRegistry
-
-ServicesRegistry.getInstance().getSchedulerService().getScheduler().unscheduleJob("DataStoreGarbageCollectorJobTrigger", "DEFAULT"); 

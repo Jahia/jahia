ec18fed60629088ffa953644ecd7268c232e0aeb
Sergiy Shyrkov <sshyrkov@jahia.com>
2014-05-14 11:59:15 +0000

    QA-6132 - Upgrade CK Editor to 4.4 Adjusted CKEDITOR_BASEPATH
    git-svn-id: svn://localhost/jahia/trunk@24514 69c369ed-ed09-0410-a4a1-e19a46c23626

--- war/src/main/webapp/WEB-INF/scripts/resources.groovy
+++ war/src/main/webapp/WEB-INF/scripts/resources.groovy
@@ -18,7 +18,7 @@ if (renderContext.mainResource.contextConfiguration == 'page') {
     def slangmap = [en:'en_US', da:'da_DK', nl:'nl_NL', fi:'fi_FI', fr:'fr_FR', de:'de_DE', el:'el_GR', it:'it_IT', nb:'nb_NO', pt:'pt_PT', es:'es_ES', sv:'sv_SE']
     println "<script type=\"text/javascript\">"
     print "var contextJsParameters={contextPath:\"${contextPath}\",lang:\"${renderContext.mainResourceLocale}\",uilang:\"${renderContext.UILocale}\",siteUuid:\"${renderContext.site.identifier}\",wcag:${renderContext.siteInfo.WCAGComplianceCheckEnabled}}; ";
-    print "var CKEDITOR_BASEPATH=\"${contextPath}/modules/assets/javascript/ckeditor/\"; "
+    print "var CKEDITOR_BASEPATH=\"${contextPath}/modules/ckeditor/javascript/\"; "
     print "var scayt_custom_params=new Array(); "
     if (slangmap[renderContext.mainResource.locale.language] != null) {
         print "scayt_custom_params['sLang']='"+slangmap[renderContext.mainResource.locale.language]+"';"

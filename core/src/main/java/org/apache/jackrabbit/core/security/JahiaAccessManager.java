03338cd416a16edd34948cbc896057260fbc587c
Benjamin Papež <bpapez@jahia.com>
2014-05-30 09:09:00 +0000

    Fix merge conflicts
    git-svn-id: svn://localhost/jahia/trunk@24653 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/apache/jackrabbit/core/security/JahiaAccessManager.java
+++ core/src/main/java/org/apache/jackrabbit/core/security/JahiaAccessManager.java
@@ -490,21 +490,11 @@ public class JahiaAccessManager extends AbstractAccessControlManager implements 
         boolean res = false;
 
         String cacheKey = absPathStr + " : " + permissions;
-<<<<<<< .working
-<<<<<<< .working
-        Boolean result = (Boolean) pathPermissionCache.get(cacheKey);
-        if (result != null) {
-            return result;
-=======
-        if (pathPermissionCache.containsKey(cacheKey)) {
-            return pathPermissionCache.get(cacheKey);
-=======
+
 
         Boolean result = pathPermissionCache.get(cacheKey);
         if (result != null) {
             return result;
->>>>>>> .merge-right.r49886
->>>>>>> .merge-right.r49884
         }
 
         try {

a53e013cf3f85c968d6c021bcbaf2cc1a11951c5
Kevan Jahanshahi <kevan@jahia.com>
2014-06-10 16:24:51 +0000

    QA-6265: revert jcr rest util merge to default, put this code into portal factory core module
    git-svn-id: svn://localhost/jahia/trunk@24746 69c369ed-ed09-0410-a4a1-e19a46c23626

--- modules/default/src/main/resources/WEB-INF/scripts/jcrRestUtilsInit.groovy
+++
@@ -1,7 +0,0 @@
-println("<script type=\"text/javascript\">");
-println("JCRRestUtils = new Jahia.JCRRestUtils({");
-options.eachWithIndex() {
-    obj, i -> println "${obj.key}: ${obj.value}${i+1 < options.size() ? ',' : ''}"
-};
-println("});")
-println("</script>")

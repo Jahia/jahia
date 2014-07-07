a57f21c8b2feb3fe50768305dd205c5c623b233c
Thomas Draier <tdraier@jahia.com>
2014-07-07 12:48:58 +0000

    BACKLOG-4791 : handle enter key to add tag
    git-svn-id: svn://localhost/jahia/trunk@24935 69c369ed-ed09-0410-a4a1-e19a46c23626

--- gwt/src/main/java/org/jahia/ajax/gwt/client/widget/form/tag/AddTagContainer.java
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/widget/form/tag/AddTagContainer.java
@@ -1,11 +1,23 @@
 package org.jahia.ajax.gwt.client.widget.form.tag;
 
+import com.extjs.gxt.ui.client.data.BaseListLoader;
+import com.extjs.gxt.ui.client.data.RpcProxy;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.ComponentEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.widget.HorizontalPanel;
 import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.form.ComboBox;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import org.jahia.ajax.gwt.client.core.JahiaGWTParameters;
+import org.jahia.ajax.gwt.client.data.GWTJahiaValueDisplayBean;
+import org.jahia.ajax.gwt.client.data.node.GWTJahiaNode;
 import org.jahia.ajax.gwt.client.messages.Messages;
+import org.jahia.ajax.gwt.client.service.content.JahiaContentManagementService;
 import org.jahia.ajax.gwt.client.util.icons.StandardIconsProvider;
+
+import java.util.List;
 
 
 /**
@@ -25,20 +37,66 @@ public class AddTagContainer extends HorizontalPanel {
         addTagButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
             @Override
             public void componentSelected(ButtonEvent ce) {
-                String tag = null;
-                if(tagComboBox.getValue() != null){
-                    tag = tagComboBox.getValue().getValue();
-                } else if(tagComboBox.getRawValue() != null && tagComboBox.getRawValue().trim().length() > 0){
-                    tag = tagComboBox.getRawValue().trim();
-                }
-                if(tag != null) {
-                    tagField.addTag(tag);
-                    tagComboBox.setRawValue("");
-                }
+                addTag();
             }
         });
 
         add(tagComboBox);
         add(addTagButton);
+    }
+
+    private void addTag() {
+        String tag = null;
+        if (tagComboBox.getValue() != null) {
+            tag = tagComboBox.getValue().getValue();
+        } else if (tagComboBox.getRawValue() != null && tagComboBox.getRawValue().trim().length() > 0) {
+            tag = tagComboBox.getRawValue().trim();
+        }
+        if (tag != null) {
+            tagField.addTag(tag);
+            tagComboBox.setRawValue("");
+        }
+    }
+
+    /**
+     * Created by kevan on 02/07/14.
+     */
+    public class TagComboBox extends ComboBox<GWTJahiaValueDisplayBean> {
+        public TagComboBox(boolean autoComplete) {
+            if (autoComplete) {
+                setDisplayField("display");
+                final ListStore<GWTJahiaValueDisplayBean> store = new ListStore<GWTJahiaValueDisplayBean>(new BaseListLoader(
+                        new RpcProxy<List<GWTJahiaValueDisplayBean>>() {
+                            @Override
+                            protected void load(Object loadConfig, AsyncCallback<List<GWTJahiaValueDisplayBean>> asyncCallback) {
+                                // TODO handle separator to provide better autocomplete
+                                GWTJahiaNode site = JahiaGWTParameters.getSiteNode();
+                                JahiaContentManagementService.App.getInstance().getTags(getRawValue(),
+                                        site != null ? site.getPath() : null, 1L, 10L, 0L, true, asyncCallback);
+                            }
+                        }));
+                setStore(store);
+                setTypeAhead(true);
+                setTypeAheadDelay(100);
+                setHideTrigger(true);
+                addKeyListener(new com.extjs.gxt.ui.client.event.KeyListener() {
+                    @Override
+                    public void componentKeyPress(ComponentEvent event) {
+                        if (event.getEvent().getKeyCode() == 13) {
+                            addTag();
+                        }
+                    }
+                });
+                setTriggerAction(TriggerAction.ALL);
+                setMinChars(2);
+                setQueryDelay(100);
+            } else {
+                // create an empty store
+                setHideTrigger(true);
+                setDisplayField("display");
+                final ListStore<GWTJahiaValueDisplayBean> store = new ListStore<GWTJahiaValueDisplayBean>();
+                setStore(store);
+            }
+        }
     }
 }

a718fb85afec4eebe8a5b57d204fef8cea2b39c2
Kevan Jahanshahi <kevan@jahia.com>
2014-09-15 13:26:23 +0000

    BACKLOG-5197: facet unit test update to new tag prop
    git-svn-id: svn://localhost/jahia/trunk@25544 69c369ed-ed09-0410-a4a1-e19a46c23626

--- taglib/src/test/java/org/jahia/taglibs/facet/FunctionsTest.java
+++ taglib/src/test/java/org/jahia/taglibs/facet/FunctionsTest.java
@@ -93,13 +93,13 @@ public class FunctionsTest {
      */
     @Test
     public void testGetDeleteFacetUrl() throws Exception {
-        String query = "j:tags###4c1b0348-89d0-461e-b31d-d725b8e6ea18###3056820\\:FACET\\:tags:4c1b0348\\-89d0\\-461e\\-b31d\\-d725b8e6ea18|||j:tags###cdc62535-bcac-44d7-b4da-be8c865d7a58###3056820\\:FACET\\:tags:cdc62535\\-bcac\\-44d7\\-b4da\\-be8c865d7a58";
-        KeyValue facetValue1 = new DefaultKeyValue("4c1b0348-89d0-461e-b31d-d725b8e6ea18", "3056820\\:FACET\\:tags:4c1b0348\\-89d0\\-461e\\-b31d\\-d725b8e6ea18");
-        KeyValue facetValue2 = new DefaultKeyValue("cdc62535-bcac-44d7-b4da-be8c865d7a58", "3056820\\:FACET\\:tags:cdc62535\\-bcac\\-44d7\\-b4da\\-be8c865d7a58");
+        String query = "j:tagList###tag1###3056820\\:FACET\\:tags:4c1b0348\\-89d0\\-461e\\-b31d\\-d725b8e6ea18|||j:tagList###tag2###3056820\\:FACET\\:tags:cdc62535\\-bcac\\-44d7\\-b4da\\-be8c865d7a58";
+        KeyValue facetValue1 = new DefaultKeyValue("tag1", "3056820\\:FACET\\:tags:4c1b0348\\-89d0\\-461e\\-b31d\\-d725b8e6ea18");
+        KeyValue facetValue2 = new DefaultKeyValue("tag2", "3056820\\:FACET\\:tags:cdc62535\\-bcac\\-44d7\\-b4da\\-be8c865d7a58");
 
 
-        assertEquals("j:tags###cdc62535-bcac-44d7-b4da-be8c865d7a58###3056820\\:FACET\\:tags:cdc62535\\-bcac\\-44d7\\-b4da\\-be8c865d7a58", Functions.getDeleteFacetUrl(facetValue1, query));
-        assertEquals("j:tags###4c1b0348-89d0-461e-b31d-d725b8e6ea18###3056820\\:FACET\\:tags:4c1b0348\\-89d0\\-461e\\-b31d\\-d725b8e6ea18", Functions.getDeleteFacetUrl(facetValue2, query));
+        assertEquals("j:tagList###tag2###3056820\\:FACET\\:tags:cdc62535\\-bcac\\-44d7\\-b4da\\-be8c865d7a58", Functions.getDeleteFacetUrl(facetValue1, query));
+        assertEquals("j:tagList###tag1###3056820\\:FACET\\:tags:4c1b0348\\-89d0\\-461e\\-b31d\\-d725b8e6ea18", Functions.getDeleteFacetUrl(facetValue2, query));
     }
 
 } 

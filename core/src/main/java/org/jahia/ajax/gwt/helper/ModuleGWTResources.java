ac12b5145f35e3d2dda3292f03cdc149c5c94d79
Thomas Draier <tdraier@jahia.com>
2014-10-16 11:26:05 +0000

    Added new  integration points : custom edit engine tab, new javascript/css resources for edit mode
    git-svn-id: svn://localhost/jahia/trunk@25913 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ core/src/main/java/org/jahia/ajax/gwt/helper/ModuleGWTResources.java
@@ -0,0 +1,26 @@
+package org.jahia.ajax.gwt.helper;
+
+import java.util.List;
+
+public class ModuleGWTResources {
+
+    private List<String> cssResources;
+    private List<String> javascriptResources;
+
+    public List<String> getCSSResources() {
+        return cssResources;
+    }
+
+    public void setCSSResources(List<String> cssResources) {
+        this.cssResources = cssResources;
+    }
+
+    public List<String> getJavascriptResources() {
+        return javascriptResources;
+    }
+
+    public void setJavascriptResources(List<String> javascriptResources) {
+        this.javascriptResources = javascriptResources;
+    }
+
+}

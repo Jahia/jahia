8233d1c3c961ed42d4150956ee3d2c149eded76b
bamboo <bamboo@jahia.com>
2015-02-24 17:16:09 +0000

    QA-7041 EL-HOT-156
    Better information in the job output, list nb of keys cleaned per cache (commit by cmailleux merged from jahia/branches/JAHIA-7-0-X-X-BRANCH revision 52092)
    
    git-svn-id: svn://localhost/jahia/trunk@26858 69c369ed-ed09-0410-a4a1-e19a46c23626

--- data/scripts/groovy/evictExpiredCacheKey.groovy
+++ data/scripts/groovy/evictExpiredCacheKey.groovy
@@ -4,11 +4,19 @@ import org.jahia.services.cache.ehcache.EhCacheProvider
 def cacheManager = ((EhCacheProvider) SpringContextSingleton.getBean("ehCacheProvider")).getCacheManager()
 def totalKeys = 0
 def cleanedKeys = 0
+def mapCleanedKeysPerCache = [:]
 cacheManager.getCacheNames().each {cacheName ->
     def cache = cacheManager.getCache(cacheName)
     if(cache!=null) {
-        totalKeys += cache.getKeys().size()
-        cleanedKeys += cache.getKeysWithExpiryCheck().size()
+        def originalSize = cache.getKeys().size()
+        totalKeys += originalSize
+        def newSize = cache.getKeysWithExpiryCheck().size()
+        cleanedKeys += newSize
+        if(newSize!=originalSize){
+            mapCleanedKeysPerCache[cache.name] = originalSize - newSize
+        }
     }
 }
-println "Cleaned " + (totalKeys - cleanedKeys) + " keys from the caches"
+println "Cleaned ${(totalKeys - cleanedKeys)} keys from the caches"
+
+mapCleanedKeysPerCache.each() {key,val -> println "\t- ${val} from cache ${key}"}

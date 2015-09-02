9c92c1f3367e4c6a9555c0daf2469918d88440af
bamboo <bamboo@jahia.com>
2015-09-02 15:25:26 +0000

    QA-7657 : Add trigger to fix bad behaviour of for auto_increment on process_instance_info (commit by tdraier merged from jahia/branches/JAHIA-7-0-X-X-BRANCH revision 52926)
    git-svn-id: svn://localhost/jahia/trunk@27692 69c369ed-ed09-0410-a4a1-e19a46c23626

--- data/db/sql/schema/mysql/jbpm-schema.sql
+++ data/db/sql/schema/mysql/jbpm-schema.sql
@@ -817,3 +817,19 @@
         add constraint FK_nuqjfe1rpp1ad7jlfjjki6tog 
         foreign key (task_data_comments_id) 
         references jbpm_task (id);
+
+    drop trigger if exists trigger_autoinc_jbpm_process_instance_info;
+
+    DELIMITER //
+    CREATE TRIGGER trigger_autoinc_jbpm_process_instance_info BEFORE INSERT ON jbpm_process_instance_info FOR EACH ROW
+    BEGIN
+       declare auto_incr1 BIGINT;
+       declare auto_incr2 BIGINT;
+       SELECT AUTO_INCREMENT INTO auto_incr1 FROM information_schema.TABLES WHERE table_schema=DATABASE() AND table_name='jbpm_process_instance_info';
+       SELECT AUTO_INCREMENT INTO auto_incr2 FROM information_schema.TABLES WHERE table_schema=DATABASE() AND table_name='jbpm_process_instance_log';
+       IF (auto_incr2 > auto_incr1 and NEW.instance_id<auto_incr2) THEN
+           SET NEW.instance_id = auto_incr2;
+       END IF;
+    END; //
+    DELIMITER ;
+

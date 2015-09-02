204ebc7b6c4bb81ad2c149972f3f3eab0864923c
Thomas Draier <tdraier@jahia.com>
2015-09-02 16:00:57 +0000

    QA-4572 : fixed merge
    git-svn-id: svn://localhost/jahia/trunk@27700 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/ajax/gwt/content/server/JahiaContentManagementServiceImpl.java
+++ core/src/main/java/org/jahia/ajax/gwt/content/server/JahiaContentManagementServiceImpl.java
@@ -827,12 +827,6 @@ public class JahiaContentManagementServiceImpl extends JahiaRemoteService implem
                         }
                     }
                 }
-<<<<<<< .working
-=======
-            } catch (RepositoryException e) {
-                logger.error(e.toString(),e);
-                throw new GWTJahiaServiceException(e);
->>>>>>> .merge-right.r52931
             }
             // save acl
             if (acl != null) {
@@ -854,17 +848,9 @@ public class JahiaContentManagementServiceImpl extends JahiaRemoteService implem
                         publication.publish(Arrays.asList(jcrSessionWrapper.getNode(node.getPath() + "/" + VisibilityService.NODE_NAME).getIdentifier()));
                     }
                 } catch (RepositoryException e) {
+                    logger.error("Error while saving visibility conditions for node "+node.getPath(),e);
                     throw new GWTJahiaServiceException(e);
                 }
-<<<<<<< .working
-=======
-                if (Boolean.TRUE.equals(node.get("conditions-published"))) {
-                    publication.publish(Arrays.asList(jcrSessionWrapper.getNode(node.getPath() + "/" + VisibilityService.NODE_NAME).getIdentifier()));
-                }
-            } catch (RepositoryException e) {
-                logger.error("Error while saving visibility conditions for node "+node.getPath(),e);
-                throw new GWTJahiaServiceException(e);
->>>>>>> .merge-right.r52931
             }
             if (node.get("activeWorkflows") != null) {
                 workflow.updateWorkflowRules(node,

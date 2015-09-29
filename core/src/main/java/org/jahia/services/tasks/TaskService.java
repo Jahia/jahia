0a7298a009ac07a54021c98902c128f869c30a54
Ahmed Chaabni <achaabni@jahia.com>
2015-09-29 09:29:05 +0000

    QA-7823 : TaskService doesn't define the assigneeUserKey attribute on task creation
    git-svn-id: svn://localhost/jahia/trunk@27824 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/tasks/TaskService.java
+++ core/src/main/java/org/jahia/services/tasks/TaskService.java
@@ -2,72 +2,72 @@
  * ==========================================================================================
  * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
  * ==========================================================================================
- *
- *     Copyright (C) 2002-2015 Jahia Solutions Group SA. All rights reserved.
- *
- *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
- *     1/GPL OR 2/JSEL
- *
- *     1/ GPL
- *     ======================================================================================
- *
- *     IF YOU DECIDE TO CHOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
- *
- *     "This program is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU General Public License
- *     as published by the Free Software Foundation; either version 2
- *     of the License, or (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- *     As a special exception to the terms and conditions of version 2.0 of
- *     the GPL (or any later version), you may redistribute this Program in connection
- *     with Free/Libre and Open Source Software ("FLOSS") applications as described
- *     in Jahia's FLOSS exception. You should have received a copy of the text
- *     describing the FLOSS exception, also available here:
- *     http://www.jahia.com/license"
- *
- *     2/ JSEL - Commercial and Supported Versions of the program
- *     ======================================================================================
- *
- *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
- *
- *     Alternatively, commercial and supported versions of the program - also known as
- *     Enterprise Distributions - must be used in accordance with the terms and conditions
- *     contained in a separate written agreement between you and Jahia Solutions Group SA.
- *
- *     If you are unsure which license is appropriate for your use,
- *     please contact the sales department at sales@jahia.com.
- *
- *
+ * <p/>
+ * Copyright (C) 2002-2015 Jahia Solutions Group SA. All rights reserved.
+ * <p/>
+ * THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ * 1/GPL OR 2/JSEL
+ * <p/>
+ * 1/ GPL
+ * ======================================================================================
+ * <p/>
+ * IF YOU DECIDE TO CHOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ * <p/>
+ * "This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * <p/>
+ * As a special exception to the terms and conditions of version 2.0 of
+ * the GPL (or any later version), you may redistribute this Program in connection
+ * with Free/Libre and Open Source Software ("FLOSS") applications as described
+ * in Jahia's FLOSS exception. You should have received a copy of the text
+ * describing the FLOSS exception, also available here:
+ * http://www.jahia.com/license"
+ * <p/>
+ * 2/ JSEL - Commercial and Supported Versions of the program
+ * ======================================================================================
+ * <p/>
+ * IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ * <p/>
+ * Alternatively, commercial and supported versions of the program - also known as
+ * Enterprise Distributions - must be used in accordance with the terms and conditions
+ * contained in a separate written agreement between you and Jahia Solutions Group SA.
+ * <p/>
+ * If you are unsure which license is appropriate for your use,
+ * please contact the sales department at sales@jahia.com.
+ * <p/>
+ * <p/>
  * ==========================================================================================
  * =                                   ABOUT JAHIA                                          =
  * ==========================================================================================
- *
- *     Rooted in Open Source CMS, Jahia’s Digital Industrialization paradigm is about
- *     streamlining Enterprise digital projects across channels to truly control
- *     time-to-market and TCO, project after project.
- *     Putting an end to “the Tunnel effect”, the Jahia Studio enables IT and
- *     marketing teams to collaboratively and iteratively build cutting-edge
- *     online business solutions.
- *     These, in turn, are securely and easily deployed as modules and apps,
- *     reusable across any digital projects, thanks to the Jahia Private App Store Software.
- *     Each solution provided by Jahia stems from this overarching vision:
- *     Digital Factory, Workspace Factory, Portal Factory and eCommerce Factory.
- *     Founded in 2002 and headquartered in Geneva, Switzerland,
- *     Jahia Solutions Group has its North American headquarters in Washington DC,
- *     with offices in Chicago, Toronto and throughout Europe.
- *     Jahia counts hundreds of global brands and governmental organizations
- *     among its loyal customers, in more than 20 countries across the globe.
- *
- *     For more information, please visit http://www.jahia.com
+ * <p/>
+ * Rooted in Open Source CMS, Jahia’s Digital Industrialization paradigm is about
+ * streamlining Enterprise digital projects across channels to truly control
+ * time-to-market and TCO, project after project.
+ * Putting an end to “the Tunnel effect”, the Jahia Studio enables IT and
+ * marketing teams to collaboratively and iteratively build cutting-edge
+ * online business solutions.
+ * These, in turn, are securely and easily deployed as modules and apps,
+ * reusable across any digital projects, thanks to the Jahia Private App Store Software.
+ * Each solution provided by Jahia stems from this overarching vision:
+ * Digital Factory, Workspace Factory, Portal Factory and eCommerce Factory.
+ * Founded in 2002 and headquartered in Geneva, Switzerland,
+ * Jahia Solutions Group has its North American headquarters in Washington DC,
+ * with offices in Chicago, Toronto and throughout Europe.
+ * Jahia counts hundreds of global brands and governmental organizations
+ * among its loyal customers, in more than 20 countries across the globe.
+ * <p/>
+ * For more information, please visit http://www.jahia.com
  */
 package org.jahia.services.tasks;
 
@@ -75,10 +75,13 @@ import org.jahia.services.content.decorator.JCRGroupNode;
 import org.jahia.services.content.decorator.JCRUserNode;
 import org.jahia.services.usermanager.JahiaUserManagerService;
 import org.slf4j.Logger;
+
 import static org.jahia.api.Constants.JAHIANT_TASK;
 import static org.jahia.api.Constants.JAHIANT_TASKS;
+
 import org.jahia.services.content.*;
 import org.jahia.services.usermanager.JahiaGroupManagerService;
+
 import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import java.util.Calendar;
@@ -86,7 +89,7 @@ import java.util.Set;
 
 /**
  * Task management service.
- * 
+ *
  * @author Sergiy Shyrkov
  */
 public class TaskService {
@@ -106,7 +109,7 @@ public class TaskService {
 
     /**
      * Creates a task for the specified user.
-     * 
+     *
      * @param task the task to be created
      * @param forUser the user name, who gets this task
      * @throws RepositoryException in case of an error
@@ -175,6 +178,8 @@ public class TaskService {
         taskNode.setProperty("state", task.getState().toString().toLowerCase());
         try {
             taskNode.setProperty("assignee", userNode);
+            // update assigneeUserKey. assignee is not used for defining the owner of the task
+            taskNode.setProperty("assigneeUserKey", userNode.getPath());
         } catch (Exception e) {
             logger.warn("Unable to find user '" + userNode.getPath() + "' to assign a task", e);
         }
@@ -184,7 +189,7 @@ public class TaskService {
      * Creates a task for all users of the specified group. Note that the task
      * will be created for users, who are currently members of the group, it is
      * not applied e.g. if a new user will be added to the group.
-     * 
+     *
      * @param task the task to be created
      * @param forGroup the group name, which members will get this task
      * @param siteKey the site key of the group

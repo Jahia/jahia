d13b54fa959cd97dda8155c7755d0d69afb0df5f
David Griffon <dgriffon@jahia.com>
2015-10-09 08:45:36 +0000

    Revert: QA-7810 : fix npe when a module is not started (will be committed after tagging)
    git-svn-id: svn://localhost/jahia/trunk@27896 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/templates/ComponentRegistry.java
+++ core/src/main/java/org/jahia/services/templates/ComponentRegistry.java
@@ -74,6 +74,7 @@ package org.jahia.services.templates;
 import com.google.common.base.Functions;
 import com.google.common.collect.Ordering;
 import org.jahia.data.templates.JahiaTemplatesPackage;
+import org.jahia.registries.ServicesRegistry;
 import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.content.JCRSessionWrapper;
 import org.jahia.services.content.decorator.JCRSiteNode;
@@ -159,7 +160,15 @@ public class ComponentRegistry {
         l.add("system-jahia");
 
         if (resolvedSite != null) {
-            l.addAll(resolvedSite.getInstalledModulesWithAllDependencies());
+            for (String s : resolvedSite.getInstalledModules()) {
+                JahiaTemplatesPackage module = ServicesRegistry.getInstance().getJahiaTemplateManagerService().getTemplatePackageById(s);
+
+                l.add(module.getId());
+
+                for (JahiaTemplatesPackage dep : module.getDependencies()) {
+                    l.add(dep.getId());
+                }
+            }
         }
 
         for (String aPackage : l) {

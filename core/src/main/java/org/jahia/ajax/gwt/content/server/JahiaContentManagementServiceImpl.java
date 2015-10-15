cc45d38744c73606a456c92864f265cf0e8e0974
David Griffon <dgriffon@jahia.com>
2015-10-15 16:20:17 +0000

    fix invalid auto merge
    git-svn-id: svn://localhost/jahia/branches/JAHIA-7-1-X-X-BRANCH@27943 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/ajax/gwt/content/server/JahiaContentManagementServiceImpl.java
+++ core/src/main/java/org/jahia/ajax/gwt/content/server/JahiaContentManagementServiceImpl.java
@@ -1337,37 +1337,6 @@ public class JahiaContentManagementServiceImpl extends JahiaRemoteService implem
             if ("live".equals(workspace) && !SettingsBean.getInstance().isUrlRewriteSeoRulesEnabled()) {
                 nodeURL = Url.appendServerNameIfNeeded(node, nodeURL, getRequest());
             }
-<<<<<<< .working
-            
-            if(SettingsBean.getInstance().isUrlRewriteSeoRulesEnabled() 
-            		&& !Url.isLocalhost(getSite().getServerName()) && !Url.isLocalhost(getRequest().getServerName()) 
-            		&& !getSite().getServerName().equals(getRequest().getServerName())) {            	
-            	 if (!(getRequest().getServerPort() == 80 && "http".equals(getRequest().getScheme()) 
-            			 || getRequest().getServerPort() == 443 && "https".equals(getRequest().getScheme()))) {
-            		 nodeURL = new StringBuilder().append(getRequest().getScheme()).append("://").append(getRequest().getServerName())
-            				 .append(":").append(getRequest().getServerPort()).append(nodeURL).toString();
-                 } else {
-                	 nodeURL = new StringBuilder().append(getRequest().getScheme()).append("://").append(getRequest().getServerName())
-            				 .append(nodeURL).toString(); 
-                 }
-=======
-<<<<<<< .working
-
-            if (SettingsBean.getInstance().isUrlRewriteSeoRulesEnabled()
-                    && !Url.isLocalhost(getSite().getServerName()) && !Url.isLocalhost(getRequest().getServerName())
-                    && !getSite().getServerName().equals(getRequest().getServerName())) {
-                if (!(getRequest().getServerPort() == 80 && "http".equals(getRequest().getScheme())
-                        || getRequest().getServerPort() == 443 && "https".equals(getRequest().getScheme()))) {
-                    nodeURL = new StringBuilder().append(getRequest().getScheme()).append("://").append(getRequest().getServerName())
-                            .append(":").append(getRequest().getServerPort()).append(nodeURL).toString();
-                } else {
-                    nodeURL = new StringBuilder().append(getRequest().getScheme()).append("://").append(getRequest().getServerName())
-                            .append(nodeURL).toString();
-                }
->>>>>>> .merge-right.r53174
-            }
-=======
->>>>>>> .merge-right.r53173
 
             return getResponse().encodeURL(nodeURL);
         } catch (MalformedURLException e) {

11df0aced2099d8e49f65b3577927346150ac2a6
Sergiy Shyrkov <sshyrkov@jahia.com>
2015-11-13 11:40:33 +0000

    Corrected SVN merge ( QA-7989 )
    git-svn-id: svn://localhost/jahia/trunk@28033 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/pwdpolicy/RuleConditions.java
+++ core/src/main/java/org/jahia/services/pwdpolicy/RuleConditions.java
@@ -298,14 +298,10 @@ public final class RuleConditions {
     public static class PreventUserInitiatedPasswordChange implements
             PasswordPolicyRuleCondition {
         public boolean evaluate(List<JahiaPasswordPolicyRuleParam> parameters, EvaluationContext ctx) {
-<<<<<<< .working
-            if (ctx.isUserInitiated() && ctx.getUser() == null) {
-=======
             if (!ctx.isUserInitiated()) {
                 return true;
             }
             if (ctx.getUser() == null) {
->>>>>>> .merge-right.r53266
                 throw new IllegalArgumentException(
                         "The user object is null. Unable to evaluate the condition");
             }

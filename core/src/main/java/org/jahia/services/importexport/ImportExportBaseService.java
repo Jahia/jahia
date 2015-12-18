bb88b2c2ba86953af50f783b39be549320073530
bamboo <bamboo@jahia.com>
2015-12-18 16:11:56 +0000

    QA-7945 : Certain methods should not be used (commit by achaabni merged from jahia/branches/JAHIA-7-1-X-X-BRANCH revision 53512)
    git-svn-id: svn://localhost/jahia/trunk@28278 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/importexport/ImportExportBaseService.java
+++ core/src/main/java/org/jahia/services/importexport/ImportExportBaseService.java
@@ -107,6 +107,7 @@ import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.util.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.SimpleDateFormat;

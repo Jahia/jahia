e8f5d374348a2f06927da776c8814afae6a42449
Kevan Jahanshahi <kevan@jahia.com>
2016-01-06 09:43:33 +0000

    QA-8132: Allow JCRMountPointNode to protect some properties from read action, to be able to protect passwords. Also extract the logical stuff of properties protection in an Abstract decorator instead of copy paste the code.
    git-svn-id: svn://localhost/jahia/branches/JAHIA-7-1-X-X-BRANCH@28353 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/content/decorator/JCRPasswordHistoryEntryNode.java
+++ core/src/main/java/org/jahia/services/content/decorator/JCRPasswordHistoryEntryNode.java
@@ -43,125 +43,24 @@
  */
 package org.jahia.services.content.decorator;
 
-import java.util.Map;
-
-import javax.jcr.PathNotFoundException;
-import javax.jcr.PropertyIterator;
 import javax.jcr.RepositoryException;
 
 import org.jahia.services.content.JCRNodeWrapper;
-import org.jahia.services.content.JCRPropertyWrapper;
-import org.jahia.services.content.LazyPropertyIterator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.common.base.Predicate;
-import com.google.common.collect.Maps;
 
 /**
  * Decorator for the JCR nodes of type <code>jnt:passwordHistoryEntry</code>.
  * 
  * @author Sergiy Shyrkov
  */
-public class JCRPasswordHistoryEntryNode extends JCRNodeDecorator {
-
-    private class FilteredIterator extends LazyPropertyIterator {
-        public FilteredIterator() throws RepositoryException {
-            super(JCRPasswordHistoryEntryNode.this, JCRPasswordHistoryEntryNode.this.getSession().getLocale());
-        }
-
-        public FilteredIterator(String singlePattern) throws RepositoryException {
-            super(JCRPasswordHistoryEntryNode.this, JCRPasswordHistoryEntryNode.this.getSession().getLocale(),
-                    singlePattern);
-        }
-
-        public FilteredIterator(String[] patternArray) throws RepositoryException {
-            super(JCRPasswordHistoryEntryNode.this, JCRPasswordHistoryEntryNode.this.getSession().getLocale(),
-                    patternArray);
-        }
-
-        @Override
-        public boolean hasNext() {
-            while (super.hasNext()) {
-                try {
-                    if (!canGetProperty(tempNext.getName())) {
-                        tempNext = null;
-                    } else {
-                        return true;
-                    }
-                } catch (RepositoryException e) {
-                    tempNext = null;
-                    logger.error("Cannot read property", e);
-                }
-            }
-            return false;
-        }
-    }
-
-    private transient static Logger logger = LoggerFactory.getLogger(JCRPasswordHistoryEntryNode.class);
+public class JCRPasswordHistoryEntryNode extends JCRProtectedNodeAbstractDecorator {
 
     private static final String PROTECTED_PROPERTY = "j:password";
 
     public JCRPasswordHistoryEntryNode(JCRNodeWrapper node) {
-        super(node);
+        super(node, true);
     }
 
-    private boolean canGetProperty(String propertyName) throws RepositoryException {
-        return getSession().isSystem() || !PROTECTED_PROPERTY.equals(propertyName);
-    }
-
-    @Override
-    public PropertyIterator getProperties() throws RepositoryException {
-        // no filtering for system sessions
-        return getSession().isSystem() ? super.getProperties() : new FilteredIterator();
-    }
-
-    @Override
-    public PropertyIterator getProperties(String s) throws RepositoryException {
-        return getSession().isSystem() ? super.getProperties(s) : new FilteredIterator(s);
-    }
-
-    @Override
-    public PropertyIterator getProperties(String[] strings) throws RepositoryException {
-        return getSession().isSystem() ? super.getProperties(strings) : new FilteredIterator(strings);
-    }
-
-    @Override
-    public Map<String, String> getPropertiesAsString() throws RepositoryException {
-        return getSession().isSystem() ? super.getPropertiesAsString() : Maps.filterKeys(super.getPropertiesAsString(),
-                new Predicate<String>() {
-                    @Override
-                    public boolean apply(String input) {
-                        try {
-                            return canGetProperty(input);
-                        } catch (RepositoryException e) {
-                            return false;
-                        }
-                    }
-                });
-    }
-
-    @Override
-    public JCRPropertyWrapper getProperty(String s) throws PathNotFoundException, RepositoryException {
-        if (!canGetProperty(s)) {
-            throw new PathNotFoundException(s);
-        }
-
-        return super.getProperty(s);
-    }
-
-    @Override
-    public String getPropertyAsString(String name) {
-        try {
-            return canGetProperty(name) ? super.getPropertyAsString(name) : null;
-        } catch (RepositoryException e) {
-            logger.error(e.getMessage(), e);
-            return null;
-        }
-    }
-
-    @Override
-    public boolean hasProperty(String s) throws RepositoryException {
-        return super.hasProperty(s) && canGetProperty(s);
+    boolean canReadProperty(String propertyName) throws RepositoryException {
+        return !PROTECTED_PROPERTY.equals(propertyName);
     }
 }

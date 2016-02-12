72455a2bc197be9af89cb889be6b3a07e63e079b
bamboo <bamboo@jahia.com>
2016-02-12 16:55:48 +0000

    QA-8240: Fix area enabling on jnt:template that redirect to wrong template (view) (commit by kevan merged from jahia/branches/JAHIA-7-1-X-X-BRANCH revision 53849)
    git-svn-id: svn://localhost/jahia/trunk@28615 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/filter/StaticAssetsFilter.java
+++ core/src/main/java/org/jahia/services/render/filter/StaticAssetsFilter.java
@@ -349,7 +349,7 @@ public class StaticAssetsFilter extends AbstractFilter implements ApplicationLis
                                 "\" locale=\"" +
                                 resource.getLocale() + "\"" +
                                 " template=\"" +
-                                (resource.getTemplate() != null ? resource.getTemplate() : "default") +
+                                resource.getResolvedTemplate() +
                                 "\"" + " nodetypes=\"" +
                                 ConstraintsHelper.getConstraints(
                                         renderContext.getMainResource().getNode()) +

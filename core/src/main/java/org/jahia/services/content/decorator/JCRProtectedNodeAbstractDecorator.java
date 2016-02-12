d1cfed36d67934c0ab2aa5ffa4398f463c8d8f27
bamboo <bamboo@jahia.com>
2016-02-12 15:26:51 +0000

    Reverted commit 53761 (BACKLOG-6237 - Password recovery leads to exception due to non-serializable attribute) as JcrUserNode is not supposed to be serializable like all our JCR nodes (commit by sshyrkov merged from jahia/branches/JAHIA-7-1-X-X-BRANCH revision 53847)
    git-svn-id: svn://localhost/jahia/trunk@28613 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/content/decorator/JCRProtectedNodeAbstractDecorator.java
+++ core/src/main/java/org/jahia/services/content/decorator/JCRProtectedNodeAbstractDecorator.java
@@ -65,7 +65,6 @@ import java.util.Map;
 public abstract class JCRProtectedNodeAbstractDecorator extends JCRNodeDecorator {
 
     private boolean alwaysAllowSystem;
-    private static final Logger logger = LoggerFactory.getLogger(JCRProtectedNodeAbstractDecorator.class);
 
     /**
      * Instantiate this decorator, by default properties will be protect also from system session
@@ -85,6 +84,8 @@ public abstract class JCRProtectedNodeAbstractDecorator extends JCRNodeDecorator
         super(node);
         this.alwaysAllowSystem = alwaysAllowSystem;
     }
+
+    private transient static Logger logger = LoggerFactory.getLogger(JCRProtectedNodeAbstractDecorator.class);
 
     private class FilteredIterator extends LazyPropertyIterator {
         public FilteredIterator() throws RepositoryException {

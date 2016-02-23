6ed4e95e3ca8db2211378e38a430a8f33cb9e2be
Benjamin Papež <bpapez@jahia.com>
2016-02-23 15:29:59 +0000

    BACKLOG-6324: Allow for Spring beans in modules to be configured as servlet filters
    git-svn-id: svn://localhost/jahia/branches/JAHIA-7-1-X-X-BRANCH@28690 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ core/src/main/java/org/jahia/bin/filters/ModuleDelegatingFilterProxy.java
@@ -0,0 +1,79 @@
+/**
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2016 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.bin.filters;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletException;
+
+import org.jahia.services.SpringContextSingleton;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+/**
+ * Extends the behaviour of standard Spring's {@link DelegatingFilterProxy} by
+ * falling back to a {@link PassThroughFilter} (pass through) filter if the
+ * target filter cannot be found in any of the modules.
+ * 
+ * @author Sergiy Shyrkov
+ */
+public class ModuleDelegatingFilterProxy extends DelegatingFilterProxy {
+
+    @Override
+    protected Filter initDelegate(WebApplicationContext wac) throws ServletException {
+        Filter delegate = null;
+
+        try {
+            delegate = (Filter) SpringContextSingleton.getBeanInModulesContext(getTargetBeanName());
+            if (isTargetFilterLifecycle()) {
+                delegate.init(getFilterConfig());
+            }
+        } catch (NoSuchBeanDefinitionException e) {
+            delegate = PassThroughFilter.INSTANCE;
+        }
+
+        return delegate;
+    }
+
+}

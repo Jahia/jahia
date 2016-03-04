659292d5aa1ae06c1d7a3f0903c61d0d56956cb8
Ahmed Chaabni <achaabni@jahia.com>
2016-03-04 16:05:53 +0000

    BACKLOG-6188 : Implement cluster discovery based on jgroups
    git-svn-id: svn://localhost/jahia/trunk@28756 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ bundles/hazelcast-discovery/src/main/java/org/jahia/bundles/hazelcast/discovery/internal/osgi/Activator.java
@@ -0,0 +1,111 @@
+/**
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2016 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.bundles.hazelcast.discovery.internal.osgi;
+
+import org.apache.karaf.cellar.core.discovery.DiscoveryService;
+
+import org.jahia.bundles.hazelcast.discovery.internal.JahiaDiscoveryService;
+import org.jahia.settings.SettingsBean;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Jahia Cluster Discovery Bundle Activator
+ * @author achaabni
+ */
+public class Activator implements BundleActivator {
+
+    /**
+     * logger
+     */
+    private static final Logger logger = LoggerFactory.getLogger(Activator.class);
+
+    /**
+     * list of registrations
+     */
+    private final Map<String, ServiceRegistration> registrations = new ConcurrentHashMap<String, ServiceRegistration>();
+
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        if (SettingsBean.getInstance().isClusterActivated()) {
+            logger.debug("Starting Jahia Discovery Service");
+            Hashtable<String, Object> serviceProperties = new Hashtable<String, Object>();
+            JahiaDiscoveryService service = new JahiaDiscoveryService();
+            serviceProperties.put(Constants.SERVICE_PID, "org.jahia.bundles.hazelcast.discovery");
+            serviceProperties.put(Constants.SERVICE_DESCRIPTION, "Jahia Discovery Service");
+            serviceProperties.put(Constants.SERVICE_VENDOR, "Jahia Solutions Group SA");
+            serviceProperties.put(Constants.SERVICE_RANKING, Integer.valueOf(100));
+            final ServiceRegistration<?> newRegistration = bundleContext.registerService(DiscoveryService.class.getName(), service, (Dictionary) serviceProperties);
+            ServiceRegistration oldRegistration = (newRegistration == null) ? registrations.remove(serviceProperties.get(Constants.SERVICE_PID)) : registrations.put
+                    (Constants.SERVICE_PID, newRegistration);
+            if (oldRegistration != null) {
+                logger.debug("Un-registering Jahia discovery service {}", serviceProperties.get(Constants.SERVICE_PID));
+                oldRegistration.unregister();
+            }
+        }
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        if (SettingsBean.getInstance().isClusterActivated()) {
+            logger.debug("Stopping Jahia Discovery Service");
+            for (String key : registrations.keySet()) {
+                final ServiceRegistration serviceRegistration = registrations.get(key);
+                serviceRegistration.unregister();
+            }
+        }
+    }
+}

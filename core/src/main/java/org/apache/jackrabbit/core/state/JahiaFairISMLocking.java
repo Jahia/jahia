d3cee10dee99b3e7feaba74cfc43771f62e7717c
Thomas Draier <tdraier@jahia.com>
2016-03-10 17:20:50 +0000

    QA-8337 : Provide an implementation of JahiaISMLocking with fair locks
    git-svn-id: svn://localhost/jahia/branches/JAHIA-7-1-X-X-BRANCH@28801 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ core/src/main/java/org/apache/jackrabbit/core/state/JahiaFairISMLocking.java
@@ -0,0 +1,110 @@
+/**
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2016 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.apache.jackrabbit.core.state;
+
+import org.apache.jackrabbit.core.id.ItemId;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Default item state locking strategy. The default strategy is simply to use
+ * a single coarse-grained read-write lock over the entire workspace.
+ */
+public class JahiaFairISMLocking implements ISMLocking {
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
+    private final Lock rl = lock.readLock();
+    private final Lock wl = lock.writeLock();
+
+    /**
+     * The read lock instance used by readers to release the acquired lock.
+     */
+    private final ReadLock readLock = new ReadLock() {
+
+        public void release() {
+            rl.unlock();
+        }
+    };
+
+    /**
+     * The write lock instance used by writers to release or downgrade the
+     * acquired lock.
+     */
+    private final WriteLock writeLock = new WriteLock() {
+
+        public void release() {
+            wl.unlock();
+        }
+
+        public ReadLock downgrade() {
+            rl.lock();
+            wl.unlock();
+            return readLock;
+        }
+    };
+
+    /**
+     * Increments the reader count and returns the acquired read lock once
+     * there are no more writers or the current writer shares the thread id
+     * with this reader.
+     */
+    public ReadLock acquireReadLock(ItemId id)
+            throws InterruptedException {
+        rl.lock();
+        return readLock;
+    }
+
+    /**
+     * Increments the writer count, sets the writer identifier and returns
+     * the acquired write lock once there are no other active readers or
+     * writers or the current writer shares the thread id with this writer.
+     */
+    public WriteLock acquireWriteLock(ChangeLog changeLog)
+            throws InterruptedException {
+        wl.lock();
+        return writeLock;
+    }
+
+}

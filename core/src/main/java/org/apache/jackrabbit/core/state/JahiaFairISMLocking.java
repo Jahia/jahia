e9352b82a4f6e91f1121d4ea9db61c4ecd7ea88d
bamboo <bamboo@jahia.com>
2016-03-11 08:55:47 +0000

    QA-8337 - reuse code for the locking strategy, based on the ReentrantReadWriteLock (commit by sshyrkov merged from jahia/branches/JAHIA-7-1-X-X-BRANCH revision 54038)
    git-svn-id: svn://localhost/jahia/trunk@28804 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/apache/jackrabbit/core/state/JahiaFairISMLocking.java
+++ core/src/main/java/org/apache/jackrabbit/core/state/JahiaFairISMLocking.java
@@ -43,68 +43,14 @@
  */
 package org.apache.jackrabbit.core.state;
 
-import org.apache.jackrabbit.core.id.ItemId;
-
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
 /**
- * Default item state locking strategy. The default strategy is simply to use
- * a single coarse-grained read-write lock over the entire workspace.
+ * Default item state locking strategy. The default strategy is simply to use a single coarse-grained read-write lock over the entire
+ * workspace. This implementation uses a fair locking strategy.
  */
-public class JahiaFairISMLocking implements ISMLocking {
+public class JahiaFairISMLocking extends JahiaISMLocking {
 
-    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
-    private final Lock rl = lock.readLock();
-    private final Lock wl = lock.writeLock();
-
-    /**
-     * The read lock instance used by readers to release the acquired lock.
-     */
-    private final ReadLock readLock = new ReadLock() {
-
-        public void release() {
-            rl.unlock();
-        }
-    };
-
-    /**
-     * The write lock instance used by writers to release or downgrade the
-     * acquired lock.
-     */
-    private final WriteLock writeLock = new WriteLock() {
-
-        public void release() {
-            wl.unlock();
-        }
-
-        public ReadLock downgrade() {
-            rl.lock();
-            wl.unlock();
-            return readLock;
-        }
-    };
-
-    /**
-     * Increments the reader count and returns the acquired read lock once
-     * there are no more writers or the current writer shares the thread id
-     * with this reader.
-     */
-    public ReadLock acquireReadLock(ItemId id)
-            throws InterruptedException {
-        rl.lock();
-        return readLock;
-    }
-
-    /**
-     * Increments the writer count, sets the writer identifier and returns
-     * the acquired write lock once there are no other active readers or
-     * writers or the current writer shares the thread id with this writer.
-     */
-    public WriteLock acquireWriteLock(ChangeLog changeLog)
-            throws InterruptedException {
-        wl.lock();
-        return writeLock;
+    public JahiaFairISMLocking() {
+        super(true);
     }
 
 }

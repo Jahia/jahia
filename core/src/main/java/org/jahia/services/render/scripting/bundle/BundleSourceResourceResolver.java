24f6625c46968c0b47dd70f33a7b188d7681a7d0
Benjamin Papež <bpapez@jahia.com>
2016-03-16 10:40:30 +0000

    Fixed SVN merge for: QA-8321 fix typo
    git-svn-id: svn://localhost/jahia/trunk@28829 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/render/scripting/bundle/BundleSourceResourceResolver.java
+++ core/src/main/java/org/jahia/services/render/scripting/bundle/BundleSourceResourceResolver.java
@@ -55,11 +55,11 @@ public class BundleSourceResourceResolver {
                 if (s == null) {
                     s = model.getParent().getVersion();
                 } else if (s.matches("\\$\\{.*\\}")) {
-                    logger.warn("'version' contains an expression but should be a constant. Stopping mount of source forlder.");
+                    logger.warn("'version' contains an expression but should be a constant. Stopping mount of source folder.");
                     return null;
                 }
                 if (!s.equals(bundle.getHeaders().get("Implementation-Version"))) {
-                    logger.warn("pom.xml 'version' is not matching the Manifest Implementation-Version. Stopping mount of source forlder.");
+                    logger.warn("pom.xml 'version' is not matching the Manifest Implementation-Version. Stopping mount of source folder.");
                     return null;
                 }
             } catch (Exception e) {

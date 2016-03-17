8652bb4f4c12114afc11f7ae36554bccdf3e1c78
bamboo <bamboo@jahia.com>
2016-03-17 15:53:35 +0000

    QA-8368 : User selection UI: add the possibility to customize the label for the users (commit by dsaulnier merged from jahia/branches/JAHIA-7-1-X-X-BRANCH revision 54079)
    git-svn-id: svn://localhost/jahia/trunk@28845 69c369ed-ed09-0410-a4a1-e19a46c23626

---
+++ core/src/main/java/org/jahia/data/viewhelper/principal/PrincipalViewHelperExtension.java
@@ -0,0 +1,8 @@
+package org.jahia.data.viewhelper.principal;
+
+import org.jahia.services.usermanager.JahiaUser;
+
+public interface PrincipalViewHelperExtension {
+
+    public String getUserDisplayName(JahiaUser jahiaUser);
+}

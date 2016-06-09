07930ea4a458f7dab2680b36a4a5234bc2c489e3
Ahmed Chaabni <achaabni@jahia.com>
2016-06-09 15:13:01 +0000

    BACKLOG-6272 : Default ModuleManager service implementation (non-clustered)
    git-svn-id: svn://localhost/jahia/trunk@29277 69c369ed-ed09-0410-a4a1-e19a46c23626

--- core/src/main/java/org/jahia/services/modulemanager/Constants.java
+++ core/src/main/java/org/jahia/services/modulemanager/Constants.java
@@ -76,6 +76,8 @@ public interface Constants {
     
     Attributes.Name ATTR_NAME_REQUIRE_CAPABILITY = new Attributes.Name("Require-Capability");
 
+    Attributes.Name ATTR_NAME_ROOT_FOLDER = new Attributes.Name("root-folder");
+
     String BUNDLE_SERVICE_PROPERTY_CLUSTERED = "clustered";
     
     String OSGI_CAPABILITY_MODULE_DEPENDENCIES = "com.jahia.modules.dependencies";

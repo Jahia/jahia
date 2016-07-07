667fe40fd6d6a0f47b6e580fa860d86db02f02a5
Dominique Gigon <dgigon@jahia.com>
2016-07-07 14:36:17 +0000

    QA-8666 : added jmix:navMenuItem to node types listed by Publication Manager to let pages under menu label be visible.
    git-svn-id: svn://localhost/jahia/branches/JAHIA-7-1-X-X-BRANCH@29433 69c369ed-ed09-0410-a4a1-e19a46c23626

--- gwt/src/main/java/org/jahia/ajax/gwt/client/widget/publication/PublicationManagerEngine.java
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/widget/publication/PublicationManagerEngine.java
@@ -109,7 +109,7 @@ public class PublicationManagerEngine extends Window {
 
         // tree component
         GWTJahiaNodeTreeFactory factory = new GWTJahiaNodeTreeFactory(Arrays.asList("/sites/"+linker.getSelectionContext().getMainNode().getSiteKey()), true);
-        factory.setNodeTypes(Arrays.asList("jmix:publication","jmix:workflowRulesable"));
+        factory.setNodeTypes(Arrays.asList("jmix:publication","jmix:workflowRulesable","jmix:navMenuItem"));
         factory.setFields(Arrays.asList(GWTJahiaNode.NAME, GWTJahiaNode.DISPLAY_NAME, GWTJahiaNode.PUBLICATION_INFOS,
                                         GWTJahiaNode.WORKFLOW_INFOS));
         factory.setSelectedPath(linker.getSelectionContext().getMainNode().getPath());

4ffeb641e3485f144386f4e26a9513de32580b71
David Griffon <dgriffon@jahia.com>
2016-08-18 10:21:12 +0200

    BACKLOG-6044 : set correct license headers

--- core/src/main/java/org/jahia/services/render/scripting/bundle/BundleScriptEngineManager.java
+++ core/src/main/java/org/jahia/services/render/scripting/bundle/BundleScriptEngineManager.java
@@ -1,76 +1,47 @@
-package org.jahia.services.render.scripting.bundle;
-
 /**
  * ==========================================================================================
  * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
  * ==========================================================================================
  *
- * Copyright (C) 2002-2015 Jahia Solutions Group SA. All rights reserved.
+ *                                 http://www.jahia.com
  *
- * THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
- * 1/GPL OR 2/JSEL
+ *     Copyright (C) 2002-2016 Jahia Solutions Group SA. All rights reserved.
  *
- * 1/ GPL
- * ======================================================================================
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
  *
- * IF YOU DECIDE TO CHOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *     1/ GPL
+ *     ==================================================================================
  *
- * "This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
  *
- * As a special exception to the terms and conditions of version 2.0 of
- * the GPL (or any later version), you may redistribute this Program in connection
- * with Free/Libre and Open Source Software ("FLOSS") applications as described
- * in Jahia's FLOSS exception. You should have received a copy of the text
- * describing the FLOSS exception, also available here:
- * http://www.jahia.com/license"
- *
- * 2/ JSEL - Commercial and Supported Versions of the program
- * ======================================================================================
- *
- * IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
- *
- * Alternatively, commercial and supported versions of the program - also known as
- * Enterprise Distributions - must be used in accordance with the terms and conditions
- * contained in a separate written agreement between you and Jahia Solutions Group SA.
- *
- * If you are unsure which license is appropriate for your use,
- * please contact the sales department at sales@jahia.com.
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  *
- * ==========================================================================================
- * =                                   ABOUT JAHIA                                          =
- * ==========================================================================================
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
  *
- * Rooted in Open Source CMS, Jahia’s Digital Industrialization paradigm is about
- * streamlining Enterprise digital projects across channels to truly control
- * time-to-market and TCO, project after project.
- * Putting an end to “the Tunnel effect”, the Jahia Studio enables IT and
- * marketing teams to collaboratively and iteratively build cutting-edge
- * online business solutions.
- * These, in turn, are securely and easily deployed as modules and apps,
- * reusable across any digital projects, thanks to the Jahia Private App Store Software.
- * Each solution provided by Jahia stems from this overarching vision:
- * Digital Factory, Workspace Factory, Portal Factory and eCommerce Factory.
- * Founded in 2002 and headquartered in Geneva, Switzerland,
- * Jahia Solutions Group has its North American headquarters in Washington DC,
- * with offices in Chicago, Toronto and throughout Europe.
- * Jahia counts hundreds of global brands and governmental organizations
- * among its loyal customers, in more than 20 countries across the globe.
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
  *
- * For more information, please visit http://www.jahia.com
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
  */
+package org.jahia.services.render.scripting.bundle;
 
 import org.apache.commons.lang.StringUtils;
 import org.osgi.framework.Bundle;
@@ -127,7 +98,7 @@ public class BundleScriptEngineManager extends ScriptEngineManager {
         this.globalScopeBindings = new SimpleBindings();
     }
 
-    public Object get(String key) { // ramzy: getBindingByKey
+    public Object get(String key) {
         return globalScopeBindings.get(key);
     }
 
@@ -312,21 +283,16 @@ public class BundleScriptEngineManager extends ScriptEngineManager {
             return null;
         }
 
-        if (bundle.getState() == Bundle.ACTIVE) {
-            Enumeration<URL> urls = bundle.findEntries(META_INF_SERVICES, SCRIPT_ENGINE_FACTORY_CLASS_NAME, false);
-            if (urls == null) {
-                return null;
+        URL u = bundle.getEntry(META_INF_SERVICES + "/" + SCRIPT_ENGINE_FACTORY_CLASS_NAME);
+        if (u != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(u.openStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                factoryCandidates.add(line.trim());
             }
-            while (urls.hasMoreElements()) {
-                URL u = urls.nextElement();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(u.openStream()));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    factoryCandidates.add(line.trim());
-                }
-            }
+            return factoryCandidates;
         }
-        return factoryCandidates;
+        return null;
     }
 
     public void removeScriptEngineFactoriesIfNeeded(Bundle bundle) {

178135a0100d69ab2fcf1c5755233d8786644f11
jkevan <kevan@jahia.com>
2016-10-07 19:12:56 +0200

    QA-8729: check session locale null in JahiaLuceneQueryFactoryImmpl

--- core/src/main/java/org/apache/jackrabbit/core/query/lucene/JahiaLuceneQueryFactoryImpl.java
+++ core/src/main/java/org/apache/jackrabbit/core/query/lucene/JahiaLuceneQueryFactoryImpl.java
@@ -222,7 +222,7 @@ public class JahiaLuceneQueryFactoryImpl extends LuceneQueryFactory {
                                             .getWorkspace().getName())
                                             || ((infos.getPublished() == null || "true"
                                                 .equals(infos.getPublished())) &&
-                                                (infos.getCheckInvalidLanguages()==null || !infos.getCheckInvalidLanguages().contains(getLocale().toString()))))) {
+                                                (infos.getCheckInvalidLanguages() == null || getLocale() == null || !infos.getCheckInvalidLanguages().contains(getLocale().toString()))))) {
                                 if (filter == Predicate.TRUE) { // <-- Added by jahia
                                     if ((hasFacets & FacetHandler.ONLY_FACET_COLUMNS) == 0) {
                                         Row row = null;

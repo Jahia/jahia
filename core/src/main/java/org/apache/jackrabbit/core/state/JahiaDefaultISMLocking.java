b024a41c1e20fc6de94200b8088a7428b412d11d
Sergiy Shyrkov <sshyrkov@jahia.com>
2017-04-10 15:00:42 +0200

    [QA-9444] Respect the writerPreference=true option and block the readers (not in the same thread) if there is a writer waiting for the lock

--- core/src/main/java/org/apache/jackrabbit/core/state/JahiaDefaultISMLocking.java
+++ core/src/main/java/org/apache/jackrabbit/core/state/JahiaDefaultISMLocking.java
@@ -1,18 +1,45 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+/**
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *                                 http://www.jahia.com
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *     Copyright (C) 2002-2017 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
  */
 package org.apache.jackrabbit.core.state;
 
@@ -22,8 +49,8 @@ import static org.apache.jackrabbit.core.TransactionContext.isSameThreadId;
 import org.apache.jackrabbit.core.id.ItemId;
 
 /**
- * Default item state locking strategy. The default strategy is simply to use
- * a single coarse-grained read-write lock over the entire workspace.
+ * DX Default item state locking strategy, which additionally to {@link DefaultISMLocking} includes a fix [QA-9444].
+ * The default strategy is simply to use a single coarse-grained read-write lock over the entire workspace.
  */
 public class JahiaDefaultISMLocking implements ISMLocking {
 
@@ -113,7 +140,7 @@ public class JahiaDefaultISMLocking implements ISMLocking {
             throws InterruptedException {
         Object currentId = getCurrentThreadId();
         while (writerId != null
-                ? (writerCount > 0 && !isSameThreadId(writerId, currentId))
+                ? ((writerCount > 0 || writerPreference && writersWaiting > 0) && !isSameThreadId(writerId, currentId))
                 : (writerPreference && writersWaiting > 0)) {
             wait();
         }

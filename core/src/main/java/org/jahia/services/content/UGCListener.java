fd60821a1e2111aa5b3ed4554406da1ba91ad318
Thomas Draier <tdraier@jahia.com>
2017-09-20 11:59:45 +0200

    QA-9749 : added comments, missing license, fixed logging / properties name, optimized code

--- core/src/main/java/org/jahia/services/content/UGCListener.java
+++ core/src/main/java/org/jahia/services/content/UGCListener.java
@@ -1,3 +1,46 @@
+/*
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2017 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
 package org.jahia.services.content;
 
 import org.jahia.api.Constants;
@@ -8,13 +51,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import javax.jcr.Value;
 import javax.jcr.observation.Event;
 import javax.jcr.observation.EventIterator;
 import java.util.*;
 
+/**
+ * Listener that watches creation of User Generated Content on published nodes.
+ *
+ * When a node that was existing in default workspace is modified in live, this listener stores the list of properties
+ * that are modified, and the list of mixins that are added.
+ *
+ * Node that are created in live workspace only are not touched by this listener.
+ *
+ */
 public class UGCListener extends DefaultEventListener {
-    private static Logger logger = LoggerFactory.getLogger(LastModifiedListener.class);
+    private static Logger logger = LoggerFactory.getLogger(UGCListener.class);
 
 
     public int getEventTypes() {
@@ -25,18 +78,17 @@ public class UGCListener extends DefaultEventListener {
     public void onEvent(final EventIterator events) {
         final JCRSessionWrapper eventSession = ((JCREventIterator) events).getSession();
 
-        final Locale sessionLocale = eventSession.getLocale();
         final JahiaUser user = eventSession.getUser();
 
-        final Map<String, Set<String>> t = new HashMap<>();
+        final Map<String, Set<String>> propertiesByNode = new HashMap<>();
         try {
             while (events.hasNext()) {
                 Event event = events.nextEvent();
                 JCRPropertyWrapper property = (JCRPropertyWrapper) eventSession.getItem(event.getPath());
                 JCRNodeWrapper node = property.getParent();
                 if (node.hasProperty("j:originWS") && node.getProperty("j:originWS").getString().equals("default")) {
-                    if (!t.containsKey(node.getIdentifier())) {
-                        t.put(node.getIdentifier(), new HashSet<String>());
+                    if (!propertiesByNode.containsKey(node.getIdentifier())) {
+                        propertiesByNode.put(node.getIdentifier(), new HashSet<String>());
                     }
                     if (property.getName().equals(Constants.JCR_MIXINTYPES)) {
                         if (node instanceof JCRNodeDecorator) {
@@ -46,40 +98,46 @@ public class UGCListener extends DefaultEventListener {
                             List<ExtendedNodeType> newMixins = new ArrayList<>(Arrays.asList(node.getMixinNodeTypes()));
                             newMixins.removeAll(Arrays.asList(((JCRNodeWrapperImpl) node).getOriginalMixinNodeTypes()));
                             for (ExtendedNodeType newMixin : newMixins) {
-                                t.get(node.getIdentifier()).add(property.getName() + "=" + newMixin.getName());
+                                propertiesByNode.get(node.getIdentifier()).add(property.getName() + "=" + newMixin.getName());
                             }
                         }
                     } else {
-                        t.get(node.getIdentifier()).add(property.getName());
+                        propertiesByNode.get(node.getIdentifier()).add(property.getName());
                     }
                 }
             }
 
-            if (!t.isEmpty()) {
+            if (!propertiesByNode.isEmpty()) {
                 JCRTemplate.getInstance().doExecuteWithSystemSessionAsUser(user, workspace, null, new JCRCallback<Object>() {
                     public Object doInJCR(JCRSessionWrapper s) throws RepositoryException {
-                        for (Map.Entry<String, Set<String>> entry : t.entrySet()) {
-                            JCRNodeWrapper n = s.getNodeByIdentifier(entry.getKey());
-                            if (!n.isNodeType("jmix:liveProperties")) {
-                                n.addMixin("jmix:liveProperties");
+                        Set<Session> sessions = new HashSet<>();
+                        for (Map.Entry<String, Set<String>> entry : propertiesByNode.entrySet()) {
+                            JCRNodeWrapper name = s.getNodeByIdentifier(entry.getKey());
+                            if (!name.isNodeType("jmix:liveProperties")) {
+                                name.addMixin("jmix:liveProperties");
                             }
-                            JCRPropertyWrapper p = n.hasProperty("j:liveProperties") ? n.getProperty("j:liveProperties") : n.setProperty("j:liveProperties", new Value[0]);
-                            for (JCRValueWrapper valueWrapper : p.getValues()) {
+                            JCRPropertyWrapper property = name.hasProperty("j:liveProperties") ? name.getProperty("j:liveProperties") : name.setProperty("j:liveProperties", new Value[0]);
+                            for (JCRValueWrapper valueWrapper : property.getValues()) {
                                 entry.getValue().remove(valueWrapper.getString());
                             }
-                            for (String v : entry.getValue()) {
-                                p.addValue(v);
+                            for (String value : entry.getValue()) {
+                                property.addValue(value);
                             }
-                            n.getRealNode().getSession().save();
+                            sessions.add(name.getRealNode().getSession());
                         }
 
+                        for (Session session : sessions) {
+                            if (session.hasPendingChanges()) {
+                                session.save();
+                            }
+                        }
                         return null;
                     }
                 });
 
             }
         } catch (RepositoryException e) {
-            e.printStackTrace();
+            logger.error("Cannot store live properties changes",e);
         }
     }
 }

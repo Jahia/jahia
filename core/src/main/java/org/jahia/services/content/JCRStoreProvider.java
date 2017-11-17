cdee60244a03766ed972c780c1d67b314652ea2b
jkevan <kevan@jahia.com>
2017-11-17 15:36:05 +0100

    QA-10015: correctly close session after registering namespaces in JCRStoreProvider to avoid internal session living in memory for nothing

--- core/src/main/java/org/jahia/services/content/JCRStoreProvider.java
+++ core/src/main/java/org/jahia/services/content/JCRStoreProvider.java
@@ -633,8 +633,9 @@ public class JCRStoreProvider implements Comparable<JCRStoreProvider> {
      * @throws RepositoryException
      */
     public void registerNamespaces() throws RepositoryException {
-        Session s = getSystemSession().getProviderSession(this);
+        JCRSessionWrapper sessionWrapper = getSystemSession();
         try {
+            Session s = getSystemSession().getProviderSession(this);
             NamespaceRegistry providerNamespaceRegistry = s.getWorkspace().getNamespaceRegistry();
             if (providerNamespaceRegistry != null) {
                 for (Map.Entry<String, String> namespaceEntry : NodeTypeRegistry.getInstance().getNamespaces().entrySet()) {
@@ -649,7 +650,7 @@ public class JCRStoreProvider implements Comparable<JCRStoreProvider> {
                 }
             }
         } finally {
-            s.logout();
+            sessionWrapper.logout();
         }
     }
 

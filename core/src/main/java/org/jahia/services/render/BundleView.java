231bcefbbb2458861c0cc86de3f39ffc0d7b4275
Sergiy Shyrkov <sshyrkov@jahia.com>
2017-12-19 00:04:38 +0100

    [QA-10154] Updated copyright year in file headers to 2018

--- core/src/main/java/org/jahia/services/render/BundleView.java
+++ core/src/main/java/org/jahia/services/render/BundleView.java
@@ -5,7 +5,7 @@
  *
  *                                 http://www.jahia.com
  *
- *     Copyright (C) 2002-2017 Jahia Solutions Group SA. All rights reserved.
+ *     Copyright (C) 2002-2018 Jahia Solutions Group SA. All rights reserved.
  *
  *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
  *     1/GPL OR 2/JSEL

0509288b9e393009bf5d243128ff9e7cee41f412
Florent BOURASSE <fbourasse@jahia.com>
2018-01-17 13:50:00 +0100

    QA-10290: CacheKeyPartGenerator to handle full read-only mode.

---
+++ core/src/main/java/org/jahia/services/render/filter/cache/ReadOnlyCacheKeyPartGenerator.java
@@ -0,0 +1,79 @@
+/**
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2016 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * 2/ JSEL - Commercial and Supported Versions of the program
+ * ===================================================================================
+ *
+ * IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ * Alternatively, commercial and supported versions of the program - also known as Enterprise Distributions - must be
+ * used in accordance with the terms and conditions contained in a separate written agreement between you and Jahia
+ * Solutions Group SA.
+ *
+ * If you are unsure which license is appropriate for your use, please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.services.render.filter.cache;
+
+import java.util.Properties;
+import org.apache.commons.lang.StringUtils;
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.Resource;
+import org.jahia.settings.readonlymode.ReadOnlyModeController;
+
+/*
+ * Cache key used to take into account the full read-only mode
+ */
+public class ReadOnlyCacheKeyPartGenerator implements CacheKeyPartGenerator {
+
+    public static final String CACHE_READ_ONLY = "cache.readOnly";
+    public static final String READ_ONLY = "read_only";
+    public static final String READ_ONLY_KEY = "_ro_";
+
+    @Override
+    public String getKey() {
+        return READ_ONLY;
+    }
+
+    @Override
+    public String getValue(Resource resource, RenderContext renderContext, Properties properties) {
+
+        if (Boolean.TRUE.toString().equals(properties.getProperty(CACHE_READ_ONLY))) {
+            return READ_ONLY_KEY;
+        }
+        return StringUtils.EMPTY;
+    }
+
+    @Override
+    public String replacePlaceholders(RenderContext renderContext, String keyPart) {
+        final String readOnlyStatus = ReadOnlyModeController.getInstance().getReadOnlyStatus().name();
+        return StringUtils.replace(keyPart, READ_ONLY_KEY, readOnlyStatus);
+    }
+}

9910f534c5129d93a1b0c490101528984905f075
Florent BOURASSE <fbourasse@jahia.com>
2018-03-02 19:34:50 +0100

    QA-9071: Addition of a unit test to test different URLs

---
+++ core/src/test/java/org/jahia/bin/LoginTest.java
@@ -0,0 +1,70 @@
+/**
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2016 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * 2/ JSEL - Commercial and Supported Versions of the program
+ * ===================================================================================
+ *
+ * IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ * Alternatively, commercial and supported versions of the program - also known as Enterprise Distributions - must be
+ * used in accordance with the terms and conditions contained in a separate written agreement between you and Jahia
+ * Solutions Group SA.
+ *
+ * If you are unsure which license is appropriate for your use, please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.bin;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginTest {
+
+    @Test
+    public void testRemoveErrorParameter() {
+        final String baseUrl = "https://wwww.jahia.com/cms/login";
+
+        String url = baseUrl + "?loginError=tutu";
+        String result = Login.removeErrorParameter(url);
+        Assert.assertEquals(baseUrl, result);
+
+        url = baseUrl + "?loginError=tutu&tata=toto";
+        result = Login.removeErrorParameter(url);
+        Assert.assertEquals(baseUrl + "?tata=toto", result);
+
+        url = baseUrl + "?titi=tete&loginError=tutu&tata=toto";
+        result = Login.removeErrorParameter(url);
+        Assert.assertEquals(baseUrl + "?titi=tete&tata=toto", result);
+
+        url = baseUrl + "?titi=tete&tata=toto&loginError=tutu";
+        result = Login.removeErrorParameter(url);
+        Assert.assertEquals(baseUrl + "?titi=tete&tata=toto", result);
+    }
+}

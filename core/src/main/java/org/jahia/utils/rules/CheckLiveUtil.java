235cdf1806fc843d8af1525001e24a09a8515db0
wassek <wassek@jahia.com>
2018-03-22 08:11:57 +0100

    QA-9945: Missing content in live, but impossible to publish. If content is removed from live workspace (e.g. with jcrBrowser) the default content is not publishable. With the new rule it get a publishable state.

---
+++ core/src/main/java/org/jahia/utils/rules/CheckLiveUtil.java
@@ -0,0 +1,75 @@
+package org.jahia.utils.rules;
+
+import javax.jcr.RepositoryException;
+
+import org.jahia.services.content.JCRNodeIteratorWrapper;
+import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.JCRObservationManager;
+import org.jahia.services.content.JCRSessionFactory;
+import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.services.content.rules.NodeFact;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.commons.lang.StringUtils;
+import org.jahia.api.Constants;
+
+
+public class CheckLiveUtil {
+
+	private static final Logger logger = LoggerFactory.getLogger(CheckLiveUtil.class);
+
+	public static boolean checkProperty(NodeFact node, String propertyName, String propertyValue) {
+
+		String path = "";
+		try {
+			path = node.getPath();
+			JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(Constants.EDIT_WORKSPACE);
+			if (session.itemExists(path)) {
+				JCRNodeWrapper defNode = session.getNode(path);
+				if (defNode.hasProperty(propertyName) && StringUtils.equals(defNode.getPropertyAsString(propertyName), propertyValue)) {
+						return true;
+				}
+			}
+		} catch (RepositoryException ex) {
+			logger.error("Exception when checking the " + propertyName + " on node " + path, ex);
+		}
+		return false;
+	}
+
+	public static void removeProperyInWorkspaceFromNode(NodeFact node, String propertyName, String workspace) {
+
+		try {
+			JCRObservationManager.pushEventListenersAvailableDuringPublishOnly();
+			JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentUserSession(workspace);
+			if (session.itemExists(node.getPath())) {
+				String otherWorkspace = Constants.EDIT_WORKSPACE.equals(workspace) ? Constants.LIVE_WORKSPACE : Constants.EDIT_WORKSPACE;
+				JCRSessionWrapper otherSession = JCRSessionFactory.getInstance().getCurrentUserSession(otherWorkspace);
+				removePropertyFromSubnode(session.getNode(node.getPath()), propertyName, otherSession);
+				session.save();
+			}
+		} catch (RepositoryException ex) {
+			logger.error("cannot remove Property " + propertyName, ex);
+		}
+		finally {
+			JCRObservationManager.popEventListenersAvailableDuringPublishOnly();
+		}
+
+	}
+	
+	private static void removePropertyFromSubnode(JCRNodeWrapper node, String propertyName, JCRSessionWrapper otherSession) throws RepositoryException {
+		if (node.hasProperty(propertyName)) {
+			node.getProperty(propertyName).remove();
+		}
+		if (node.hasNodes()) {
+			JCRNodeIteratorWrapper it = node.getNodes();
+			while (it.hasNext()) {
+				JCRNodeWrapper subNode = (JCRNodeWrapper)it.next();
+				if (!otherSession.itemExists(subNode.getPath())) {
+					removePropertyFromSubnode(subNode, propertyName, otherSession);
+				}
+			}
+		}
+		
+	}
+
+}

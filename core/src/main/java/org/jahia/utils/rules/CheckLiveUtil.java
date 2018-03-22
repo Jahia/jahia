811169fc189661de209d7de52051895e09edab79
wassek <wassek@jahia.com>
2018-03-22 15:33:40 +0100

    javadoc and formated.

--- core/src/main/java/org/jahia/utils/rules/CheckLiveUtil.java
+++ core/src/main/java/org/jahia/utils/rules/CheckLiveUtil.java
@@ -14,8 +14,8 @@ import org.apache.commons.lang.StringUtils;
 import org.jahia.api.Constants;
 
 /**
- * drools Util class to remove properties from other workspace (for instance j:published) if a node is 
- * removed from the other workspace.
+ * drools Util class to remove properties from other workspace (for instance
+ * j:published) if a node is removed from the other workspace.
  *
  * @author wassek
  */
@@ -23,16 +23,30 @@ public class CheckLiveUtil {
 
 	private static final Logger logger = LoggerFactory.getLogger(CheckLiveUtil.class);
 
+	/*
+	 * Checks if the node has a specific property with specific value
+	 * 
+	 * @param node rules NodeFact which contains the node which should be
+	 * checked
+	 * 
+	 * @param propertyName Name of the property
+	 * 
+	 * @param propertyValue Value of the property to check
+	 * 
+	 * @return <code>true</code> if property with value exists
+	 */
 	public static boolean checkProperty(NodeFact node, String propertyName, String propertyValue) {
 
 		String path = "";
 		try {
 			path = node.getPath();
-			JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentSystemSession(Constants.EDIT_WORKSPACE, node.getSession().getLocale(), null);
+			JCRSessionWrapper session = JCRSessionFactory.getInstance()
+					.getCurrentSystemSession(Constants.EDIT_WORKSPACE, node.getSession().getLocale(), null);
 			if (session.itemExists(path)) {
 				JCRNodeWrapper defNode = session.getNode(path);
-				if (defNode.hasProperty(propertyName) && StringUtils.equals(defNode.getPropertyAsString(propertyName), propertyValue)) {
-						return true;
+				if (defNode.hasProperty(propertyName)
+						&& StringUtils.equals(defNode.getPropertyAsString(propertyName), propertyValue)) {
+					return true;
 				}
 			}
 		} catch (RepositoryException ex) {
@@ -41,40 +55,53 @@ public class CheckLiveUtil {
 		return false;
 	}
 
+	/*
+	 * Remove a property from a node in a specific workspace
+	 * 
+	 * @param node rules NodeFact which contains the node which should be
+	 * checked
+	 * 
+	 * @param propertyName Name of the property
+	 * 
+	 * @param workspace workspace where the property should removed
+	 */
 	public static void removeProperyInWorkspaceFromNode(NodeFact node, String propertyName, String workspace) {
 
 		try {
 			JCRObservationManager.pushEventListenersAvailableDuringPublishOnly();
-			JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentSystemSession(workspace, node.getSession().getLocale(), null);
+			JCRSessionWrapper session = JCRSessionFactory.getInstance().getCurrentSystemSession(workspace,
+					node.getSession().getLocale(), null);
 			if (session.itemExists(node.getPath())) {
-				String otherWorkspace = Constants.EDIT_WORKSPACE.equals(workspace) ? Constants.LIVE_WORKSPACE : Constants.EDIT_WORKSPACE;
-				JCRSessionWrapper otherSession = JCRSessionFactory.getInstance().getCurrentSystemSession(otherWorkspace, node.getSession().getLocale(), null);
+				String otherWorkspace = Constants.EDIT_WORKSPACE.equals(workspace) ? Constants.LIVE_WORKSPACE
+						: Constants.EDIT_WORKSPACE;
+				JCRSessionWrapper otherSession = JCRSessionFactory.getInstance().getCurrentSystemSession(otherWorkspace,
+						node.getSession().getLocale(), null);
 				removePropertyFromSubnode(session.getNode(node.getPath()), propertyName, otherSession);
 				session.save();
 			}
 		} catch (RepositoryException ex) {
 			logger.error("cannot remove Property " + propertyName, ex);
-		}
-		finally {
+		} finally {
 			JCRObservationManager.popEventListenersAvailableDuringPublishOnly();
 		}
 
 	}
-	
-	private static void removePropertyFromSubnode(JCRNodeWrapper node, String propertyName, JCRSessionWrapper otherSession) throws RepositoryException {
+
+	private static void removePropertyFromSubnode(JCRNodeWrapper node, String propertyName,
+			JCRSessionWrapper otherSession) throws RepositoryException {
 		if (node.hasProperty(propertyName)) {
 			node.getProperty(propertyName).remove();
 		}
 		if (node.hasNodes()) {
 			JCRNodeIteratorWrapper it = node.getNodes();
 			while (it.hasNext()) {
-				JCRNodeWrapper subNode = (JCRNodeWrapper)it.next();
+				JCRNodeWrapper subNode = (JCRNodeWrapper) it.next();
 				if (!otherSession.itemExists(subNode.getPath())) {
 					removePropertyFromSubnode(subNode, propertyName, otherSession);
 				}
 			}
 		}
-		
+
 	}
 
 }

1fe471f3e60fd9a847a51c14271b5f7ad51feb68
Thomas Draier <tdraier@jahia.com>
2018-06-29 15:55:38 +0200

    BACKLOG-8114 : Added action item to execute custom JS

---
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/widget/toolbar/action/JSActionItem.java
@@ -0,0 +1,74 @@
+/**
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2018 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.ajax.gwt.client.widget.toolbar.action;
+
+import org.jahia.ajax.gwt.client.data.node.GWTJahiaNode;
+
+/**
+ * An action item for executing JS
+ * 
+ */
+public class JSActionItem extends NodeTypeAwareBaseActionItem {
+    private static final long serialVersionUID = -1317342305404063292L;
+    protected String js;
+
+    public void onComponentSelection() {
+        GWTJahiaNode gwtJahiaNodes = linker.getSelectionContext().getSingleSelection();
+
+        String res = doIt(js, gwtJahiaNodes.getPath());
+
+//        JSONValue jsondata = JSONParser.parseStrict(res);
+    }
+
+    public native String doIt(String js, String path) /*-{
+        var call = eval("(function (path) { return " + js + " })");
+        return JSON.stringify(call(path));
+    }-*/;
+
+    public void setJs(String js) {
+        this.js = js;
+    }
+
+}
+

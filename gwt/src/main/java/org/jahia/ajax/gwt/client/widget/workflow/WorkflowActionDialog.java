6ce752d7c2bc755f8bdb5678a1c9b01a1b950640
Sergiy Shyrkov <sshyrkov@jahia.com>
2018-10-08 10:41:39 +0200

    [QA-11081] Adjusted ordering of workflow task buttons: they are now ordered by priority, defined in the workflow definition file (*.bpmn2). Cancel button is always the left most one.

--- gwt/src/main/java/org/jahia/ajax/gwt/client/widget/workflow/WorkflowActionDialog.java
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/widget/workflow/WorkflowActionDialog.java
@@ -171,7 +171,7 @@ public class WorkflowActionDialog extends LayoutContainer {
                 }
             });
             cancel.addStyleName("button-cancel");
-            buttonsBar.add(cancel);
+            buttonsBar.insert(cancel, 0);
         }
     }
 
@@ -188,15 +188,12 @@ public class WorkflowActionDialog extends LayoutContainer {
     }
 
     public void initStartWorkflowDialog(final GWTJahiaWorkflowDefinition workflowDefinition) {
-        Button button = generateStartWorkflowButton(workflowDefinition);
-        buttonsBar.insert(button, buttonsBar.getItems().size() > 0 ? buttonsBar.getItems().size() - 1 : 0);
+        buttonsBar.add(generateStartWorkflowButton(workflowDefinition));
     }
 
     public void initExecuteActionDialog(final GWTJahiaWorkflowTask task) {
-        List<Button> buttons = generateActionButtons(task);
-
-        for (Button button : buttons) {
-            buttonsBar.insert(button, buttonsBar.getItems().size() > 0 ? buttonsBar.getItems().size() - 1 : 0);
+        for (Button button : generateActionButtons(task)) {
+            buttonsBar.add(button);
         }
 
     }

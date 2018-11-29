dcc6667956dbf7212da574c2fec1d2b39a088305
David Griffon <dgriffon@jahia.com>
2018-11-29 12:08:13 +0100

    BACKLOG-9109 : add keyboard keys support in cancel pop up of edit engine

--- gwt/src/main/java/org/jahia/ajax/gwt/client/widget/contentengine/AbstractContentEngine.java
+++ gwt/src/main/java/org/jahia/ajax/gwt/client/widget/contentengine/AbstractContentEngine.java
@@ -2,44 +2,44 @@
  * ==========================================================================================
  * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
  * ==========================================================================================
- *
- *                                 http://www.jahia.com
- *
- *     Copyright (C) 2002-2018 Jahia Solutions Group SA. All rights reserved.
- *
- *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
- *     1/GPL OR 2/JSEL
- *
- *     1/ GPL
- *     ==================================================================================
- *
- *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- *
- *     2/ JSEL - Commercial and Supported Versions of the program
- *     ===================================================================================
- *
- *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
- *
- *     Alternatively, commercial and supported versions of the program - also known as
- *     Enterprise Distributions - must be used in accordance with the terms and conditions
- *     contained in a separate written agreement between you and Jahia Solutions Group SA.
- *
- *     If you are unsure which license is appropriate for your use,
- *     please contact the sales department at sales@jahia.com.
+ * <p>
+ * http://www.jahia.com
+ * <p>
+ * Copyright (C) 2002-2018 Jahia Solutions Group SA. All rights reserved.
+ * <p>
+ * THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ * 1/GPL OR 2/JSEL
+ * <p>
+ * 1/ GPL
+ * ==================================================================================
+ * <p>
+ * IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * <p>
+ * 2/ JSEL - Commercial and Supported Versions of the program
+ * ===================================================================================
+ * <p>
+ * IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ * <p>
+ * Alternatively, commercial and supported versions of the program - also known as
+ * Enterprise Distributions - must be used in accordance with the terms and conditions
+ * contained in a separate written agreement between you and Jahia Solutions Group SA.
+ * <p>
+ * If you are unsure which license is appropriate for your use,
+ * please contact the sales department at sales@jahia.com.
  */
 package org.jahia.ajax.gwt.client.widget.contentengine;
 
@@ -81,6 +81,8 @@ import org.jahia.ajax.gwt.client.widget.toolbar.action.LanguageSwitcherActionIte
 
 import java.util.*;
 
+import static com.extjs.gxt.ui.client.widget.MessageBox.QUESTION;
+import static com.extjs.gxt.ui.client.widget.MessageBox.YESNO;
 import static org.jahia.ajax.gwt.client.widget.contentengine.SeoTabItem.VANITY_MAPPINGS;
 
 /**
@@ -135,6 +137,9 @@ public abstract class AbstractContentEngine extends LayoutContainer implements N
     protected final Map<String, List<GWTJahiaNodeProperty>> changedI18NProperties = new HashMap<String, List<GWTJahiaNodeProperty>>();
 
     protected String parentPath;
+
+    private transient boolean noFocused = true;
+    private transient MessageBox confirmCancelBox;
 
     protected AbstractContentEngine(GWTEngineConfiguration config, Linker linker, String parentPath, boolean skipRefreshOnSave) {
         this.config = config;
@@ -704,11 +709,23 @@ public abstract class AbstractContentEngine extends LayoutContainer implements N
         //Handling escape button to leave the engine
         cancelConfirmEscapeListener = Event.addNativePreviewHandler(new Event.NativePreviewHandler() {
             public void onPreviewNativeEvent(final Event.NativePreviewEvent event) {
+                NativeEvent nEvent = event.getNativeEvent();
                 if (!cancelConfirmOpen) {
-                    NativeEvent nEvent = event.getNativeEvent();
                     if ("keydown".equals(nEvent.getType())) {
                         if (event.getNativeEvent().getKeyCode() == KeyCodes.KEY_ESCAPE) {
                             cancelAndClose();
+                        }
+                    }
+                } else {
+                    if ("keydown".equals(nEvent.getType())) {
+                        if (confirmCancelBox != null && nEvent.getKeyCode() == KeyCodes.KEY_LEFT || nEvent.getKeyCode() == KeyCodes.KEY_RIGHT || nEvent.getKeyCode() == KeyCodes.KEY_TAB) {
+                            confirmCancelBox.getDialog().getButtonBar().getItem(noFocused ? 0 : 1).focus();
+                            noFocused = !noFocused;
+                            nEvent.preventDefault();
+                        }
+                        if (confirmCancelBox != null && nEvent.getKeyCode() == KeyCodes.KEY_ESCAPE) {
+                            cancelConfirmOpen = false;
+                            confirmCancelBox.close();
                         }
                     }
                 }
@@ -718,15 +735,22 @@ public abstract class AbstractContentEngine extends LayoutContainer implements N
 
     protected void confirmCancel() {
         cancelConfirmOpen = true;
-        MessageBox.confirm(Messages.get("message.confirm.unsavedTitle","Changes won't be saved"),
-                Messages.get("message.confirm.unsavedModifications","Close without saving?"), new Listener<MessageBoxEvent>() {
-                    @Override public void handleEvent(MessageBoxEvent boxEvent) {
-                        if (Dialog.YES.equalsIgnoreCase(boxEvent.getButtonClicked().getItemId())) {
-                            close();
-                        } else if(Dialog.NO.equalsIgnoreCase(boxEvent.getButtonClicked().getItemId())) {
-                            cancelConfirmOpen = false;
-                        }
-                    }
-                });
+
+        confirmCancelBox = new MessageBox();
+        confirmCancelBox.setTitleHtml(Messages.get("message.confirm.unsavedTitle", "Changes won't be saved"));
+        confirmCancelBox.setMessage(Messages.get("message.confirm.unsavedModifications", "Close without saving?"));
+        confirmCancelBox.addCallback(new Listener<MessageBoxEvent>() {
+            @Override
+            public void handleEvent(MessageBoxEvent boxEvent) {
+                if (boxEvent.getButtonClicked() != null && Dialog.YES.equalsIgnoreCase(boxEvent.getButtonClicked().getItemId())) {
+                    close();
+                } else {
+                    cancelConfirmOpen = false;
+                }
+            }
+        });
+        confirmCancelBox.setIcon(QUESTION);
+        confirmCancelBox.setButtons(YESNO);
+        confirmCancelBox.show();
     }
 }

aaa6ec084ce6c83328a4b2b2bf73cba11720a27d
Sergiy Shyrkov <sshyrkov@jahia.com>
2018-12-18 12:52:03 +0100

    [BACKLOG-9215] Reverted the behaviour for the SystemPropertiesFactoryBean to the original one. I.e. we only consider system properties, prefixed with jahiaConfig., in our DX configuration.

--- core/src/main/java/org/jahia/settings/SystemPropertiesFactoryBean.java
+++ core/src/main/java/org/jahia/settings/SystemPropertiesFactoryBean.java
@@ -44,95 +44,50 @@
 package org.jahia.settings;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
-import org.jahia.bin.listeners.JahiaContextLoaderListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 
 /**
- * Properties factory bean that retrieves matching system properties.
+ * Properties factory bean that retrieves the matching system properties.
  * 
  * @author Sergiy Shyrkov
  */
 public class SystemPropertiesFactoryBean extends PropertiesFactoryBean {
 
-    private static final Logger logger = LoggerFactory.getLogger(SystemPropertiesFactoryBean.class);
-
-    private static Map<String, String> toMap(Properties properties) {
-        Map<String, String> map = new HashMap<>(properties.size());
-        for (Object keyObj : properties.keySet()) {
-            String key = keyObj.toString();
-            map.put(key, properties.getProperty(key));
-        }
-        return map;
-    }
-
-    private boolean isPrefixMandatory;
+    private static final Logger logger = LoggerFactory.getLogger(EnvironmentVariablesPropertiesFactoryBean.class);
 
     private String prefix;
 
-    protected String convertKey(String key) {
-        return key;
-    }
-
     @Override
     protected Properties createProperties() throws IOException {
-        Properties props = processProperties(toMap(System.getProperties()));
+        Properties props = new Properties();
+
+        Properties sysProps = System.getProperties();
+        for (Object keyObj : sysProps.keySet()) {
+            String key = keyObj.toString();
+            if (StringUtils.isNotEmpty(prefix) && key.startsWith(prefix)) {
+                String value = sysProps.getProperty(key);
+                if (value != null) {
+                    props.put(StringUtils.substringAfter(key, prefix), value);
+                }
+            }
+        }
 
         logger.info("Detected the following system properties, which will be considered in the configuration: {}",
                 (Object) props.keySet().toArray());
 
-        setSystemProperties(props, false);
-
         return props;
     }
 
-    protected Properties processProperties(Map<String, String> availableProps) {
-        Properties props = new Properties();
-
-        for (Map.Entry<String, String> entry : availableProps.entrySet()) {
-            String key = entry.getKey();
-            String effectiveKey = null;
-            String value = entry.getValue();
-            if (value != null) {
-                if (StringUtils.isNotEmpty(prefix) && key.toLowerCase().startsWith(prefix)
-                        && key.length() > prefix.length()) {
-                    effectiveKey = key.substring(prefix.length());
-                } else if (!isPrefixMandatory) {
-                    // in this case we consider all properties no matter the prefix
-                    effectiveKey = key;
-                }
-
-                if (effectiveKey != null) {
-                    props.put(convertKey(effectiveKey), value);
-                }
-            }
-        }
-
-        return props;
+    protected String getPrefix() {
+        return prefix;
     }
 
     public void setPrefix(String prefix) {
-        this.prefix = prefix != null ? prefix.toLowerCase() : prefix;
-    }
-
-    public void setPrefixMandatory(boolean isPrefixMandatory) {
-        this.isPrefixMandatory = isPrefixMandatory;
-    }
-
-    protected void setSystemProperties(Properties props, boolean overwriteIfPresent) {
-        for (Object keyObj : props.keySet()) {
-            String key = keyObj.toString();
-
-            if (overwriteIfPresent || System.getProperty(key) == null) {
-                logger.info("Setting system property: {}", key);
-                JahiaContextLoaderListener.setSystemProperty(key, props.getProperty(key));
-            }
-        }
+        this.prefix = prefix;
     }
 }

ed9776ce69e4d8f2fd9fe56e403d059ac1dc2310
tdraier <tdraier@jahia.com>
2019-05-28 11:28:45 +0200

    BACKLOG-10348 : Add REST endpoint for karaf commands

---
+++ bundles/jcrcommands/src/main/java/org/jahia/bundles/rest/CommandResource.java
@@ -0,0 +1,73 @@
+/*
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2019 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.bundles.rest;
+
+import org.apache.karaf.jaas.boot.principal.RolePrincipal;
+import org.jahia.bundles.executor.KarafCommandExecutor;
+import org.jahia.osgi.BundleUtils;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+@Path("/api/commands")
+@Produces({ MediaType.APPLICATION_JSON })
+public class CommandResource {
+
+    @POST
+    @Path("{command}")
+    public Response executeCommand(@PathParam("command") String command, @Context UriInfo uriInfo, @Context SecurityContext securityContext) {
+        String output = null;
+        try {
+            output = getKarafCommand().executeCommand(command, 1000L, securityContext.getUserPrincipal(), new RolePrincipal("manager"), new RolePrincipal("admin"));
+        } catch (Exception e) {
+            return Response.serverError().entity(e.getMessage() + '\n').build();
+        }
+        return Response.ok(output).build();
+    }
+
+    private KarafCommandExecutor getKarafCommand() {
+        return BundleUtils.getOsgiService(KarafCommandExecutor.class, null);
+    }
+
+}

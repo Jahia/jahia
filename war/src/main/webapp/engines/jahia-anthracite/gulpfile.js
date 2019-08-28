9d3b7f0de943ecbf4583c4fe9de6a9f5c9ee3bb2
leebowyer <lbowyer@jahia.com>
2019-08-28 16:32:42 +0200

    DESIGN-295: Created a Watch on Anthracite project (#169)

--- war/src/main/webapp/engines/jahia-anthracite/gulpfile.js
+++ war/src/main/webapp/engines/jahia-anthracite/gulpfile.js
@@ -5,6 +5,8 @@ const minify = require('gulp-minify');
 const uglify = require('gulp-uglify');
 const javascriptObfuscator = require('gulp-javascript-obfuscator');
 const concat = require('gulp-concat');
+const watch = require('gulp-watch');
+const gutil = require('gulp-util');
 
 const jsFiles = ['js/dependencies.js', 'js/methods.js', 'js/eventListeners.js', 'js/localisedString.js', 'js/edit.js'];
 const jsDest = 'js/dist';
@@ -24,6 +26,31 @@ gulp.task('build', done => {
         }))
         .pipe(gulp.dest('./js/dist/build/'));
     done();
+});
+
+gulp.task('watch', () => {
+    return watch('js/*.js', function () {
+        const h = new Date().getHours();
+        const m = new Date().getMinutes();
+        const s = new Date().getSeconds();
+
+        console.log("Build " + h + ':' + m + ':' + s);
+
+        if (gutil.env.dest === undefined) {
+            console.log("Dest env variable not set ! Use --dest=");
+        } else {
+            gulp.src(jsFiles)
+                .pipe(concat('anthracite.js'))
+                .pipe(gulp.dest(jsDest));
+            gulp.src(['js/dist/anthracite.js'])
+                .pipe(minify())
+                .pipe(uglify())
+                .pipe(javascriptObfuscator({
+                    compact: true
+                }))
+                .pipe(gulp.dest(gutil.env.dest));
+        }
+    })
 });
 
 gulp.task('generate', gulp.series('concat', 'build'));

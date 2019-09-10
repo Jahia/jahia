5613391fec354a8ab333cc600597ff16ea36bc3d
David Griffon <dgriffon@jahia.com>
2019-09-10 09:42:31 +0200

    Revert "QA-11864: fix session leak (#180)"
    This reverts commit c20e878

--- core/src/main/java/org/jahia/params/valves/CookieAuthValveImpl.java
+++ core/src/main/java/org/jahia/params/valves/CookieAuthValveImpl.java
@@ -49,7 +49,6 @@ import org.jahia.pipelines.PipelineException;
 import org.jahia.pipelines.valves.ValveContext;
 import org.jahia.registries.ServicesRegistry;
 import org.jahia.services.content.JCRSessionFactory;
-import org.jahia.services.content.JCRTemplate;
 import org.jahia.services.content.decorator.JCRUserNode;
 import org.jahia.services.usermanager.JahiaUser;
 import org.jahia.settings.SettingsBean;

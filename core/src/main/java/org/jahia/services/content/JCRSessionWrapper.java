e3044123e1c4cd392c7d3c02bf1c4b074baa7cd5
David Griffon <dgriffon@jahia.com>
2020-01-07 14:48:40 +0100

    QA-12394 : fix mandatory auto created properties + tests

--- core/src/main/java/org/jahia/services/content/JCRSessionWrapper.java
+++ core/src/main/java/org/jahia/services/content/JCRSessionWrapper.java
@@ -562,7 +562,8 @@ public class JCRSessionWrapper implements Session {
                         if (propertyDefinition.isMandatory() &&
                                 propertyDefinition.getRequiredType() != PropertyType.WEAKREFERENCE &&
                                 propertyDefinition.getRequiredType() != PropertyType.REFERENCE &&
-                                !propertyDefinition.isProtected() && !(propertyDefinition.isAutoCreated() && node.isNew()) &&
+                                !propertyDefinition.isProtected() &&
+                                !(propertyDefinition.isAutoCreated() && node.isNew()) &&
                                 (!propertyDefinition.isInternationalized() || getLocale() != null) &&
                                 (
                                         !node.hasProperty(propertyName) ||

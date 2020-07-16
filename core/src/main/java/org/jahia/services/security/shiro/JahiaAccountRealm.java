9746228a67c32fb2672afb32142e19931725ff93
Yousria <ybenchadi@jahia.com>
2020-07-16 11:44:41 +0200

    BACKLOG-14163: Use new Jahia realm and authc (#569)

---
+++ core/src/main/java/org/jahia/services/security/shiro/JahiaAccountRealm.java
@@ -0,0 +1,120 @@
+/*
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2020 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.services.security.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.jahia.registries.ServicesRegistry;
+import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.JCRTemplate;
+import org.jahia.services.usermanager.JahiaUser;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.RepositoryException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class JahiaAccountRealm extends AuthorizingRealm {
+    public static final Logger logger = LoggerFactory.getLogger(JahiaAccountRealm.class);
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        if (principals.getPrimaryPrincipal() instanceof JahiaUser) {
+            JahiaUser user = (JahiaUser) principals.getPrimaryPrincipal();
+
+            try {
+                return new SimpleAuthorizationInfo(JCRTemplate.getInstance().doExecute(user, null, null, session -> {
+                    JCRNodeWrapper node = session.getNode("/");
+
+                    if (user.isRoot()) {
+                        return node.getAvailableRoles().entrySet().stream().flatMap(e -> e.getValue().stream()).map(JCRNodeWrapper::getName).collect(Collectors.toSet());
+                    } else {
+                        return getRoles(user, node);
+                    }
+                }));
+            } catch (RepositoryException e) {
+                logger.error("Cannot login", e);
+            }
+        }
+
+        return new SimpleAuthorizationInfo(Collections.emptySet());
+    }
+
+    @NotNull
+    private Set<String> getRoles(JahiaUser userNode, JCRNodeWrapper node) {
+        Set<String> roles = new HashSet<>();
+        Map<String, Map<String, String>> actualAclEntries = node.getActualAclEntries();
+        for (Map.Entry<String, Map<String, String>> actualAclEntry : actualAclEntries.entrySet()) {
+            String principal = actualAclEntry.getKey();
+            final String principalName = principal.substring(2);
+            if ((principal.charAt(0) == 'u' && principalName.equals(userNode.getName())) ||
+                    (principal.charAt(0) == 'g' && ServicesRegistry.getInstance().getJahiaGroupManagerService().lookupGroup(null, principalName).isMember(userNode.getLocalPath()))
+            ) {
+                for (Map.Entry<String, String> aclEntry : actualAclEntry.getValue().entrySet()) {
+                    if ("GRANT".equals(aclEntry.getValue())) {
+                        roles.add(aclEntry.getKey());
+                    }
+                }
+            }
+        }
+        return roles;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        return new SimpleAuthenticationInfo(token.getPrincipal(), token.getPrincipal(), getName());
+    }
+
+}
---
+++ core/src/main/java/org/jahia/services/security/shiro/JahiaAuthenticationFilter.java
@@ -0,0 +1,99 @@
+/*
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2020 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.services.security.shiro;
+
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.apache.shiro.web.util.WebUtils;
+import org.jahia.api.Constants;
+import org.jahia.params.valves.AuthValveContext;
+import org.jahia.pipelines.Pipeline;
+import org.jahia.services.content.JCRSessionFactory;
+import org.jahia.services.usermanager.JahiaUser;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class JahiaAuthenticationFilter extends AuthenticatingFilter {
+    private Pipeline authPipeline;
+    private JCRSessionFactory sessionFactory;
+
+    public void setAuthPipeline(Pipeline authPipeline) {
+        this.authPipeline = authPipeline;
+    }
+
+    public void setSessionFactory(JCRSessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) throws Exception {
+        return new JahiaUserToken((JahiaUser) WebUtils.toHttp(request).getSession().getAttribute(Constants.SESSION_USER));
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        AuthValveContext authValveContext = new AuthValveContext(WebUtils.toHttp(request), WebUtils.toHttp(response), sessionFactory);
+        authPipeline.invoke(authValveContext);
+
+        JahiaUser jahiaUser = sessionFactory.getCurrentUser();
+        boolean userSet = jahiaUser != null;
+
+        if (userSet) {
+            executeLogin(request, response);
+        } else {
+            saveRequestAndRedirectToLogin(request, response);
+        }
+
+        return userSet;
+    }
+
+    @Override
+    protected void redirectToLogin(ServletRequest request, ServletResponse response) throws IOException {
+        String loginUrl = getLoginUrl() + "?redirect=" + WebUtils.toHttp(request).getRequestURI();
+        WebUtils.issueRedirect(request, response, loginUrl);
+    }
+
+}

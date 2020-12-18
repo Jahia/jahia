a316afca2daae38f45359af3d83ff94a13c08fa4
Thomas Draier <tdraier@jahia.com>
2020-12-18 11:09:12 +0100

    TECH-315: Skip migration patches when not needed. Fixed patch naming (#712)

--- war/src/data/resources/patches/groovy/0.modules.beforeContextInitializing.groovy
+++ war/src/data/resources/patches/groovy/0.modules.beforeContextInitializing.groovy
@@ -1,12 +1,11 @@
 import org.apache.commons.io.FileUtils
 import org.jahia.bin.Jahia
-import org.jahia.osgi.FrameworkService
 import org.jahia.settings.SettingsBean
 import org.jahia.tools.patches.Patcher
 
 setResult("keep")
 
-if (!FrameworkService.getInstance().isStarted() && !Patcher.getInstance().getJahiaPreviousVersion().equals(Jahia.JAHIA_VERSION)) {
+if (Patcher.getInstance().getJahiaPreviousVersion() != null && !Patcher.getInstance().getJahiaPreviousVersion().equals(Jahia.JAHIA_VERSION)) {
     def file = new File(SettingsBean.getInstance().getJahiaVarDiskPath() + "/bundles-deployed")
     if (file.exists()) {
         log.info("*** Migration detected, cleaning all previous bundles and instructing for modules reinstallation ***")

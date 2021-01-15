c81a09e5e836f278616750355d70f2263e043ef0
David Griffon <dgriffon@jahia.com>
2021-01-15 12:41:38 +0100

    BACKLOG-15342 : fix jdk11 issue (#730)

--- taglib/src/main/java/org/jahia/taglibs/template/include/ModuleTag.java
+++ taglib/src/main/java/org/jahia/taglibs/template/include/ModuleTag.java
@@ -560,7 +560,7 @@ public class ModuleTag extends BodyTagSupport implements ParamParent {
     }
 
     private String filterNodeTypes(String nodeTypes) throws RepositoryException {
-        if (nodeTypes == null) {
+        if (nodeTypes == null || StringUtils.isEmpty(StringUtils.trim(nodeTypes))) {
             return null;
         }
         Set<String> modules = getNode() != null && getNode().getResolveSite() != null ?
@@ -568,7 +568,7 @@ public class ModuleTag extends BodyTagSupport implements ParamParent {
         if (modules != null) {
             modules.add("system-jahia");
         }
-        return Patterns.SPACE.splitAsStream(nodeTypes)
+        return Arrays.stream(StringUtils.split(nodeTypes))
                 .filter(ThrowingPredicate.unchecked(nt -> NodeTypeRegistry.getInstance().hasNodeType(nt)))
                 .filter(ThrowingPredicate.unchecked(nt -> modules == null || modules.contains(NodeTypeRegistry.getInstance().getNodeType(nt).getSystemId())))
                 .sorted()

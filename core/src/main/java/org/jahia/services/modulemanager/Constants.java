b8c77852abf3f7f58162a49fe26219e4c4967b94
Geofrey Flores <gflores@jahia.com>
2021-01-28 09:03:47 -0500

    TECH-327: Fix SonarQube check

--- core/src/main/java/org/jahia/services/modulemanager/Constants.java
+++ core/src/main/java/org/jahia/services/modulemanager/Constants.java
@@ -50,57 +50,60 @@ import java.util.jar.Attributes;
  * 
  * @author Sergiy Shyrkov
  */
-public interface Constants {
+public final class Constants {
+
+    private Constants() {}
+
     
-    String ATTR_JAHIA_MODULE_TYPE = "Jahia-Module-Type";
+    public static final  String ATTR_JAHIA_MODULE_TYPE = "Jahia-Module-Type";
     
-    Attributes.Name ATTR_NAME_BUNDLE_NAME = new Attributes.Name(org.osgi.framework.Constants.BUNDLE_NAME);
+    public static final  Attributes.Name ATTR_NAME_BUNDLE_NAME = new Attributes.Name(org.osgi.framework.Constants.BUNDLE_NAME);
 
-    Attributes.Name ATTR_NAME_BUNDLE_SYMBOLIC_NAME = new Attributes.Name(org.osgi.framework.Constants.BUNDLE_SYMBOLICNAME);
+    public static final  Attributes.Name ATTR_NAME_BUNDLE_SYMBOLIC_NAME = new Attributes.Name(org.osgi.framework.Constants.BUNDLE_SYMBOLICNAME);
     
-    Attributes.Name ATTR_NAME_BUNDLE_VERSION = new Attributes.Name(org.osgi.framework.Constants.BUNDLE_VERSION);
+    public static final  Attributes.Name ATTR_NAME_BUNDLE_VERSION = new Attributes.Name(org.osgi.framework.Constants.BUNDLE_VERSION);
 
-    Attributes.Name ATTR_NAME_FRAGMENT_HOST = new Attributes.Name(org.osgi.framework.Constants.FRAGMENT_HOST);
+    public static final  Attributes.Name ATTR_NAME_FRAGMENT_HOST = new Attributes.Name(org.osgi.framework.Constants.FRAGMENT_HOST);
 
-    Attributes.Name ATTR_NAME_GROUP_ID = new Attributes.Name("Jahia-GroupId");
+    public static final  Attributes.Name ATTR_NAME_GROUP_ID = new Attributes.Name("Jahia-GroupId");
 
-    Attributes.Name ATTR_NAME_IMPL_TITLE = new Attributes.Name("Implementation-Title");
+    public static final  Attributes.Name ATTR_NAME_IMPL_TITLE = new Attributes.Name("Implementation-Title");
 
-    Attributes.Name ATTR_NAME_IMPL_VERSION = new Attributes.Name("Implementation-Version");
+    public static final  Attributes.Name ATTR_NAME_IMPL_VERSION = new Attributes.Name("Implementation-Version");
 
-    Attributes.Name ATTR_NAME_JAHIA_DEPENDS = new Attributes.Name("Jahia-Depends");
+    public static final  Attributes.Name ATTR_NAME_JAHIA_DEPENDS = new Attributes.Name("Jahia-Depends");
 
-    Attributes.Name ATTR_NAME_JAHIA_MODULE_TYPE = new Attributes.Name(ATTR_JAHIA_MODULE_TYPE);
+    public static final  Attributes.Name ATTR_NAME_JAHIA_MODULE_TYPE = new Attributes.Name(ATTR_JAHIA_MODULE_TYPE);
     
-    Attributes.Name ATTR_NAME_JAHIA_PACKAGE_DESCRIPTION = new Attributes.Name("Jahia-Package-Description");
+    public static final  Attributes.Name ATTR_NAME_JAHIA_PACKAGE_DESCRIPTION = new Attributes.Name("Jahia-Package-Description");
 
-    Attributes.Name ATTR_NAME_JAHIA_PACKAGE_LICENSE = new Attributes.Name("Jahia-Package-License");
+    public static final  Attributes.Name ATTR_NAME_JAHIA_PACKAGE_LICENSE = new Attributes.Name("Jahia-Package-License");
 
-    Attributes.Name ATTR_NAME_JAHIA_PACKAGE_NAME = new Attributes.Name("Jahia-Package-Name");
+    public static final  Attributes.Name ATTR_NAME_JAHIA_PACKAGE_NAME = new Attributes.Name("Jahia-Package-Name");
     
-    Attributes.Name ATTR_NAME_JAHIA_PACKAGE_VERSION = new Attributes.Name("Jahia-Package-Version");
+    public static final Attributes.Name ATTR_NAME_JAHIA_PACKAGE_VERSION = new Attributes.Name("Jahia-Package-Version");
 
-    Attributes.Name ATTR_NAME_JAHIA_REQUIRED_VERSION = new Attributes.Name("Jahia-Required-Version");
-    
-    Attributes.Name ATTR_NAME_PROVIDE_CAPABILITY = new Attributes.Name("Provide-Capability");
-    
-    Attributes.Name ATTR_NAME_REQUIRE_CAPABILITY = new Attributes.Name("Require-Capability");
+    public static final Attributes.Name ATTR_NAME_JAHIA_REQUIRED_VERSION = new Attributes.Name("Jahia-Required-Version");
 
-    Attributes.Name ATTR_NAME_ROOT_FOLDER = new Attributes.Name("root-folder");
+    public static final Attributes.Name ATTR_NAME_PROVIDE_CAPABILITY = new Attributes.Name("Provide-Capability");
 
-    String BUNDLE_SERVICE_PROPERTY_CLUSTERED = "clustered";
-    
-    String OSGI_CAPABILITY_SERVER = "com.jahia.server";
+    public static final Attributes.Name ATTR_NAME_REQUIRE_CAPABILITY = new Attributes.Name("Require-Capability");
 
-    String OSGI_CAPABILITY_SERVER_VERSION = "version";
+    public static final Attributes.Name ATTR_NAME_ROOT_FOLDER = new Attributes.Name("root-folder");
 
-    String OSGI_CAPABILITY_MODULE_DEPENDENCIES = "com.jahia.modules.dependencies";
+    public static final String BUNDLE_SERVICE_PROPERTY_CLUSTERED = "clustered";
 
-    String OSGI_CAPABILITY_MODULE_DEPENDENCIES_KEY = "moduleIdentifier";
+    public static final String OSGI_CAPABILITY_SERVER = "com.jahia.server";
 
-    String OSGI_CAPABILITY_MODULE_DEPENDENCIES_VERSION_KEY = "moduleVersion";
-    
-    String URL_PROTOCOL_DX = "dx";
-    
-    String URL_PROTOCOL_MODULE_DEPENDENCIES = "legacydepends";
+    public static final String OSGI_CAPABILITY_SERVER_VERSION = "version";
+
+    public static final String OSGI_CAPABILITY_MODULE_DEPENDENCIES = "com.jahia.modules.dependencies";
+
+    public static final String OSGI_CAPABILITY_MODULE_DEPENDENCIES_KEY = "moduleIdentifier";
+
+    public static final String OSGI_CAPABILITY_MODULE_DEPENDENCIES_VERSION_KEY = "moduleVersion";
+
+    public static final String URL_PROTOCOL_DX = "dx";
+
+    public static final String URL_PROTOCOL_MODULE_DEPENDENCIES = "legacydepends";
 }

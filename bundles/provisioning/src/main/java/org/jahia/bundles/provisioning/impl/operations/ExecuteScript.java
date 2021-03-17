3be46f8b7b192a717202fd9c7d8d00c3add41649
Thomas Draier <tdraier@jahia.com>
2021-03-17 18:13:49 +0100

    TECH-396: Add executeScript operation (#802)

---
+++ bundles/provisioning/src/main/java/org/jahia/bundles/provisioning/impl/operations/ExecuteScript.java
@@ -0,0 +1,84 @@
+/*
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *                                 http://www.jahia.com
+ *
+ *     Copyright (C) 2002-2020 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.bundles.provisioning.impl.operations;
+
+import org.jahia.services.provisioning.ExecutionContext;
+import org.jahia.services.provisioning.Operation;
+import org.jahia.tools.patches.Patcher;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.UrlResource;
+
+import java.net.URL;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Execute script operation
+ */
+@Component(service = Operation.class, property = "type=executeScript")
+public class ExecuteScript implements Operation {
+    private static final Logger logger = LoggerFactory.getLogger(ExecuteScript.class);
+    public static final String EXECUTE_SCRIPT = "executeScript";
+
+    @Override
+    public boolean canHandle(Map<String, Object> entry) {
+        return entry.get(EXECUTE_SCRIPT) instanceof String;
+    }
+
+    @Override
+    public void perform(Map<String, Object> entry, ExecutionContext executionContext) {
+        String script = (String) entry.get(EXECUTE_SCRIPT);
+        if (script != null) {
+            try {
+                Patcher.getInstance().executeScripts(Collections.singleton(new UrlResource(new URL(script))), "",
+                        (resource, s) -> logger.info("Script {} result: {}", script, s)
+                );
+            } catch (Exception e) {
+                logger.error("Cannot include {}", script, e);
+            }
+        }
+    }
+}

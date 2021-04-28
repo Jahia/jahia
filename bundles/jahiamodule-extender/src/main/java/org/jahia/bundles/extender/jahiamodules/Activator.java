d83db95620a3bee20f2e45d505b536bdd50c4658
Alex Karmanov <akarmanov@jahia.com>
2021-04-28 11:04:47 -0400

    QA-13664 Add persistent state application log (#895)

--- bundles/jahiamodule-extender/src/main/java/org/jahia/bundles/extender/jahiamodules/Activator.java
+++ bundles/jahiamodule-extender/src/main/java/org/jahia/bundles/extender/jahiamodules/Activator.java
@@ -327,7 +327,9 @@ public class Activator implements BundleActivator {
     private void checkPersistentStateMarker() {
         File applyBundlesPersistentStatesMarker = new File(SettingsBean.getInstance().getJahiaVarDiskPath(), "[persisted-bundles].dorestore");
         if (System.getProperty("applyBundlesPersistentStates") != null || applyBundlesPersistentStatesMarker.exists()) {
+            logger.info("== Applying persistent states");
             ModuleUtils.getModuleManager().applyBundlesPersistentStates(null);
+            logger.info("== Successfully applied persistent states");
             if (applyBundlesPersistentStatesMarker.exists()) {
                 applyBundlesPersistentStatesMarker.delete();
             }

2ddcd5c6e3fc5c1540b4b35fcee2691e2adaec58
Michael De Checchi <mdechecchi@jahia.com>
2022-02-23 15:16:00 +0100

    QA-14203 - Update copyright year (#1223)

--- core/src/main/java/org/jahia/services/pwdpolicy/JahiaPasswordPolicyManager.java
+++ core/src/main/java/org/jahia/services/pwdpolicy/JahiaPasswordPolicyManager.java
@@ -5,7 +5,7 @@
  *
  *                                 http://www.jahia.com
  *
- *     Copyright (C) 2002-2020 Jahia Solutions Group SA. All rights reserved.
+ *     Copyright (C) 2002-2022 Jahia Solutions Group SA. All rights reserved.
  *
  *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
  *     1/GPL OR 2/JSEL
@@ -40,45 +40,6 @@
  *
  *     If you are unsure which license is appropriate for your use,
  *     please contact the sales department at sales@jahia.com.
- *
- *      INCLUDED LICENSES:
- *
- *      Indiana University Extreme! Lab Software License Version 1.1.1
- *
- *      Copyright (c) 2002 Extreme! Lab, Indiana University. All rights reserved.
- *
- *      Redistribution and use in source and binary forms, with or without modification, are permitted provided that
- *      the following conditions are met:
- *
- *      1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
- *         following disclaimer.
- *
- *      2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
- *         following disclaimer in the documentation and/or other materials provided with the distribution.
- *
- *      3. The end-user documentation included with the redistribution, if any, must include the following
- *         acknowledgment:
- *
- *      "This product includes software developed by the Indiana University Extreme! Lab
- *       (http://www.extreme.indiana.edu/)."
- *
- *      Alternately, this acknowledgment may appear in the software itself, if and wherever such third-party
- *      acknowledgments normally appear.
- *
- *      4. The names "Indiana University" and "Indiana University Extreme! Lab" must not be used to endorse or promote
- *         products derived from this software without prior written permission. For written permission, please contact
- *          (http://www.extreme.indiana.edu/).
- *
- *      5. Products derived from this software may not use "Indiana University" name nor may "Indiana University"
- *         appear in their name, without prior written permission of the Indiana University.
- *
- *     THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *     IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *     AUTHORS, COPYRIGHT HOLDERS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- *     EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- *     IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- *     THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.jahia.services.pwdpolicy;
 
--- core/src/main/java/org/springframework/orm/hibernate5/HibernateOptimisticLockingFailureException.java
+++ core/src/main/java/org/springframework/orm/hibernate5/HibernateOptimisticLockingFailureException.java
@@ -1,19 +1,46 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *                                 http://www.jahia.com
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *     Copyright (C) 2002-2022 Jahia Solutions Group SA. All rights reserved.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ===================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
  */
-
 package org.springframework.orm.hibernate5;
 
 import org.hibernate.StaleObjectStateException;

7e4cb437cd5a4c891ffff960a6c58a472b93012a
Cedric Mailleux <cmailleux@jahia.com>
2022-03-16 10:22:21 -0400

    TECH-644: Use newest JVM settings for JDK 11 performances/stability (#1234)
    * TECH-644: Use newest JVM setiings for JDK 11 performances/stability
    
    * TECH-644: Use newest JVM setiings for JDK 11 performances/stability

--- docker/docker-jahia-core/setenv.sh
+++ docker/docker-jahia-core/setenv.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 # GC setup
-export CATALINA_OPTS="${CATALINA_OPTS} -XX:+UseParallelGC -XX:SurvivorRatio=8"
-
+export CATALINA_OPTS="${CATALINA_OPTS} -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseStringDeduplication -XX:MaxTenuringThreshold=7"
+export CATALINA_OPTS="${CATALINA_OPTS} -XX:+ParallelRefProcEnabled -XshowSettings:vm -XX:+UnlockDiagnosticVMOptions "
+export CATALINA_OPTS="${CATALINA_OPTS} -XX:GuaranteedSafepointInterval=0 -XX:-UseBiasedLocking -XX:+UseCountedLoopSafepoints -XX:LoopStripMiningIter=100"
+export CATALINA_OPTS="${CATALINA_OPTS} -XX:+SafepointTimeout -XX:SafepointTimeoutDelay=1000"
 # JMX setup
 export CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7199 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
 
 # Log/debug info
-export CATALINA_OPTS="${CATALINA_OPTS} -Xlog:gc::time,uptime,level,pid,tid,tags -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks"
+export CATALINA_OPTS="${CATALINA_OPTS} -Xlog:gc*,gc+ref=debug,gc+heap=debug,gc+age=trace:file=gc-%p-%t.log:tags,uptime,time,level:filecount=10,filesize=20m"
+export CATALINA_OPTS="${CATALINA_OPTS} -Xlog:os+container=debug,pagesize=debug:file=os-container-pagesize-%p-%t.log:tags,uptime,time,level:filecount=10,filesize=20m"
+export CATALINA_OPTS="${CATALINA_OPTS} -Xlog:safepoint*:file=safepoints-%p-%t.log:tags,uptime,time,level:filecount=10,filesize=20m"
+export CATALINA_OPTS="${CATALINA_OPTS} -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks"
 
 # Memory settings
 export CATALINA_OPTS="${CATALINA_OPTS} -XX:+UseContainerSupport -XX:MaxRAMPercentage=${MAX_RAM_PERCENTAGE}"

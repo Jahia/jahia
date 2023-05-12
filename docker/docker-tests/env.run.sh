7ac3650518730776067d552e90b8c56fac558bee
Michael De Checchi <mdechecchi@jahia.com>
2023-05-12 15:04:04 +0200

    BACKLOG-20794 - Update tomcat version and remove useless call in tests (#1584)

--- docker/docker-tests/env.run.sh
+++ docker/docker-tests/env.run.sh
@@ -23,22 +23,6 @@ done
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 echo " == Jahia became alive in ${ELAPSED_TIME} seconds"
 
-echo "$(date +'%d %B %Y - %k:%M') == Fetching the list of installed modules =="
-./node_modules/@jahia/jahia-reporter/bin/run utils:modules \
-  --moduleId="${MODULE_ID}" \
-  --jahiaUrl="${JAHIA_URL}" \
-  --jahiaPassword="${SUPER_USER_PASSWORD}" \
-  --filepath="results/installed-jahia-modules.json"
-echo "$(date +'%d %B %Y - %k:%M') == Modules fetched =="
-INSTALLED_MODULE_VERSION=$(cat results/installed-jahia-modules.json | jq '.module.version')
-if [[ $INSTALLED_MODULE_VERSION == "UNKNOWN" ]]; then
-  echo "$(date +'%d %B %Y - %k:%M') ERROR: Unable to detect module: ${MODULE_ID} on the remote system "
-  echo "$(date +'%d %B %Y - %k:%M') ERROR: The Script will exit"
-  echo "$(date +'%d %B %Y - %k:%M') ERROR: Tests will NOT run"
-  echo "failure" > ./results/test_failure
-  exit 1
-fi
-
 echo "$(date +'%d %B %Y - %k:%M') == Run tests =="
 yarn e2e:ci
 if [[ $? -eq 0 ]]; then

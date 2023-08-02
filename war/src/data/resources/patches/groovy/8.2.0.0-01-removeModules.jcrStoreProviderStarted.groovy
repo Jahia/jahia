12562889f8ddff4f0ca70a5a09d6fe4e16460570
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2023-08-02 16:16:16 -0400

    BACKLOG-20402: Uninstall content-editor for migration (#1674)

---
+++ war/src/data/resources/patches/groovy/8.2.0.0-01-removeModules.jcrStoreProviderStarted.groovy
@@ -0,0 +1,12 @@
+import java.util.stream.Collectors
+import javax.jcr.*
+import javax.jcr.query.*
+import org.jahia.services.content.*
+import org.jahia.services.modulemanager.persistence.jcr.BundleInfoJcrHelper;
+
+BundleInfoJcrHelper.storePersistentStates(
+        BundleInfoJcrHelper.getPersistentStates()
+                .stream()
+                .filter { bpi -> !(bpi.getSymbolicName() in ["content-editor"]) }
+                .collect(Collectors.toList())
+);

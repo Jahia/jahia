e0912aa50d03f9e8d416ac68b6e514ec775a43b5
Alex Karmanov <alexandrk_@hotmail.com>
2023-09-06 15:59:53 -0400

    TECH-759 Remove jdom v1 dependency (#1666)
    * TECH-759 Remove jdom v1 dependency
    
    * TECH-759 Restore exclusion
    
    * TECH-759 Restore exclusion
    
    * TECH-759 Restore exclusion
    
    * TECH-759 Restore exclusion
    
    * TECH-759 Remove unused exclusion
    
    * TECH-759 Remove conflict
    
    * TECH-759 Restore exclusion

--- war/src/data/resources/patches/groovy/8.0.0.0-01-jackrabbitUpgrade.beforeContextInitializing.groovy
+++ war/src/data/resources/patches/groovy/8.0.0.0-01-jackrabbitUpgrade.beforeContextInitializing.groovy
@@ -2,13 +2,13 @@ import org.apache.commons.io.FileUtils
 import org.jahia.commons.Version
 import org.jahia.settings.SettingsBean
 import org.jahia.tools.patches.Patcher
-import org.jdom.Document
-import org.jdom.Element
-import org.jdom.JDOMException
-import org.jdom.Namespace
-import org.jdom.input.SAXBuilder
-import org.jdom.output.Format
-import org.jdom.output.XMLOutputter
+import org.jdom2.Document
+import org.jdom2.Element
+import org.jdom2.JDOMException
+import org.jdom2.Namespace
+import org.jdom2.input.SAXBuilder
+import org.jdom2.output.Format
+import org.jdom2.output.XMLOutputter
 import org.slf4j.Logger
 
 

e2af2d8d78b0e9de0e0de173860a20c69399b9bc
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2024-05-22 15:44:33 -0400

    TECH-1823: Add support for default caching_sha2_password auth option (#2038)

--- docker/docker-jahia-core/bin/entrypoint.sh
+++ docker/docker-jahia-core/bin/entrypoint.sh
@@ -55,7 +55,7 @@ if [ ! -f "/usr/local/tomcat/conf/configured" ]; then
       case "$DB_VENDOR" in
           "mysql")
               DB_PORT=${DB_PORT:-3306}
-              DB_URL="jdbc:mysql://${DB_HOST}:${DB_PORT}/jahia?characterEncoding=UTF-8&sslMode=DISABLED"
+              DB_URL="jdbc:mysql://${DB_HOST}:${DB_PORT}/jahia?characterEncoding=UTF-8&sslMode=DISABLED&allowPublicKeyRetrieval=true"
               ;;
           "mariadb")
               DB_PORT=${DB_PORT:-3306}

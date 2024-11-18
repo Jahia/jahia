46e6a92172bb6ba7c95f896c37ad455d602c424a
Sonia Dusolle <42536778+sdusolle@users.noreply.github.com>
2024-11-18 13:52:12 +0100

    BACKLOG-23291: update copyright year (#2265)

--- core/src/main/java/org/jahia/bin/Jahia.java
+++ core/src/main/java/org/jahia/bin/Jahia.java
@@ -99,7 +99,7 @@ import java.util.Properties;
  */
 public final class Jahia {
 
-    public static final String YEAR = "2023";
+    public static final String YEAR = "2024";
     public static final String CODE_NAME = "Indigo";
     public static final String PRODUCT_NAME = "Jahia";
     public static final String VENDOR_NAME = "Jahia Solutions Group SA";

2072f730c6608a16330b2dac8caa8a77237899d8
Sonia Dusolle <sdusolle@jahia.com>
2024-11-19 10:42:06 +0100

    BACKLOG-23291: update copyright year

--- core/src/main/java/org/jahia/services/content/impl/jackrabbit/JahiaJournal.java
+++ core/src/main/java/org/jahia/services/content/impl/jackrabbit/JahiaJournal.java
@@ -48,7 +48,7 @@ package org.jahia.services.content.impl.jackrabbit;
  *
  *                                 http://www.jahia.com
  *
- *     Copyright (C) 2002-2023 Jahia Solutions Group SA. All rights reserved.
+ *     Copyright (C) 2002-2025 Jahia Solutions Group SA. All rights reserved.
  *
  *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
  *     1/GPL OR 2/JSEL

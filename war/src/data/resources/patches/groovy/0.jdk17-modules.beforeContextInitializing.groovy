18f5db085bf244564988a9b81ed302e0175d1745
David Griffon <dgriffon@jahia.com>
2025-03-05 10:27:20 +0100

    fix: always check for jdk version to install javascript modules  (#3464)

--- war/src/data/resources/patches/groovy/0.jdk17-modules.beforeContextInitializing.groovy
+++ war/src/data/resources/patches/groovy/0.jdk17-modules.beforeContextInitializing.groovy
@@ -13,6 +13,11 @@ def nonJDK11CompatibleModules = ["npm-modules-engine", "javascript-modules-engin
 def provisioningDirectory = Paths.get(SettingsBean.getInstance().getJahiaVarDiskPath() + "/patches/provisioning")
 def jdk = System.getProperty("java.version");
 
+// setResult(value) is bind to the context to provide the return value (a string) of the execution
+// if the value do not start with a dot, nothing is happening
+// if null or if it starts with a dot, the file is suffixed by ".installed" and the script is not executed at the next startup
+setResult("keep")
+
 def processProvisioningFile = { Path file ->
     {
         def lines = file.toFile().readLines()

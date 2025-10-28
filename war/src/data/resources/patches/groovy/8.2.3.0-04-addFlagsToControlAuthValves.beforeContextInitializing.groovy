6ad713bc5f29b039848455f780ec3589e2d88ad8
Baptiste Grimaud <bgrimaud@jahia.com>
2025-10-28 10:06:34 +0100

    feat: Allow to disable core authentication valves individually (#4395)

---
+++ war/src/data/resources/patches/groovy/8.2.3.0-04-addFlagsToControlAuthValves.beforeContextInitializing.groovy
@@ -0,0 +1,33 @@
+import org.jahia.settings.JahiaPropertiesUtils
+
+private updateJahiaProperties(){
+    JahiaPropertiesUtils.addEntry("auth.valve.basicAccess.enabled", "true",
+            "# HttpBasicAuthValveImpl::\n",
+            ".*auth\\.token\\.enabled.*",
+            "A new property was introduced with this version to enable the basic access authentication valve (HttpBasicAuthValveImpl).\n" +
+                    "Please manually add the following line into your jahia.properties file if you need to change the default value\n" +
+                    "auth.valve.basicAccess.enabled = true");
+    JahiaPropertiesUtils.addEntry("auth.valve.loginEngine.enabled", "true",
+            "# LoginEngineAuthValveImpl:\n",
+            ".*auth\\.valve\\.basicAccess\\.enabled.*",
+            "A new property was introduced with this version to enable the login engine valve " +
+                    "(LoginEngineAuthValveImpl).\n" +
+                    "Please manually add the following line into your jahia.properties file if you need to change the default value\n" +
+                    "auth.valve.loginEngine.enabled = true");
+    JahiaPropertiesUtils.addEntry("auth.valve.session.enabled", "true",
+            "# LoginEngineAuthValveImpl:\n",
+            ".*auth\\.valve\\.loginEngine\\.enabled.*",
+            "A new property was introduced with this version to enable the session valve " +
+                    "(SessionAuthValveImpl).\n" +
+                    "Please manually add the following line into your jahia.properties file if you need to change the default value\n" +
+                    "auth.valve.session.enabled = true");
+    JahiaPropertiesUtils.addEntry("auth.valve.cookie.enabled", "true",
+            "# LoginEngineAuthValveImpl:\n",
+            ".*auth\\.valve\\.session\\.enabled.*",
+            "A new property was introduced with this version to enable the cookie valve " +
+                    "(CookieAuthValveImpl).\n" +
+                    "Please manually add the following line into your jahia.properties file if you need to change the default value\n" +
+                    "auth.valve.cookie.enabled = true");
+}
+
+updateJahiaProperties();

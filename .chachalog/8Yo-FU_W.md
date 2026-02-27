50e5425dcd2aabba61062f9d83ef0b4535dd9311
Baptiste Grimaud <bgrimaud@jahia.com>
2026-02-27 17:17:53 +0100

    fix: Add indexes for content history table for fresh installations (#4761)

---
+++ .chachalog/8Yo-FU_W.md
@@ -0,0 +1,6 @@
+---
+# Allowed version bumps: patch, minor, major
+jahia-private: patch
+---
+
+Add indexes for content history table for fresh installations (#4761)
--- war/src/data/resources/db/sql/schema/mssql/01-create-jahia-schema.sql
+++ war/src/data/resources/db/sql/schema/mssql/01-create-jahia-schema.sql
@@ -11,6 +11,10 @@
         primary key (id)
     );
 
+    create index idx_history_duplicate_check on jahia_contenthistory(uuid, entry_date, property_name, entry_action);
+    create index idx_history_date on jahia_contenthistory(entry_date);
+    create index idx_history_uuid_date on jahia_contenthistory(uuid, entry_date);
+
     create table jahia_db_test (
         testfield varchar(255) not null,
         primary key (testfield)

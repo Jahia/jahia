bd8b6976838c80139020c714cdb13276fb24a46f
Volodymyr Tereshchuk <vtereshchuk@jahia.com>
2026-04-08 09:05:38 +0200

    fix: fix patch file (filename and missing property) (#4916)

--- war/src/data/resources/patches/groovy/8.2.4.0-02-documentServiceJahiaProperties.beforeContextInitializing.groovy
+++ war/src/data/resources/patches/groovy/8.2.4.0-02-documentServiceJahiaProperties.beforeContextInitializing.groovy
@@ -3,5 +3,6 @@ import org.jahia.settings.JahiaPropertiesUtils
 JahiaPropertiesUtils.addCommentBeforeProperty("documentConverter.enabled", "Document conversion service is deprecated since Jahia 8.2.4.0 and will be removed in a future version");
 JahiaPropertiesUtils.addCommentBeforeProperty("jahia.dm.viewer.enabled", "Document viewer service is deprecated since Jahia 8.2.4.0 and will be removed in a future version");
 JahiaPropertiesUtils.addCommentBeforeProperty("jahia.dm.thumbnails.enabled", "Document thumbnails service is deprecated since Jahia 8.2.4.0 and will be removed in a future version");
+JahiaPropertiesUtils.addCommentBeforeProperty("jahia.dm.thumbnails.video.enabled", "Video thumbnails service is deprecated since Jahia 8.2.4.0 and will be removed in a future version");
 JahiaPropertiesUtils.addCommentBeforeProperty("jahia.dm.thumbnails.video.ffmpeg", "Video thumbnails service is deprecated since Jahia 8.2.4.0 and will be removed in a future version");
 
